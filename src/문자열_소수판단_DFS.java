0ab37db0b9e6e592fe85981adf2e506b285bc835
leejk298 <5552269@naver.com>
2023-02-14 09:46:50 +0900

    문자열_소수판단_DFS

---
+++ src/문자열_소수판단_DFS.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class 문자열_소수판단_DFS {
+    public static void main(String[] arg) {
+
+        Solution solution = new Solution();
+        solution.solution("1087");
+    }
+
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static boolean[] check = new boolean[7];
+
+    static class Solution {
+        public int solution(String numbers) {
+            int answer = 0;
+            for(int i=0; i<numbers.length(); i++){
+                dfs(numbers,"",i+1);
+            }
+
+            for(int i=0; i<arr.size(); i++){
+                if(prime(arr.get(i))) answer++;
+            }
+
+            return answer;
+
+        }
+    }
+
+    static void dfs(String str, String temp, int m) {
+        if(temp.length() == m){
+            int num = Integer.parseInt(temp);
+            if(!arr.contains(num)){
+                arr.add(num);
+            }
+        }
+
+        for(int i=0; i<str.length(); i++){
+            if(!check[i]){
+                check[i] = true;
+                temp += str.charAt(i);
+                dfs(str, temp, m);
+                check[i] = false;
+                temp = temp.substring(0, temp.length()-1);
+            }
+        }
+
+    }
+    //소수 판단
+    static boolean prime(int n) {
+        if(n<2) return false;
+
+        for(int i=2; i*i<=n; i++) {
+            if(n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
