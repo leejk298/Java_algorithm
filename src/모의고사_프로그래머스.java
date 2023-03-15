ca7e4e8680a7629b659a7b37a93a1d7502ed6654
leejk298 <5552269@naver.com>
2023-03-15 12:03:26 +0900

    모의고사_프로그래머스

---
+++ src/모의고사_프로그래머스.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class 모의고사_프로그래머스 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.solution(new int[] {1, 3, 2, 4, 2})));
+    }
+
+    static class Solution {
+        public int[] solution(int[] answers) {
+            int[] answer;
+            int[] a = new int[]{1, 2, 3, 4, 5};
+            int[] b = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+            int[] c = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+            int[] count = new int[3];
+            for(int i = 0; i < answers.length; i++) {
+                if(answers[i] == a[i % 5])
+                    count[0]++;
+                if(answers[i] == b[i % 8])
+                    count[1]++;
+                if(answers[i] == c[i % 10])
+                    count[2]++;
+            }
+
+            int max = -1;
+            for(int i = 0; i < 3; i++)
+                if(count[i] > max)
+                    max = count[i];
+
+            int maxCount = 0;
+            for(int i = 0; i < 3; i++)
+                if(count[i] == max)
+                    maxCount++;
+
+            answer = new int[maxCount];
+            int index = 0;
+            for(int i = 0; i < 3; i++)
+                if(count[i] == max)
+                    answer[index++] = i + 1;
+
+            return answer;
+        }
+    }
+}
