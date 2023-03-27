988b560d1bfc0cdd4413bf171743df92070fc0f9
leejk298 <5552269@naver.com>
2023-03-28 08:48:14 +0900

    시저암호_프로그래머스

---
+++ src/시저암호_프로그래머스.java
@@ -0,0 +1,38 @@
+public class 시저암호_프로그래머스 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("AB", 1));
+    }
+
+    static class Solution {
+        public String solution(String s, int n) {
+            String answer = "";
+
+            for(int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+
+                if(ch == ' ') {
+                    answer += ch;
+
+                    continue;
+                }
+
+                if(ch >= 'a' && ch <= 'z') {
+                    if(ch + n > 'z') {
+                        answer += (char)(ch + n - 26);
+                    } else {
+                        answer += (char)(ch + n);
+                    }
+                } else if(ch >= 'A' && ch <= 'Z') {
+                    if(ch + n > 'Z') {
+                        answer += (char)(ch + n - 26);
+                    } else {
+                        answer += (char)(ch + n);
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
