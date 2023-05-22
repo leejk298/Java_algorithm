b7212ef28c6919d971d99712ba131fca347f572a
leejk298 <5552269@naver.com>
2023-05-22 12:12:13 +0900

    너비우선탐색2_백준

---
+++ src/너비우선탐색2_백준.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+/*
+5 5 1
+1 4
+1 2
+2 3
+2 4
+3 4
+ */
+
+public class 너비우선탐색2_백준 {
+    static int N, M, S, num;    // 크기, 시작점, 탐색 순서
+    static List<Integer>[] A;   // 인접리스트
+    static int[] order;         // 탐색 순서배열
+    static boolean[] visited;   // 방문배열
+
+    public static void init() { // 초기화
+
+        Scanner sc = new Scanner(System.in);    // 입력
+
+        N = sc.nextInt();   // 정점
+        M = sc.nextInt();   // 엣지
+        S = sc.nextInt();   // 시작점
+        num = 1;            // 탐색 순서
+
+        // 초기화
+        A = new ArrayList[N + 1];
+        order = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        for(int i = 1; i <= N; i++)
+            A[i] = new ArrayList<>();   // 인접리스트 구현
+
+        for(int i = 0; i < M; i++) {    // 엣지 개수만큼
+            int s = sc.nextInt();   // 시작
+            int e = sc.nextInt();   // 끝
+
+            A[s].add(e);    // 무방향
+            A[e].add(s);
+        }
+
+        for(int i = 1; i <= N; i++) // 정점 개수만큼
+            Collections.sort(A[i], Collections.reverseOrder()); // 내림차순 정렬, 번호가 높은 노드부터
+    }
+
+    public static void BFS(int v) { // BFS
+        Queue<Integer> queue = new LinkedList<>();  // 큐
+
+        queue.offer(v); // 시작점 삽입
+        visited[v] = true;  // 방문여부 갱신
+
+        while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
+            int now = queue.poll(); // 하나 꺼내어
+            order[now] = num++; // 탐색 순서 저장
+
+            for(int i = 0; i < A[now].size(); i++) {    // 인접리스트 순회
+                int next = A[now].get(i);   // 다음 노드
+
+                if(!visited[next]) {    // 방문하지 않았으면
+                    visited[next] = true;   // 방문여부 갱신
+                    queue.offer(next);  // 큐에 삽입
+                }
+            }
+        }
+    }
+
+    public static void printOrder() {   // 탐색 순서 출력
+
+        for(int i = 1; i <= N; i++) // 정점 개수만큼
+            System.out.println(order[i]);   // 출력
+    }
+
+    public static void main(String[] args) {
+
+        init(); // 초기화
+
+        BFS(S); // BFS
+
+        printOrder();   // 탐색 순서 출력
+    }
+}
