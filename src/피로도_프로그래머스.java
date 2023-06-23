1ff268eed45b9d094650fdc25c3997494287220c
leejk298 <5552269@naver.com>
2023-06-23 11:55:22 +0900

    피로도_프로그래머스

--- src/피로도_프로그래머스.java
+++ src/피로도_프로그래머스.java
@@ -5,28 +5,32 @@ public class 피로도_프로그래머스 {
     }
 
     static class Solution {
-        static int answer = 0;
-        static boolean[] visited;
+        static int answer;  // 결과값
+        static boolean[] visited;   // 방문배열
 
-        public int solution(int k, int[][] dungeons) {
-            visited = new boolean[dungeons.length];
+        public static void DFS(int count, int k, int[][] d) {   // DFS
 
-            DFS(0, k, dungeons);
+            answer = Math.max(answer, count);   // 최대값 설정
 
-            return answer;
-        }
+            for(int i = 0; i < d.length; i++) { // 크기만큼
+                if(d[i][0] <= k && !visited[i]) {   // 필요 피로도보다 크거나 같고 방문한 적이 없으면
+                    visited[i] = true;  // 방문
+                    DFS(count + 1, k - d[i][1], d); // DFS, 횟수 1개 카운트
 
-        static void DFS(int depth, int k, int[][] d) {
-            for(int i = 0; i < d.length; i++) {
-                if(!visited[i] && k >= d[i][0]) {
-                    visited[i] = true;
-
-                    DFS(depth + 1, k - d[i][1], d);
-                    visited[i] = false;
+                    visited[i] = false; // 리턴되면 방문 여부 갱신
                 }
             }
+        }
 
-            answer = Math.max(answer, depth);
+        public int solution(int k, int[][] dungeons) {
+
+            // 초기화
+            answer = -1;
+            visited = new boolean[dungeons.length];
+
+            DFS(0, k, dungeons);    // DFS, 횟수, 총 피로도
+
+            return answer;  // 결과값 출력
         }
     }
 }
