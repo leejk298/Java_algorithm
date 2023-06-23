8d5ea179eb2b26f5b65997688eeb6fde0b324923
leejk298 <5552269@naver.com>
2023-06-23 13:27:20 +0900

    모음사전_프로그래머스

--- src/모음사전_프로그래머스.java
+++ src/모음사전_프로그래머스.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class 모음사전_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -6,16 +8,17 @@ public class 모음사전_프로그래머스 {
 
     static class Solution {
         public int solution(String word) {
-            int answer = 0;
-            String str = "AEIOU";
-            int total = 3905;
 
-            for(String s : word.split("")) {
-                total /= 5;
-                answer += total * str.indexOf(s) + 1;
+            String str = "AEIOU";   // 기준 문자열
+            int[] order = {781, 156, 31, 6, 1}; // 자리수가 해당 인덱스만큼 커짐
+
+            int answer = word.length(); // 길이로 초기화
+            for(int i = 0; i < word.length(); i++){ // 길이만큼
+                int index = str.indexOf(word.charAt(i));    // 기준 문자열에서 어디 인덱스인지
+                answer += order[i] * index; // 인덱스에 해당하는 자리수만큼 곱해서 더해줌
             }
 
-            return answer;
+            return answer;  // 결과값 리턴
         }
     }
 }
