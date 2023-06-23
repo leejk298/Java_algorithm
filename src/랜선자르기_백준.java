469c2df16674cefcc1b425de3b725ab9468b356e
leejk298 <5552269@naver.com>
2023-06-23 19:58:34 +0900

    랜선자르기_백준

--- src/랜선자르기_백준.java
+++ src/랜선자르기_백준.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /*
 4 11
@@ -9,45 +10,58 @@ import java.util.*;
  */
 
 public class 랜선자르기_백준 {
+    static int N, K;    // 크기, 잘라야하는 개수
+    static long S, E;   // 인덱스
+    static long[] arr;  // 입력배열
 
-    public static long BinarySearch(int[] arr, int K, long max) {   // 이분탐색
+    public static void init() throws IOException {  // 초기화
 
-        long min = 1, mid = 0;  // 초기화
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        while(min <= max) {  // 역전이 아니면 반복
-            mid = (max + min) / 2;  // 중앙값
+        N = Integer.parseInt(st.nextToken());   // 크기
+        K = Integer.parseInt(st.nextToken());   // 개수
 
-            long count = 0; // 개수
-            for(long num : arr) // 순회
-                count += num / mid; // 개수 카운트
-
-            if(count < K)   // 작으면
-                max = mid - 1;  // 큰 쪽 인덱스 갱신
-            else    // 크면
-                min = mid + 1;  // 작은 쪽 인덱스 갱신
-        }
-
-        return (max + min) / 2; // 최대 길이 리턴
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);    // 입력
-
-        int N = sc.nextInt();   // 크기
-        int K = sc.nextInt();   // 필요한 개수
+        // 인덱스 초기화
+        S = 1;  // 길이가 1부터이므로 최소 1
+        E = Integer.MIN_VALUE;  // 최대 길이구해야하므로, 최소값으로 초기화
 
         // 초기화
-        int[] arr = new int[N];
-        long max = 0, res = 0;
+        arr = new long[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(bf.readLine());
+            E = Math.max(E, arr[i]);    // 최악의 경우 구하기
+        }
+    }
 
-        for(int i = 0; i < N; i++) {    // 크기만큼
-            arr[i] = sc.nextInt();  // 저장
-            max = Math.max(max, arr[i]);    // 최대값 저장
+    public static void BinarySearch() {   // 이분 탐색
+
+        long res = 0;   // 결과값
+
+        while (S <= E) {    // 역전이 아니면 반복
+            long mid = (S + E) / 2; // 중앙값
+            int count = 0;  // 개수
+
+            for (long i : arr)  // 배열 순회
+                count += i / mid;   // 잘린 개수 카운트
+
+            if (K < count) {   // 자를 개수보다 큰 경우, 덜 잘라야하므로
+                S = mid + 1;    // 시작 인덱스 갱신
+            } else if (K > count)   // 작은 경우, 더 잘라야하므로
+                E = mid - 1;    // 끝 인덱스 갱신
+            else {  // 같으면
+                res = mid;  // 결과값 저장
+                S = mid + 1;    // 최대 길이 구해야하므로, 시작 인덱스 갱신
+            }
         }
 
-        res = BinarySearch(arr, K, max);    // 이분탐색
-
         System.out.println(res);    // 결과값 출력
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init(); // 초기화
+
+        BinarySearch();   // 최대 길이 출력
     }
 }
