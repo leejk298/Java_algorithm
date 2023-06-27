8462d196f7dfb1ff6bd1d25da2465c1f16f4c6ab
leejk298 <5552269@naver.com>
2023-06-27 16:24:12 +0900

    여행경로_프로그래머스

--- src/여행경로_프로그래머스.java
+++ src/여행경로_프로그래머스.java
@@ -3,40 +3,48 @@ import java.util.*;
 public class 여행경로_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.solution(new String[][]{{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}})));
+        System.out.println(Arrays.toString(solution.solution(new String[][] {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}})));
     }
 
     static class Solution {
-        boolean[] visited;
-        ArrayList<String> allRoute;
+        static boolean[] visited;   // 방문배열
+        static List<String> allRoute;   // 모든 여행 경로
+
+        public static void DFS(int count, String b, String route, String[][] s) {   // 백트래킹
+
+            // 베이스 케이스
+            if(count == s.length) { // 전부 경유했으면
+                allRoute.add(route);    // 경로 추가
+
+                return; // 함수 리턴, 완전 탐색 위해
+            }
+
+            // 재귀 케이스
+            for(int i = 0; i < s.length; i++) { // 경로배열 크기만큼
+                if(b.equals(s[i][0]) && !visited[i]) {  // 출발점이 같고 방문하지 않았으면
+                    visited[i] = true;  // 방문
+                    DFS(count + 1, s[i][1], route + " " + s[i][1], s);  // DFS
+
+                    visited[i] = false; // 리턴되면 해당 경로 방문 여부 갱신
+                }
+            }
+        }
 
         public String[] solution(String[][] tickets) {
-            String[] answer = {};
-            int cnt = 0;
+
+            String[] answer = {};   // 결과배열
+
+            // 초기화
             visited = new boolean[tickets.length];
             allRoute = new ArrayList<>();
 
-            dfs("ICN", "ICN", tickets, cnt);
+            DFS(0, "ICN", "ICN", tickets);  // DFS, "ICN"부터 시작, 경로를 문자열로 관리
 
-            Collections.sort(allRoute);
-            answer = allRoute.get(0).split(" ");
+            Collections.sort(allRoute); // 알파벳 순으로
 
-            return answer;
-        }
+            answer = allRoute.get(0).split(" ");    // 공백 기준으로 나눠
 
-        public void dfs(String start, String route, String[][] tickets, int cnt){
-            if(cnt == tickets.length){
-                allRoute.add(route);
-                return;
-            }
-
-            for(int i=0; i<tickets.length; i++){
-                if(start.equals(tickets[i][0]) && !visited[i]){
-                    visited[i] = true;
-                    dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
-                    visited[i] = false;
-                }
-            }
+            return answer;  // 결과배열 리턴
         }
     }
 }
