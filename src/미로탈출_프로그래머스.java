5b1b98dd23bc13af9b204a72d8af813feb73377e
leejk298 <5552269@naver.com>
2023-06-28 19:37:13 +0900

    미로탈출_프로그래머스

--- src/미로탈출_프로그래머스.java
+++ src/미로탈출_프로그래머스.java
@@ -3,92 +3,95 @@ import java.util.*;
 public class 미로탈출_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(new String[] {"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"}));
+        System.out.println(solution.solution(new String[]{"SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"}));
     }
 
     static class Solution {
-        static char[][] map;    // 문자배열
-        static int[] dx = {-1, 1, 0, 0};    // 상 하 좌 우
-        static int[] dy = {0, 0, -1, 1};
+        static char[][] map;    // 입력배열
+        static int[][] D;       // 거리배열
         static boolean[][] visited; // 방문배열
+        static int[] dx = {-1, 1, 0, 0};    // 4방향
+        static int[] dy = {0, 0, -1, 1};
+        static int[] S, L, E;   // 좌표
 
-        public int solution(String[] maps) {
-            int answer = 0; // 결과
+        public static void init(String[] maps) {    // 초기화
+
             // 초기화
             map = new char[maps.length][maps[0].length()];
             visited = new boolean[maps.length][maps[0].length()];
-            Node S = null, L = null, E = null;
+            D = new int[maps.length][maps[0].length()];
 
-            for(int i = 0; i < maps.length; i++) {  // 행
-                for(int j = 0; j < maps[i].length(); j++) { // 열
-                    if(maps[i].charAt(j) == 'S')    // 시작
-                        S = new Node(i, j, 0);
-                    if(maps[i].charAt(j) == 'L')    // 레버
-                        L = new Node(i, j, 0);
-                    if(maps[i].charAt(j) == 'E')    // 끝
-                        E = new Node(i, j, 0);
+            for (int i = 0; i < maps.length; i++) {  // 행
+                for (int j = 0; j < maps[i].length(); j++) { // 열
 
-                    map[i][j] = maps[i].charAt(j);  // 저장
+                    map[i][j] = maps[i].charAt(j);  // 입력배열 저장
+
+                    if (map[i][j] == 'S')    // 시작 좌표
+                        S = new int[]{i, j};
+                    if (map[i][j] == 'L')    // 레버
+                        L = new int[]{i, j};
+                    if (map[i][j] == 'E')    // 도착
+                        E = new int[]{i, j};
                 }
             }
-
-            answer += BFS(S.x, S.y, L.x, L.y);    // S -> L, BFS
-
-            if(answer != -1) {  // S -> L로 도달 가능하면
-                visited = new boolean[map.length][map[0].length];
-                int num = BFS(L.x, L.y, E.x, E.y);    // L -> E, BFS 수행
-
-                if(num == -1)   // L -> E 도달 불가
-                    answer = -1;    // -1 리턴
-                else    // 도달 가능
-                    answer += num;  // 결과 더해줌
-            }
-
-            return answer;  // 결과 리턴
         }
 
-        static int BFS(int sX, int sY, int eX, int eY) {    // BFS
-            Queue<Node> queue = new LinkedList<>(); // 큐
+        public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
 
-            queue.offer(new Node(sX, sY, 0)); // 시작점 넣고
-            visited[sX][sY] = true;   // 방문여부 갱신
+            return (x < 0 || x >= map.length || y < 0 || y >= map[0].length);
+        }
 
-            while(!queue.isEmpty()) {   // 비어있지않으면
-                Node now = queue.poll();    // 하나 꺼내어
+        public static int BFS(int[] S, int[] E) {   // BFS
 
-                int x = now.x;  // 좌표, 거리
-                int y = now.y;
-                int d = now.d;
+            Queue<int[]> queue = new LinkedList<>();    // 큐
 
-                if(x == eX && y == eY)    // 출구에 도달
-                    return d;   // 거리 리턴
+            queue.offer(new int[]{S[0], S[1]});    // 큐에 시작점 추가
+            visited[S[0]][S[1]] = true; // 방문 여부 갱신
+            D[S[0]][S[1]] = 0;  // 거리 0
 
-                for(int i = 0; i < 4; i++) {    // 4방향
-                    int tmpX = x + dx[i];
-                    int tmpY = y + dy[i];
+            while (!queue.isEmpty()) {   // 큐가 비어있지 않으면
+                int[] now = queue.poll();   // 하나 꺼내어
 
-                    if(tmpX < 0 || tmpY < 0 || tmpX >= map.length || tmpY >= map[0].length)    // 유효하지않으면
-                        continue;   // 건너뛰기
+                int nowX = now[0], nowY = now[1];   // 현재 좌표
+                if (nowX == E[0] && nowY == E[1])    // 도착하면
+                    return D[nowX][nowY];   // 거리값 리턴
 
-                    if(!visited[tmpX][tmpY] && map[tmpX][tmpY] != 'X') {    // 방문하지않았고 갈 수 있는 통로면
-                        visited[tmpX][tmpY] = true; // 방문여부 갱신
+                for (int i = 0; i < 4; i++) {   // 4방향
+                    int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
 
-                        queue.offer(new Node(tmpX, tmpY, d + 1));   // 큐에 삽입, 거리 1 증가
+                    if (isNotValidPos(tmpX, tmpY))  // 유효한지
+                        continue;
+
+                    if (!visited[tmpX][tmpY] && map[tmpX][tmpY] != 'X') {   // 방문하지 않았고 벽이 아니면
+                        visited[tmpX][tmpY] = true; // 방문
+                        D[tmpX][tmpY] = D[nowX][nowY] + 1;  // 거리값 갱신
+                        queue.offer(new int[]{tmpX, tmpY}); // 큐에 삽입
                     }
                 }
             }
 
-            return -1;  // 큐가 다 끝나고 여기로 오면 도달 불가이므로 -1 리턴
+            return -1;  // 여기 도달하면 도착하지 못한 것이므로 -1 리턴
         }
 
-        static class Node { // Node 클래스
-            int x, y, d;    // 좌표, 거리
+        public int solution(String[] maps) {
 
-            Node(int x, int y, int d) { // 생성자
-                this.x = x;
-                this.y = y;
-                this.d = d;
+            int answer = 0; // 결과값
+
+            init(maps); // 초기화
+
+            answer += BFS(S, L);    // BFS
+
+            if (answer != -1) { // 레버에 도달했으면
+                visited = new boolean[maps.length][maps[0].length()];   // 방문배열 초기화, 이전 경로로 돌아가야되는 경우도 있으므로
+                int num = BFS(L, E);    // BFS, 도착 좌표까지 비용
+
+                if (num == -1)  // 도달하지 못했으면
+                    answer = -1;    // -1
+                else    // 도달하면
+                    answer += num;  // 총 비용 갱신
             }
+
+            return answer;  // 총 비용 리턴
         }
     }
 }
