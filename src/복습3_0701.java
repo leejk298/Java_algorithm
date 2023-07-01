e9ff8d834e97d50fc153da8be4d3970cc9cf770d
leejk298 <5552269@naver.com>
2023-07-01 16:06:39 +0900

    복습3_0701

--- src/복습3_0701.java
+++ src/복습3_0701.java
@@ -8,19 +8,19 @@ public class 복습3_0701 {
     public static void init(int[][] lottery) {  // 초기화
 
         // 초기화
-        answer= 0;
+        answer = 0;
         count = new HashMap<>();
         check = new HashMap<>();
 
-        for(int i = 0; i < lottery.length; i++) {   // 입력배열 크기만큼
+        for (int i = 0; i < lottery.length; i++) {   // 입력배열 크기만큼
             int id = lottery[i][0], flag = lottery[i][1];   // id, 당첨 여부
 
             // 당첨이 아니면 => 당첨 전까지만 개수 카운트
-            if(flag == 0 && !check.containsKey(id)) // 꽝 개수 카운트
+            if (flag == 0 && !check.containsKey(id)) // 꽝 개수 카운트
                 count.put(id, count.getOrDefault(id, 0) + 1);   // 해시맵 저장
 
             // 당첨이면
-            if(flag == 1)
+            if (flag == 1)
                 check.put(id, flag);    // 해시맵 저장
         }
     }
@@ -30,12 +30,12 @@ public class 복습3_0701 {
         // 총 몇 명 당첨 + id
         int cnt = 0, sum = 0;
 
-        for(int id : check.keySet()) {  // 당첨 여부 해시맵 순회
-            if(check.get(id) == 1) {    // 당첨된 사람이면
+        for (int id : check.keySet()) {  // 당첨 여부 해시맵 순회
+            if (check.get(id) == 1) {    // 당첨된 사람이면
                 cnt++;  // 몇 명인 지 카운트
 
                 // 해당 사람이 몇 번 시도만에 당첨되었는지
-                if(count.containsKey(id))   // 키가 존재하면 여러 번 시도만에 당첨이므로
+                if (count.containsKey(id))   // 키가 존재하면 여러 번 시도만에 당첨이므로
                     sum += count.get(id);   // 총 합에 더해줌
 
                 /**
@@ -46,12 +46,12 @@ public class 복습3_0701 {
             }
         }
 
-        if(cnt != 0)    // 당첨자가 있으면
+        if (cnt != 0)    // 당첨자가 있으면
             answer = (sum + cnt) / cnt;
         /**
-         * (sum + cnt) => 처음 당첨될 때까지의 횟수 계산이므로
-                          여기서 한 번에 당첨한 경우도 들어감
-         */
+        * (sum + cnt) => 처음 당첨될 때까지의 횟수 계산이므로
+                         여기서 한 번에 당첨한 경우도 들어감
+        */
     }
 
     public static int solution(int[][] lottery) {
@@ -65,6 +65,6 @@ public class 복습3_0701 {
 
     public static void main(String[] args) {
 
-        System.out.println(solution(new int[][] {{1, 0}, {2, 0}, {3, 1}}));
+        System.out.println(solution(new int[][]{{1, 0}, {2, 0}, {3, 1}}));
     }
 }
