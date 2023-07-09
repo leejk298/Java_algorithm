95fe0d5905dd3fec4aef0d9b834226e6f29af4fe
leejk298 <5552269@naver.com>
2023-07-10 08:56:28 +0900

    거스름돈_프로그래머스

--- src/거스름돈_프로그래머스.java
+++ src/거스름돈_프로그래머스.java
@@ -6,17 +6,18 @@ public class 거스름돈_프로그래머스 {
 
     static class Solution {
         public int solution(int n, int[] money) {
-            int[] answer = new int[n + 1];
 
-            answer[0] = 1;
-            for(int i = 0; i < money.length; i++) {
-                for(int j = money[i]; j <= n; j++) {
-                    answer[j] += answer[j - money[i]];
+            int[] answer = new int[n + 1];  // 결과배열 초기화
+
+            answer[0] = 1;  // 1부터 시작
+            for(int i = 0; i < money.length; i++) { // 길이만큼
+                for(int j = money[i]; j <= n; j++) {    // n 만큼
+                    answer[j] += answer[j - money[i]];  // 거스름돈 더하기
                     answer[j] %= 1000000007;
                 }
             }
 
-            return answer[n];
+            return answer[n];   // 총 결과값 리턴
         }
     }
 }
