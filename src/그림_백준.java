5f9026330001655c3fbc0478743652d1b06015cb
leejk298 <5552269@naver.com>
2023-07-14 14:13:32 +0900

    그림_백준

--- src/그림_백준.java
+++ src/그림_백준.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.*;
+
 /*
 6 5
 1 1 0 1 1
@@ -8,6 +10,7 @@ import java.util.*;
 0 0 1 1 1
 0 0 1 1 1
  */
+
 public class 그림_백준 {
     static int N, M, count; // 크기, 넓이
     static int[][] map; // 맵
@@ -16,27 +19,34 @@ public class 그림_백준 {
     static int[] dy = {0, 0, -1, 1};
     static List<Integer> answer;    // 결과리스트
 
-    public static void init() { // 초기화
-        Scanner sc = new Scanner(System.in);    // 입력
+    public static void init() throws IOException { // 초기화
 
-        N = sc.nextInt();   // 행
-        M = sc.nextInt();   // 열
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+        N = Integer.parseInt(st.nextToken());    // 행
+        M = Integer.parseInt(st.nextToken());    // 열
 
         // 초기화
         answer = new ArrayList<>();
         map = new int[N][M];
         visited = new boolean[N][M];
 
-        for(int i = 0; i < N; i++)  // 행
-            for(int j = 0; j < M; j++)  // 열
-                map[i][j] = sc.nextInt();   // 저장
+        for(int i = 0; i < N; i++) { // 행
+            st = new StringTokenizer(bf.readLine());
+
+            for (int j = 0; j < M; j++) // 열
+                map[i][j] = Integer.parseInt(st.nextToken());   // 저장
+        }
     }
 
     public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
+
         return (x < 0 || x >= N || y < 0 || y >= M);
     }
     
     public static void BFS(int x, int y) {  // BFS
+
         Queue<int[]> queue = new LinkedList<>();    // 큐
 
         queue.offer(new int[] {x, y});  // 시작점 삽입
@@ -59,23 +69,23 @@ public class 그림_백준 {
                     queue.offer(new int[] {tmpX, tmpY});    // 큐에 삽입
                 }
             }
-
         }
     }
 
-    public static void main(String[] args) {
+    public static void findPrintings() {    // 그림 찾기
 
-        init(); // 초기화
-
-        for(int i = 0; i < N; i++) {    // 행
-            for(int j = 0; j < M; j++) {    // 열
-                if(map[i][j] == 1 && !visited[i][j]) {  // 갈 수 있으면
+        for (int i = 0; i < N; i++) {    // 행
+            for (int j = 0; j < M; j++) {    // 열
+                if (map[i][j] == 1 && !visited[i][j]) {  // 갈 수 있으면
                     BFS(i, j);  // BFS
                     answer.add(count);  // 결과리스트에 추가
                     count = 0;  // 초기화
                 }
             }
         }
+    }
+
+    public static void printPrintings() {   // 결과 출력
 
         System.out.println(answer.size());  // 총 개수 출력
 
@@ -85,5 +95,14 @@ public class 그림_백준 {
             System.out.println(0);  // 넓이 0
         else    // 있으면
             System.out.println(answer.get(answer.size() - 1));  // 최대 넓이 출력
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init(); // 초기화
+
+        findPrintings();    // 그림 찾기
+
+        printPrintings();   // 그림 개수, 넓이 출력
     }
 }
