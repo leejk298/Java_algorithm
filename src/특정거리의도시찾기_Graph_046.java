3269c1c175b10478da41b715cf3004b24d9b3774
leejk298 <5552269@naver.com>
2023-07-14 22:27:14 +0900

    특정거리의도시찾기_Graph_046

--- src/특정거리의도시찾기_Graph_046.java
+++ src/특정거리의도시찾기_Graph_046.java
@@ -1,17 +1,23 @@
 import java.util.*;
 
 public class 특정거리의도시찾기_Graph_046 {
-    public static void main(String[] args) {
+    static int N, M, K, X;
+    static int[] visited;
+    static ArrayList<Integer>[] A;
+    static List<Integer> res;
+
+    public static void init() {
+
         Scanner sc = new Scanner(System.in); // 입력
 
-        int N = sc.nextInt(); // 노드 개수
-        int M = sc.nextInt(); // 엣지 개수
-        int K = sc.nextInt(); // 최단 거리
-        int X = sc.nextInt(); // 시작 노드
+        N = sc.nextInt(); // 노드 개수
+        M = sc.nextInt(); // 엣지 개수
+        K = sc.nextInt(); // 최단 거리
+        X = sc.nextInt(); // 시작 노드
 
-        int visited[] = new int[N + 1]; // 방문 배열, 크기: N + 1 => 1번 노드부터 시작
-        ArrayList<Integer>[] A = new ArrayList[N + 1]; // 인접리스트
-        List<Integer> res = new ArrayList<>(); // 결과리스트
+        visited = new int[N + 1]; // 방문 배열, 크기: N + 1 => 1번 노드부터 시작
+        A = new ArrayList[N + 1]; // 인접리스트
+        res = new ArrayList<>(); // 결과리스트
 
         for (int i = 1; i <= N; i++) { // 노드 개수만큼
             A[i] = new ArrayList<Integer>(); // 인접리스트 생성
@@ -25,8 +31,29 @@ public class 특정거리의도시찾기_Graph_046 {
 
             A[S].add(E); // 방향성 그래프
         }
+    }
 
-        BFS(X, A, visited); // BFS 수행 - 최단경로 탐색
+    public static void BFS(int v, ArrayList<Integer>[] A, int[] visited) { // BFS
+
+        Queue<Integer> queue = new LinkedList<>(); // 큐로 구현
+
+        queue.add(v); // 우선 방문 노드 큐에 삽입
+        visited[v]++; // 방문 배열값도 설정 => 가중치가 전부 1이므로 ++; (가중치 없는 그래프)
+
+        while (!queue.isEmpty()) { // 비어있지 않으면
+            int now_node = queue.poll(); // 큐에서 하나 꺼내어
+
+            for (int i : A[now_node]) { // 해당 노드를 순회하여
+                if (visited[i] == -1) { // 방문하지 않은 노드가 있으면
+                    visited[i] = visited[now_node] + 1; // 해당 노드의 방문 배열을 갱신 => 현재 노드 + 1 => 최단경로
+
+                    queue.add(i); // 큐에 삽입
+                }
+            }
+        }
+    }
+
+    public static void printShortestCity() {
 
         for (int i = 1; i <= N; i++) // 노드 개수만큼
             if (visited[i] == K) // 방문 배열의 값이 찾는 최단경로이면
@@ -43,23 +70,12 @@ public class 특정거리의도시찾기_Graph_046 {
         }
     }
 
-    private static void BFS(int v, ArrayList<Integer>[] A, int[] visited) { // BFS
+    public static void main(String[] args) {
 
-        Queue<Integer> queue = new LinkedList<>(); // 자료구조 - Queue로 구현
+        init();
 
-        queue.add(v); // 우선 방문 노드 큐에 삽입
-        visited[v]++; // 방문 배열값도 설정 => 가중치가 전부 1이므로 ++; (가중치 없는 그래프)
+        BFS(X, A, visited); // BFS 수행 - 최단경로 탐색
 
-        while (!queue.isEmpty()) { // 비어있지 않으면
-            int now_node = queue.poll(); // 큐에서 하나 꺼내어
-
-            for (int i : A[now_node]) { // 해당 노드를 순회하여
-                if (visited[i] == -1) { // 방문하지 않은 노드가 있으면
-                    visited[i] = visited[now_node] + 1; // 해당 노드의 방문 배열을 갱신 => 현재 노드 + 1 => 최단경로
-
-                    queue.add(i); // 큐에 삽입
-                }
-            }
-        }
+        printShortestCity();
     }
 }
