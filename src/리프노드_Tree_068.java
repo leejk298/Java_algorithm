d57a3e44b934c54a9fc1ecb2837b04f896960742
leejk298 <5552269@naver.com>
2023-07-30 19:09:16 +0900

    리프노드_Tree_068

--- src/리프노드_Tree_068.java
+++ src/리프노드_Tree_068.java
@@ -3,18 +3,20 @@ import java.util.*;
 public class 리프노드_Tree_068 {
     static ArrayList<Integer>[] tree; // 트리
     static boolean visited[]; // 방문배열
-    static int cnt = 0; // 리프노드 개수
+    static int N, count, root; // 리프노드 개수
     static int deleteNode = 0; // 삭제할 노드
 
-    public static void main(String[] args) {
+    public static void init() { // 초기화
+
         Scanner sc = new Scanner(System.in); // 입력
 
-        int N = sc.nextInt(); // 노드 개수
+        N = sc.nextInt(); // 노드 개수
+        count = 0;
 
         tree = new ArrayList[N]; // 초기화, 0번부터 시작 => 크기: N
         visited = new boolean[N];
 
-        int root = 0; // 루트노드
+        root = 0; // 루트노드
         for (int i = 0; i < N; i++) // 노드 개수만큼
             tree[i] = new ArrayList<>(); // 트리 구현
 
@@ -29,19 +31,13 @@ public class 리프노드_Tree_068 {
         }
 
         deleteNode = sc.nextInt(); // 삭제할 노드
-        if (deleteNode == root) // 삭제할 노드가 루트노드이면
-            System.out.println(0); // 0 출력 => 리프노드가 없으므로
-
-        else { // 루트노드가 아니면
-            DFS(root); // 루트노드로 DFS 수행
-            System.out.println(cnt); // 리프노드 개수 출력
-        }
     }
 
-    private static void DFS(int v) { // DFS
-        visited[v] = true; // 방문배열 갱신
-        int cNode = 0; // 자식노드 개수
+    public static void DFS(int v) { // DFS
 
+        visited[v] = true; // 방문배열 갱신
+
+        int cNode = 0; // 자식노드 개수
         for (int i : tree[v]) { // 해당노드의 인접노드(=자식노드) 탐색
             if (!visited[i] && i != deleteNode) { // 방문하지않았고 삭제할 노드가 아니면
                 cNode++; // 해당노드의 자식노드 개수 카운트
@@ -50,6 +46,24 @@ public class 리프노드_Tree_068 {
         }
 
         if (cNode == 0) // 해당노드의 자식노드가 없으면 => 리프노드
-            cnt++; // 리프노드 개수 카운트
+            count++; // 리프노드 개수 카운트
+    }
+
+    public static void printCount() {   // 리프노드 개수 출력
+
+        if (deleteNode == root) // 삭제할 노드가 루트노드이면
+            System.out.println(0); // 0 출력 => 리프노드가 없으므로
+
+        else { // 루트노드가 아니면
+            DFS(root); // 루트노드로 DFS 수행
+            System.out.println(count); // 리프노드 개수 출력
+        }
+    }
+
+    public static void main(String[] args) {
+
+        init(); // 초기화
+
+        printCount();   // 리프노드 개수 출력
     }
 }
