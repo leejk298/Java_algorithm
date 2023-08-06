19e4964fb68e6661e3d4e0c8b43a036c221b1247
leejk298 <5552269@naver.com>
2023-08-06 18:37:29 +0900

    현수막_백준

--- src/현수막_백준.java
+++ src/현수막_백준.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /*
 8 19
@@ -14,25 +15,30 @@ import java.util.*;
 
 public class 현수막_백준 {
     static int N, M;    // 크기
-    static int[][] map; // 맵
+    static int[][] map; // 입력배열
     static boolean[][] visited; // 방문배열
     static int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};  // 8방향
     static int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
 
-    public static void init() { // 초기화
-        Scanner sc = new Scanner(System.in);    // 입력
+    public static void init() throws IOException { // 초기화
 
-        N = sc.nextInt();   // 행
-        M = sc.nextInt();   // 열
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+        N = Integer.parseInt(st.nextToken());   // 행
+        M = Integer.parseInt(st.nextToken());   // 열
 
         // 초기화
         map =  new int[N][M];
         visited = new boolean[N][M];
 
         // 맵 저장
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < M; j++)
-                map[i][j] = sc.nextInt();
+        for(int i = 0; i < N; i++) {    // 행
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+            for (int j = 0; j < M; j++) // 열
+                map[i][j] = Integer.parseInt(st.nextToken());   // 입력배열 저장
+        }
     }
 
     public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
@@ -40,6 +46,7 @@ public class 현수막_백준 {
     }
 
     public static void BFS(int x, int y) {  // BFS
+
         Queue<int[]> queue = new LinkedList<>();    // 큐
 
         queue.offer(new int[] {x, y});  // 시작점 삽입
@@ -64,6 +71,7 @@ public class 현수막_백준 {
     }
 
     public static int findLetterCount() {   // 글자수 세기
+
         int count = 0;  // 개수
 
         for(int i = 0; i < N; i++) {    // 행
@@ -78,7 +86,7 @@ public class 현수막_백준 {
         return count;   // 개수 리턴
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
