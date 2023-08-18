41bb968194814c1a1d9d3227e5f1bdf81908d1c7
leejk298 <5552269@naver.com>
2023-08-18 13:23:03 +0900

    가장큰정사각형_DP_091

--- src/가장큰정사각형_DP_091.java
+++ src/가장큰정사각형_DP_091.java
@@ -3,25 +3,26 @@ import java.io.*;
 
 public class 가장큰정사각형_DP_091 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
         int N = Integer.parseInt(st.nextToken()); // 행
         int M = Integer.parseInt(st.nextToken()); // 열
-        int D[][] = new int[N][M]; // 점화식 배열
         long max = 0; // 한 변의 가장 큰 길이
 
+        int D[][] = new int[N][M]; // 점화식 배열
         for (int i = 0; i < N; i++) { // 행 개수만큼
             st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
+
             for (int j = 0; j < M; j++) { // 열 개수만큼
                 D[i][j] = Integer.parseInt(st.nextToken()); // 배열 저장
-                if (D[i][j] == 1 && i > 0 && j > 0) { // 배열값이 1 이고 2번째 행, 열부터
+
+                if (D[i][j] == 1 && i > 0 && j > 0) // 배열값이 1 이고 2번째 행, 열부터
                     // D[i][j]가 오른쪽 아래 꼭짓점이므로 대각선, 위, 왼쪽 중 가장 작은 값 + 1 로 세팅
                     D[i][j] = Math.min(D[i - 1][j - 1], Math.min(D[i - 1][j], D[i][j - 1])) + 1; // + 1 은 자기자신
-                }
 
-                if (max < D[i][j]) // 가장 큰 변의 길이
-                    max = D[i][j];
+                max = Math.max(max, D[i][j]); // 가장 큰 변의 길이
             }
         }
 
