661f2d21357fc83ce4b15c0f8d58f4356c3439f1
leejk298 <5552269@naver.com>
2023-08-23 16:21:08 +0900

    블랙잭_백준

--- src/블랙잭_백준.java
+++ src/블랙잭_백준.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*
@@ -9,22 +10,25 @@ public class 블랙잭_백준 {
     static int N, M;    // 크기
     static int[] arr;   // 입력배열
 
-    public static void init() { // 초기화
+    public static void init() throws IOException { // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        N = sc.nextInt();   // 크기
-        M = sc.nextInt();   // 경계값
+        N = Integer.parseInt(st.nextToken());   // 크기
+        M = Integer.parseInt(st.nextToken());  // 경계값
 
-        // 초기화
-        arr = new int[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+        arr = new int[N];   // 초기화
+        for (int i = 0; i < N; i++) // 크기만큼
+            arr[i] = Integer.parseInt(st.nextToken());  // 입력배열 저장
     }
 
     public static void printCloseCardSum() {    // M에 가장 가까운 숫자 출력
 
         int sum = 0, res = 0;   // 합, 결과값
+
         for (int i = 0; i < N - 2; i++) {   // 첫 번째 카드
             for (int j = i + 1; j < N - 1; j++) {   // 두 번째 카드
                 for (int k = j + 1; k < N; k++) {   // 세 번째 카드
@@ -39,7 +43,7 @@ public class 블랙잭_백준 {
         System.out.println(res);    // 출력
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
