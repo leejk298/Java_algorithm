0a375266bc9b295eb5dc2a3bf8cec22d35ee765a
leejk298 <5552269@naver.com>
2023-08-28 14:53:51 +0900

    알고스팟_백준

--- src/알고스팟_백준.java
+++ src/알고스팟_백준.java
@@ -12,23 +12,24 @@ import java.io.*;
  */
 
 public class 알고스팟_백준 {
-    static int N, M, count; // 크기
-    static int[][] map, D;  // 맵, 거리배열
+    static int N, M;    // 크기
+    static char[][] map;    // 입력배열
+    static boolean[][] visited; // 방문배열
     static int[] dx = {-1, 1, 0, 0};    // 4방향
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dy = {0, 0, -1 ,1};
 
-    static class Node implements Comparable<Node> { // 내부 클래스
-        int x, y, c;
+    static class Edge implements Comparable<Edge> { // 엣지 클래스
+        int s, e, w;    // 시작, 도착, 가중치
 
-        public Node(int x, int y, int c) {
-            this.x = x;
-            this.y = y;
-            this.c = c;
+        public Edge(int s, int e, int w) {  // 파라미터 생성자
+            this.s = s;
+            this.e = e;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Node v) {  // 오버라이딩
-            return this.c - v.c;    // 오름차순
+        public int compareTo(Edge e) {  // 메소드 오버라이딩
+            return this.w - e.w;    // 오름차순 정렬
         }
     }
 
@@ -39,42 +40,33 @@ public class 알고스팟_백준 {
 
         M = Integer.parseInt(st.nextToken());   // 열
         N = Integer.parseInt(st.nextToken());   // 행
-        count = 0;  // 벽 부순 개수
 
         // 초기화
-        map = new int[N][M];
-        D = new int[N][M];
+        map = new char[N][M];
+        visited = new boolean[N][M];
 
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < M; j++)
-                D[i][j] = Integer.MAX_VALUE;    // 거리배열 무한대로 초기화
-
-        // 맵 저장
-        for(int i = 0; i < N; i++) {
-            char[] ch = bf.readLine().toCharArray();
-
-            for(int j = 0; j < M; j++)
-                map[i][j] = ch[j] - '0';
-        }
+        for (int i = 0; i < N; i++) // 행
+            map[i] = bf.readLine().toCharArray();    // 입력배열 저장
     }
 
-    public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
+    public static boolean isNotValidPos(int x, int y) {     // 좌표가 유효한지
         return (x < 0 || x >= N || y < 0 || y >= M);
     }
 
-    public static void Dijkstra() { // 다익스트라
+    public static void Dijkstra(int x, int y) { // 다익스트라
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(); // 우선순위 큐
+        PriorityQueue<Edge> pq = new PriorityQueue<>(); // 우선순위 큐
 
-        pq.offer(new Node(0, 0, 0));    // 시작점 삽입
-        D[0][0] = 0;    // 시작점 거리 0
+        pq.offer(new Edge(x, y, 0));    // 시작점 우선순위 큐에 삽입, 개수 0부터
+        visited[x][y] = true;   // 시작점부터 방문
 
-        while(!pq.isEmpty()) {  // 우선순위 큐가 비어있지 않으면
-            Node now = pq.poll();   // 하나 꺼내어
+        while (!pq.isEmpty()) { // 우선순위 큐가 비어있지 않으면
+            Edge now = pq.poll();   // 하나 꺼내어 => 벽을 가장 적게 부순 객체
 
-            int nowX = now.x, nowY = now.y; // 좌표
-            if(nowX == N - 1 && nowY == M - 1) {    // 도착하면
-                count = now.c;  // 벽 부순 횟수 저장
+            int nowX = now.s, nowY = now.e, nowD = now.w;   // 현재 노드
+
+            if(nowX == N - 1 && nowY == M - 1) {    // 도착점에 도달하면
+                System.out.println(nowD);   // 최소비용 출력
 
                 return; // 함수 종료
             }
@@ -82,13 +74,15 @@ public class 알고스팟_백준 {
             for(int i = 0; i < 4; i++) {    // 4방향
                 int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
 
-                if(isNotValidPos(tmpX, tmpY))   // 유효한지
+                if(isNotValidPos(tmpX, tmpY) || visited[tmpX][tmpY])    // 유효한지
                     continue;
 
-                if(D[tmpX][tmpY] > D[nowX][nowY] + map[tmpX][tmpY]) {   // 최소 벽 부순 비용이면
-                    D[tmpX][tmpY] = D[nowX][nowY] + map[tmpX][tmpY];    // 갱신
-                    pq.offer(new Node(tmpX, tmpY, D[tmpX][tmpY]));      // 우선순위 큐에 삽입
-                }
+                visited[tmpX][tmpY] = true; // 방문
+
+                if(map[tmpX][tmpY] == '0')  // 벽이 아니면
+                    pq.offer(new Edge(tmpX, tmpY, nowD));   // 개수 그대로
+                else    // 벽이면
+                    pq.offer(new Edge(tmpX, tmpY, nowD + 1));   // 개수 + 1
             }
         }
     }
@@ -97,8 +91,6 @@ public class 알고스팟_백준 {
 
         init(); // 초기화
 
-        Dijkstra(); // 다익스트라
-
-        System.out.println(count);  // 벽 부순 횟수 출력
+        Dijkstra(0, 0); // 다익스트라
     }
 }
