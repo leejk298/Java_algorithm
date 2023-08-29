a192d58298ade9fb6e2368cf94936655f68a7fbc
leejk298 <5552269@naver.com>
2023-08-29 19:04:06 +0900

    최소스패닝트리_백준

--- src/최소스패닝트리_백준.java
+++ src/최소스패닝트리_백준.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /*
 3 3
@@ -9,86 +10,88 @@ import java.util.*;
 
 public class 최소스패닝트리_백준 {
     static int N, M;    // 크기
-    static int[] parent;    // 부모배열, 대표노드
-    static PriorityQueue<Edge> pq;  // 우선순위 큐 => 엣지리스트
+    static int[] parent;    // 부모배열
+    static PriorityQueue<Edge> pq;  // 우선순위 큐, 엣지클래스 => 그래프 표현
 
-    static class Edge implements Comparable<Edge> { // 엣지 클래스 => Comparable 인터페이스 구현 => compareTo() 메소드 재정의
-        int S, E, W;    // 멤버변수
+    static class Edge implements Comparable<Edge> { // 엣지클래스, 우선순위 큐 => Comparable 클래스 구현 => compareTo() 메소드 재정의
+        int s, e, w;
 
-        public Edge(int S, int E, int W) {  // 생성자
-            this.S = S;
-            this.E = E;
-            this.W = W;
+        public Edge(int s, int e, int w) {  // 파라미터 생성자
+            this.s = s;
+            this.e = e;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Edge e) {  // 오버라이딩
-            return this.W - e.W;    // 오름차순 정렬
+        public int compareTo(Edge e) {  // 메소드 오버라이딩
+            return this.w - e.w;    // 가중치 오름차순 정렬
         }
     }
 
-    public static void init() { // 초기화
+    public static void init() throws IOException {  // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        N = sc.nextInt();   // 노드
-        M = sc.nextInt();   // 엣지
+        N = Integer.parseInt(st.nextToken());   // 정점 개수
+        M = Integer.parseInt(st.nextToken());   // 엣지 개수
 
         // 초기화
-        pq = new PriorityQueue<>();
         parent = new int[N + 1];
+        pq = new PriorityQueue<>();
 
-        for(int i = 1; i <= N; i++) // 노드 개수만큼
-            parent[i] = i;  // 대표노드 초기화
+        for(int i = 1; i <= N; i++) // 정점 개수만큼
+            parent[i] = i;  // 부모배열 저장
 
         for(int i = 0; i < M; i++) {    // 엣지 개수만큼
-            int s = sc.nextInt();   // 시작
-            int e = sc.nextInt();   // 끝
-            int w = sc.nextInt();   // 가중치
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            pq.offer(new Edge(s, e, w));    // 우선순위 큐 삽입
+            int S = Integer.parseInt(st.nextToken());   // 시작
+            int E = Integer.parseInt(st.nextToken());   // 도착
+            int W = Integer.parseInt(st.nextToken());   // 가중치
+
+            pq.offer(new Edge(S, E, W));    // 우선순위 큐에 삽입, 가중치 오름차순 정렬(최소힙)
         }
+    }
+
+    public static int find(int a) { // find()
+
+        if(a == parent[a])  // 부모와 동일하면
+            return a;   // 그대로 리턴
+
+        // 다르면 부모배열 갱신 후 리턴
+        return parent[a] = find(parent[a]);
     }
 
     public static void union(int a, int b) {    // 합집합
 
-        a = find(a);    // 대표노드 찾기
-        b = find(b);
+        a = find(a);    // a의 부모
+        b = find(b);    // b의 부모
 
-        if(a < b)   // 작은 값으로 저장
-            parent[b] = a;
-        else
-            parent[a] = b;
+        if(a != b)  // 다르면
+            parent[b] = a;  // 합집합
     }
 
-    public static int find(int a) { // find
+    public static void Kruskal() {  // 크루스칼, MST 알고리즘
 
-        if(a == parent[a])  // 같으면 그대로
-            return a;
-
-        return parent[a] = find(parent[a]); // 다르면 부모노드로 바꾸고 리턴
-    }
-
-    public static void printMinWeight() {   // 최소가중치값 출력
-
-        int min = 0;    // 최소값
+        int res = 0;    // 최소값
 
         while(!pq.isEmpty()) {  // 우선순위 큐가 비어있지 않으면
-            Edge now = pq.poll();   // 하나 꺼내어
+            Edge now = pq.poll();   // 현재 노드
 
-            if(find(now.S) != find(now.E)) {    // 다르면
-                union(now.S, now.E);    // 합집합
-                min += now.W;   // 최소값 갱신
+            if(find(now.s) != find(now.e)) { // 시작과 도착이 다르면
+                union(now.s, now.e);    // 합집합
+                res += now.w;   // 가중치 합, 최소값 저장
             }
         }
 
-        System.out.println(min);    // 출력
+        System.out.println(res);    // 최소비용 출력
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
-        printMinWeight();   // 최소비용 출력
+        Kruskal();  // 크루스칼
     }
 }
