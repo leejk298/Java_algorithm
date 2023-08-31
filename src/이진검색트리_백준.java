9845c176dea3e2e9c39290b897b59dce7b82a0e3
leejk298 <5552269@naver.com>
2023-08-31 13:31:05 +0900

    이진검색트리_백준

--- src/이진검색트리_백준.java
+++ src/이진검색트리_백준.java
@@ -1,65 +1,80 @@
 import java.util.*;
 import java.io.*;
 
+/*
+50
+30
+24
+5
+28
+45
+98
+52
+60
+ */
+
 public class 이진검색트리_백준 {
-    static Node root;   // 루트노드
+    static Node root;   // 루트 노드
 
     static class Node { // 노드 클래스
-        int num;    // 데이터
-        Node l, r;  // 왼, 오른쪽 자식 노드
+        // 멤버 변수
+        int v;  // 데이터
+        Node l, r;  // 자식 노드
 
-        public Node(int num) {  // 생성자
-            this.num = num;
+        // 멤버 함수
+        public Node(int v) {    // 파라미터 생성자
+            this.v = v;
         }
 
-        public void insert(int num) {   // 멤버 함수
+        public void insert(int v) { // insert()
 
-            if(num < this.num) {    // 작으면, 왼쪽 자식 노드 이동
-                if(this.l == null)  // 해당 노드가 비어있으면
-                    this.l = new Node(num); // 삽입
-                else    // 이미 존재하면
-                    this.l.insert(num); // 재귀콜
-
-            } else {    // 크면, 오른쪽 자식 노드
-                if(this.r == null)
-                    this.r = new Node(num);
-                else
-                    this.r.insert(num);
+            if(v < this.v) {    // 자신보다 작으면 왼쪽
+                if(this.l == null)  // 왼쪽 자식 노드가 없으면
+                    this.l = new Node(v);   // 생성
+                else    // 있으면
+                    this.l.insert(v);   // 해당 노드로 재귀콜
+            } else {    // 자신보다 크면 오른쪽
+                if(this.r == null)  // 오른쪽 자식 노드가 없으면
+                    this.r = new Node(v);   // 생성
+                else    // 있으면
+                    this.r.insert(v);   // 해당 노드로 재귀콜
             }
         }
+    }
+
+    public static void postOrder(Node node) {   // 후위순회
+
+        // 베이스 케이스
+        if(node == null)    // 리프노드면
+            return; // 리턴
+
+        // 재귀케이스
+        postOrder(node.l);  // 왼
+        postOrder(node.r);  // 오
+        System.out.println(node.v); // 자
     }
 
     public static void init() throws IOException {  // 초기화
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
 
-        root = new Node(Integer.parseInt(bf.readLine()));   // 루트노드 생성
+        root = new Node(Integer.parseInt(bf.readLine()));   // 루트 노드 생성
 
+        // 트리 생성
         while(true) {
             String str = bf.readLine(); // 한 줄 스트링
 
-            if(str == null || str.equals(""))   // 문자열이 null 이거나 null 문자인 경우
-                break;
+            if(str == null) // 없으면
+                break;  // while 종료
 
-            root.insert(Integer.parseInt(str)); // 아니면 삽입
+            root.insert(Integer.parseInt(str)); // 있으면 삽입
         }
-    }
-
-    public static void postOrder(Node node) {   // 후위순회
-
-        if(node == null)    // 베이스케이스
-            return;
-
-        // 재귀케이스
-        postOrder(node.l);  // 왼
-        postOrder(node.r);  // 오
-        System.out.println(node.num);   // 자
     }
 
     public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
-        postOrder(root);    // 루트노드로 후위순회 출력
+        postOrder(root);    // 출력, 후위순회
     }
 }
