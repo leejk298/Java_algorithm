1fe897cae3c6b3a924fbefdb171e2023b03476f0
leejk298 <5552269@naver.com>
2023-09-03 17:09:28 +0900

    욕심쟁이판다_백준

--- src/욕심쟁이판다_백준.java
+++ src/욕심쟁이판다_백준.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /*
 4
@@ -15,19 +16,23 @@ public class 욕심쟁이판다_백준 {
     static int[] dx = {-1, 1, 0, 0};    // 4방향
     static int[] dy = {0, 0, -1, 1};
 
-    public static void init() { // 초기화
+    public static void init() throws IOException { // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        N = sc.nextInt();   // 크기
+        N = Integer.parseInt(st.nextToken());   // 크기
 
         // 초기화
         map = new int[N][N];
         dp = new int[N][N];
 
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < N; j++)
-                map[i][j] = sc.nextInt();   // 맵 저장
+        for (int i = 0; i < N; i++) {   // 행
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+            for (int j = 0; j < N; j++) // 열
+                map[i][j] = Integer.parseInt(st.nextToken());   // 입력배열 저장
+        }
     }
 
     public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
@@ -36,18 +41,18 @@ public class 욕심쟁이판다_백준 {
 
     public static int DFS(int x, int y) {   // DFS
 
-        if(dp[x][y] != 0)   // 메모이제이션, 이미 정해진 경우 그대로
+        if (dp[x][y] != 0)   // 메모이제이션, 이미 정해진 경우 그대로
             return dp[x][y];
 
         dp[x][y] = 1;   // 0이면 1로, 자기 자신
 
-        for(int i = 0; i < 4; i++) {    // 4방향
+        for (int i = 0; i < 4; i++) {    // 4방향
             int tmpX = x + dx[i], tmpY = y + dy[i]; // 다음 좌표
 
-            if(isNotValidPos(tmpX, tmpY))   // 유효한지
+            if (isNotValidPos(tmpX, tmpY))   // 유효한지
                 continue;
 
-            if(map[x][y] < map[tmpX][tmpY]) // 갈 수 있는 경로이면
+            if (map[x][y] < map[tmpX][tmpY]) // 갈 수 있는 경로이면
                 dp[x][y] = Math.max(dp[x][y], DFS(tmpX, tmpY) + 1); // DP 배열, 다음 좌표 + 1 중 최대값
         }
 
@@ -57,14 +62,14 @@ public class 욕심쟁이판다_백준 {
     public static int printMaxShift() { // 최대값 출력
 
         int max = 0;    // 최대값
-        for(int i = 0; i < N; i++)  // 행
-            for(int j = 0; j < N; j++)  // 열
+        for (int i = 0; i < N; i++)  // 행
+            for (int j = 0; j < N; j++)  // 열
                 max = Math.max(max, DFS(i, j)); // 최대값, DFS
 
         return max; // 리턴
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
