a40592e3f98900453c334dea49bb4c5955e93782
leejk298 <5552269@naver.com>
2023-09-04 20:07:49 +0900

    암호만들기_백준

--- src/암호만들기_백준.java
+++ src/암호만들기_백준.java
@@ -22,43 +22,43 @@ public class 암호만들기_백준 {
         ch = new char[M];
         res = new char[N];
 
-        String[] str = bf.readLine().split(" ");    // 공백 기준으로 문자열 나눔
-        for(int i = 0; i < M; i++)  // 크기만큼
-            ch[i] = str[i].charAt(0);   // 문자로 형변환하여 저장
+        String[] str = bf.readLine().split(" ");    // 공백 기준으로 문자열 나누기
+        for(int i = 0; i < M; i++)  // 입력배열 크기만큼
+            ch[i] = str[i].charAt(0);   // 문자열 -> 문자 형변환
 
-        Arrays.sort(ch);    // 사전순 -> 오름차순 정렬
+        Arrays.sort(ch);    // 사전순 정렬
     }
 
     public static boolean isValidPassword(char[] res) { // 암호가 가능한지
 
         int mo = 0, ja = 0; // 모음, 자음 개수
 
-        for(char c : res) { // 결과배열 순회
+        for(char c : res) { // 결과배열 순회하여
             if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')    // 모음
                 mo++;
             else    // 자음
                 ja++;
         }
 
-        if(mo >= 1 && ja >= 2)  // 모음 1개 이상 자음 2개 이상이면
-            return true;    // true 리턴
+        if(mo >= 1 && ja >= 2)  // 모음 1개 이상이면서 자음 2개 이상이면
+            return true;    // true
 
-        return false;   // 아니면 false 리턴
+        return false;   // 아니면 false
     }
 
     public static void DFS(int depth, int index) {  // DFS, 브루트포스
 
-        if(depth == N) {    // 길이 도달하면
+        if(depth == N) {    // 길이만큼 도달하면
             if(isValidPassword(res))    // 암호가 가능하면
                 System.out.println(res);    // 출력
 
-            return; // 완전 탐색위해 함수 리턴
+            return; // 완전 탐색하기 위해 함수 리턴
         }
 
-        // 도달하지 않으면
-        for(int i = index; i < M; i++) {    // 입력 크기만큼
+        // 도달하지 않았으면
+        for(int i = index; i < M; i++) {    // 입력배열 크기만큼
             res[depth] = ch[i]; // 결과배열 저장
-            DFS(depth + 1, i + 1);  // 재귀콜: 문자열길이 + 1, 중복되지않게 인덱스 + 1
+            DFS(depth + 1, i + 1);  // 재귀콜, 문자열 길이 + 1, 인덱스 + 1 (중복 x)
         }
     }
 
