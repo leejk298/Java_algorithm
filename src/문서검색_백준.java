27a063d4e5d4b4ac405f0b89da89569926ae492f
leejk298 <5552269@naver.com>
2023-09-04 20:15:23 +0900

    문서검색_백준

--- src/문서검색_백준.java
+++ src/문서검색_백준.java
@@ -7,7 +7,6 @@ aba
  */
 
 public class 문서검색_백준 {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
@@ -15,18 +14,17 @@ public class 문서검색_백준 {
         String str = bf.readLine(); // 입력 문자열
         String target = bf.readLine();  // 찾을 문자열
 
-        int lenS = str.length();    // 입력 문자열 길이
-        int lenT = target.length(); // 찾을 문자열 길이
+        int lenS = str.length(), lenT = target.length();    // 문자열 길이
 
         int count = 0;  // 개수
-        for(int i = 0; i <= lenS - lenT; i++) { // 탐색 가능한 길이만큼
-            if(str.substring(i, i + lenT).equals(target)) { // 같으면
+        for(int i = 0; i <= lenS - lenT; i++) { // 찾을 범위
+            if(str.substring(i, i + lenT).equals(target)) { // 찾을 문자열 길이만큼 비교하여 같으면
                 count++;    // 개수 카운트
 
-                i += lenT - 1;  // -1 -> i++ -> lenT만큼 증가
+                i += lenT - 1;  // 다음 인덱스
             }
         }
 
-        System.out.println(count);  // 총 개수 출력
+        System.out.println(count);  // 개수 출력
     }
 }
