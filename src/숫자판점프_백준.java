dce4499c53b80c79bc279d41b4aef8b62ef256e1
leejk298 <5552269@naver.com>
2023-09-06 19:54:29 +0900

    숫자판점프_백준

--- src/숫자판점프_백준.java
+++ src/숫자판점프_백준.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*
@@ -9,32 +10,32 @@ import java.util.*;
  */
 
 public class 숫자판점프_백준 {
-    static String[][] map;  // 문자열 맵
+    static String[][] map;  // 입력배열
     static HashSet<String> hashSet; // 해시셋, 중복제거
     static int[] dx = {-1, 1, 0, 0};    // 4방향
     static int[] dy = {0, 0, -1, 1};
 
-    static class Node { // 내부클래스
+    static class Node { // 내부 클래스
         int x, y;   // 좌표
-        String s;   // data
+        String s;   // 문자열
 
-        public Node (int x, int y, String s) {  // 생성자
+        public Node(int x, int y, String s) {   // 파라미터 생성자
             this.x = x;
             this.y = y;
             this.s = s;
         }
     }
 
-    public static void init() { // 초기화
+    public static void init() throws IOException {  // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
 
         // 초기화
         map = new String[5][5];
         hashSet = new HashSet<>();
 
-        for(int i = 0; i < 5; i++)
-            map[i] = sc.nextLine().split(" ");  // 한 줄 스트링을 공백 기준으로 나눠 문자열로 저장
+        for(int i = 0; i < 5; i++)  // 행
+            map[i] = bf.readLine().split(" ");  // 공백 기준으로 문자열 나눠서 입력배열 저장
     }
 
     public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
@@ -44,38 +45,42 @@ public class 숫자판점프_백준 {
     public static void BFS(Node v) {    // BFS
 
         Queue<Node> queue = new LinkedList<>(); // 큐
-        queue.offer(v); // 시작점 삽입
+        queue.offer(v); // 큐에 시작 노드 삽입
 
         while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
-            Node now = queue.poll();
+            Node now = queue.poll();    // 하나 꺼내어
 
-            if(now.s.length() == 6) {   // 6자리수가 되면
-                hashSet.add(now.s); // 해시셋에 저장, 중복 허용 X
+            if(now.s.length() == 6) {   // 현재 노드의 문자열 길이가 6이면
+                hashSet.add(now.s); // 해시셋에 저장
 
-                continue;   // 다음 반복문으로 -> while()
+                continue;   // 다음 while()
             }
 
+            // 문자열 길이가 6이 안되면
+            int nowX = now.x, nowY = now.y; // 현재 좌표
+
             for(int i = 0; i < 4; i++) {    // 4방향
-                int tmpX = now.x + dx[i], tmpY = now.y + dy[i]; // 다음 좌표
+                int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
 
                 if(isNotValidPos(tmpX, tmpY))   // 유효한지
                     continue;
 
-                queue.offer(new Node(tmpX, tmpY, now.s + map[tmpX][tmpY])); // 큐에 삽입, 문자열 합하여
+                // 유효하면
+                queue.offer(new Node(tmpX, tmpY, now.s + map[tmpX][tmpY])); // 큐에 삽입
             }
         }
     }
 
-    public static void printNumCount() {    // 6자리수 개수
+    public static void printNumCount() {    // 개수 출력
 
         for(int i = 0; i < 5; i++)  // 행
             for(int j = 0; j < 5; j++)  // 열
-                BFS(new Node(i, j, map[i][j])); // 노드 객체로 BFS
+                BFS(new Node(i, j, map[i][j])); // 해당 좌표로 BFS
 
-        System.out.println(hashSet.size()); // 개수 출력
+        System.out.println(hashSet.size()); // 해시셋 크기 출력
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
