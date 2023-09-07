6d12181b03a766ea03d527ac4792a42da77efac6
leejk298 <5552269@naver.com>
2023-09-07 11:27:10 +0900

    보물섬_백준

--- src/보물섬_백준.java
+++ src/보물섬_백준.java
@@ -17,10 +17,10 @@ public class 보물섬_백준 {
     static int[] dx = {-1, 1, 0, 0};    // 4방향
     static int[] dy = {0, 0, -1, 1};
 
-    static class Node { // 노드 클래스
+    static class Node { // 내부 클래스
         int x, y, d;    // 좌표, 거리
 
-        public Node(int x, int y, int d) {
+        public Node(int x, int y, int d) {  // 파라미터 생성자
             this.x = x;
             this.y = y;
             this.d = d;
@@ -35,9 +35,10 @@ public class 보물섬_백준 {
         N = Integer.parseInt(st.nextToken());   // 행
         M = Integer.parseInt(st.nextToken());   // 열
 
-        map = new char[N][M];   // 초기화
-        for(int i = 0; i < N; i++)  // 행 개수만큼
-            map[i] = bf.readLine().toCharArray();   // 한 줄 스트링, 문자배열로 저장
+        // 초기화
+        map = new char[N][M];
+        for(int i = 0; i < N; i++)  // 행
+            map[i] = bf.readLine().toCharArray();   // 입력배열 저장
     }
 
     public static boolean isNotValidPos(int x, int y) { // 좌표가 유효한지
@@ -48,46 +49,46 @@ public class 보물섬_백준 {
 
         Queue<Node> queue = new LinkedList<>(); // 큐
 
-        queue.offer(new Node(x, y, 0)); // 시작점 큐에 삽입
-        visited[x][y] = true;   // 방문 여부 갱신
+        queue.offer(new Node(x, y, 0)); // 시작 좌표와 거리 0인 노드 객체를 큐에 삽입
+        visited[x][y] = true;   // 시작점부터 방문
+        int distance = 0;   // 거리 0 => 종료 조건이 없으므로 따로 관리
 
-        int distance = 0;   // 가장 먼 거리
         while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
             Node now = queue.poll();    // 하나 꺼내어
 
-            int nowX = now.x, nowY = now.y, nowD = now.d;   // 현재 좌표와 거리
+            int nowX = now.x, nowY = now.y, nowD = now.d;   // 현재 좌표, 거리
             for(int i = 0; i < 4; i++) {    // 4방향
                 int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
 
                 if(isNotValidPos(tmpX, tmpY) || visited[tmpX][tmpY])    // 유효한지
                     continue;
 
-                if(map[tmpX][tmpY] == 'L') {    // 육지이면
-                    distance = Math.max(distance, nowD + 1);    // 최대거리 갱신
-                    visited[tmpX][tmpY] = true; // 방문 여부 갱신
+                if(map[tmpX][tmpY] == 'L') {    // 육지면
+                    distance = Math.max(distance, nowD + 1);    // 거리 갱신 => 보물 찾기
+                    visited[tmpX][tmpY] = true; // 방문
                     queue.offer(new Node(tmpX, tmpY, nowD + 1));    // 큐에 삽입
                 }
             }
         }
 
-        return distance;    // 최대 거리 리턴
+        return distance;    // 시작점으로부터 가장 먼 거리 리턴
     }
 
-    public static void printShortestPath() {    // 보물이 있는 최소 경로
+    public static void printShortestPath() {    // 보물이 위치한 거리 출력
 
-        int max = 0;    // 최대값
+        int max = 0;    // 거리
 
         for(int i = 0; i < N; i++) {    // 행
             for(int j = 0; j < M; j++) {    // 열
-                if(map[i][j] == 'L') {  // 육지이면
-                    visited = new boolean[N][M];    // 초기화
-                    int res = BFS(i, j);    // BFS, 해당 좌표의 최대 거리
-                    max = Math.max(max, res);   // 모든 좌표의 최대 거리 값
+                if(map[i][j] == 'L') {  // 육지면
+                    visited = new boolean[N][M];    // 방문배열 초기화
+                    int res = BFS(i, j);    // BFS, 해당 좌표에 가장 먼 거리
+                    max = Math.max(max, res);   // 가장 먼 거리 저장
                 }
             }
         }
 
-        System.out.println(max);    // 결과 출력
+        System.out.println(max);    // 최대 거리 출력
     }
 
     public static void main(String[] args) throws IOException {
