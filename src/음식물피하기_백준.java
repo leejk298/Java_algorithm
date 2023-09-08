288cdff08a61a7ce77b3f0878aeab4f5fd2c8c24
leejk298 <5552269@naver.com>
2023-09-08 16:37:49 +0900

    음식물피하기_백준

--- src/음식물피하기_백준.java
+++ src/음식물피하기_백준.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*
@@ -10,28 +11,32 @@ import java.util.*;
  */
 
 public class 음식물피하기_백준 {
-    static int N, M, K, count;  // 크기, 개수
-    static int[][] map; // 맵
+    static int N, M, K; // 크기
+    static int[][] map; // 입력배열
     static boolean[][] visited; // 방문배열
     static int[] dx = {-1, 1, 0, 0};    // 4방향
     static int[] dy = {0, 0, -1, 1};
 
-    public static void init() { // 초기화
-        Scanner sc = new Scanner(System.in);    // 입력
+    public static void init() throws IOException {  // 초기화
 
-        N = sc.nextInt();   // 행
-        M = sc.nextInt();   // 열
-        K = sc.nextInt();   // 개수
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+        N = Integer.parseInt(st.nextToken());   // 행
+        M = Integer.parseInt(st.nextToken());   // 열
+        K = Integer.parseInt(st.nextToken());   // 좌표 개수
 
         // 초기화
         map = new int[N][M];
         visited = new boolean[N][M];
 
-        for(int i = 0; i < K; i++) {    // 개수만큼
-            int x = sc.nextInt();   // 좌표
-            int y = sc.nextInt();
+        for (int i = 0; i < K; i++) {   // 개수만큼
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            map[x - 1][y - 1] = 1;  // 맵 저장
+            int x = Integer.parseInt(st.nextToken());   // x
+            int y = Integer.parseInt(st.nextToken());   // y
+
+            map[x - 1][y - 1] = 1;  // 해당 좌표에 1 저장
         }
     }
 
@@ -39,54 +44,51 @@ public class 음식물피하기_백준 {
         return (x < 0 || x >= N || y < 0 || y >= M);
     }
 
-    public static void BFS(int x, int y) {  // BFS
+    public static int BFS(int x, int y) {   // BFS
+
         Queue<int[]> queue = new LinkedList<>();    // 큐
 
-        queue.offer(new int[] {x, y});  // 시작점 삽입
-        visited[x][y] = true;   // 방문여부 갱신
-        count++;    // 개수 카운트
+        queue.offer(new int[]{x, y});   // 큐에 시작점 삽입
+        visited[x][y] = true;   // 시작점 방문
+        int count = 1;  // 개수 카운트
 
-        while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
+        while (!queue.isEmpty()) {  // 큐가 비어있지 않으면
             int[] now = queue.poll();   // 하나 꺼내어
 
-            int nowX = now[0], nowY = now[1];   // 좌표
-            for(int i = 0; i < 4; i++) {    // 4방향
+            int nowX = now[0], nowY = now[1];   // 현재 좌표
+            for (int i = 0; i < 4; i++) {   // 4방향
                 int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
 
-                if(isNotValidPos(tmpX, tmpY) || visited[tmpX][tmpY])    // 유효한지
+                if (isNotValidPos(tmpX, tmpY) || visited[tmpX][tmpY])   // 유효한지
                     continue;
 
-                if(map[tmpX][tmpY] == 1) {  // 1이면
-                    visited[tmpX][tmpY] = true; // 방문여부 갱신
+                if (map[tmpX][tmpY] == 1) { // 1이면
+                    visited[tmpX][tmpY] = true; // 방문
                     count++;    // 개수 카운트
-                    queue.offer(new int[] {tmpX, tmpY});    // 큐에 삽입
+                    queue.offer(new int[]{tmpX, tmpY}); // 큐에 삽입
                 }
             }
         }
+
+        return count;   // 총 개수 리턴
     }
 
-    public static int findMaxCount() {  // 최대 개수 찾기
+    public static void findMaxCount() { // 최대값 찾기
+
         int max = 0;    // 최대값
 
-        for(int i = 0; i < N; i++) {    // 행
-            for(int j = 0; j < M; j++) {    // 열
-                if(map[i][j] == 1 && !visited[i][j]) {  // 1이고 방문하지 않았으면
-                    count = 0;  // 초기화
-                    BFS(i, j);  // BFS
+        for (int i = 0; i < N; i++) // 행
+            for (int j = 0; j < M; j++) // 열
+                if (map[i][j] == 1 && !visited[i][j])   // 1이고 방문한 적이 없으면
+                    max = Math.max(max, BFS(i, j)); // BFS, 최대값 저장
 
-                    if(max < count) // 최대값 찾기
-                        max = count;
-                }
-            }
-        }
-
-        return max; // 리턴
+        System.out.println(max);    // 최대값 출력
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
-        System.out.println(findMaxCount()); // 최대값 출력
+        findMaxCount(); // 최대값 찾기
     }
 }
