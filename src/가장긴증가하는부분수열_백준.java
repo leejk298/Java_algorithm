8ae2dbfb419c150756492b7703d60b1521ed6ddb
leejk298 <5552269@naver.com>
2023-09-12 18:48:09 +0900

    가장긴증가하는부분수열_백준

--- src/가장긴증가하는부분수열_백준.java
+++ src/가장긴증가하는부분수열_백준.java
@@ -28,11 +28,11 @@ public class 가장긴증가하는부분수열_백준 {
     public static void printLIS() { // 가장 긴 증가 수열 길이 출력
 
         int max = 1;    // 최대값
+
         for(int i = 0; i < N; i++) {    // 크기만큼
-            for(int j = 0; j < i; j++) {    // 이전값과 비교하여
+            for(int j = 0; j < i; j++)   // 이전값과 비교하여
                 if(arr[i] > arr[j]) // 크면
                     dp[i] = Math.max(dp[i], dp[j] + 1); // 해당 증가 길이 저장
-            }
 
             max = Math.max(max, dp[i]); // 최대 증가 길이
         }
