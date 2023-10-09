a02833aeb8f96e0a9da78a72d7af5359539f7160
leejk298 <5552269@naver.com>
2023-10-09 10:34:23 +0900

    순위검색_프로그래머스

--- src/순위검색_프로그래머스.java
+++ src/순위검색_프로그래머스.java
@@ -9,14 +9,46 @@ public class 순위검색_프로그래머스 {
                 new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
                         "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"})));
     }
-
     static class Solution {
         static HashMap<String, List<Integer>> map;  // info
+        // 이분 탐색
+        public static int binarySearch(String key, int score) {
 
+            List<Integer> list = map.get(key);  // 점수
+
+            int start = 0, end = list.size() - 1;   // 시작, 끝
+            while (start <= end) {  // 역전이 아니면
+                int mid = (start + end) / 2;    // 중앙값
+                if (list.get(mid) < score)  // 원하는 점수보다 낮으면
+                    start = mid + 1;        // 시작 인덱스 갱신
+                else                        // 같거나 높으면
+                    end = mid - 1;          // 끝 인덱스 갱신
+            }
+            // 역전이 일어나서 시작 > 끝
+            return list.size() - start; // 해당하는 점수의 개수 리턴
+        }
+        // info가 포함될 수 있는 문장
+        public static void makeSentence(String[] p, String str, int cnt) {
+            // 베이스케이스
+            if (cnt == 4) { // 완성되면
+                if (!map.containsKey(str)) {    // 맵에 없는지 확인
+                    List<Integer> list = new ArrayList<Integer>();  // 없으면, 점수 저장할 리스트 생성
+                    map.put(str, list); // 문자열과 빈리스트 저장, 정렬하기위해
+                }
+
+                map.get(str).add(Integer.parseInt(p[4]));   // 빈리스트에 해당하는 모든 점수 저장 => 정렬
+                return; // 리턴
+            }
+
+            // 재귀케이스, 완성이 아니면
+            makeSentence(p, str + "-", cnt + 1);    // -
+            makeSentence(p, str + p[cnt], cnt + 1); // 해당 문자열 추가
+        }
         public static int[] solution(String[] info, String[] query) {
-            int[] answer = new int[query.length];   // 결과배열
-            map = new HashMap<String, List<Integer>>(); // info 저장할 해시맵
 
+            int[] answer = new int[query.length];   // 결과배열
+
+            map = new HashMap<String, List<Integer>>(); // info 저장할 해시맵
             for (int i = 0; i < info.length; i++) {
                 String[] p = info[i].split(" ");    // 공백 구분
                 makeSentence(p, "", 0); // 모든 경우의 수 만들기, 백트래킹
@@ -28,43 +60,11 @@ public class 순위검색_프로그래머스 {
             for (int i = 0; i < query.length; i++) {    // 쿼리 개수만큼
                 query[i] = query[i].replaceAll(" and ", ""); // 문자열 바꾸기
                 String[] q = query[i].split(" ");   // 공백 기준으로 나눠, 문자열과 점수로 해시맵과 같은 형태로
+
                 answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0; // 문자열에 해당하는 점수를 이분탐색 (정렬되었으므로 가능)
             }
 
             return answer;  // 결과 리턴
-        }
-
-        // 이분 탐색
-        private static int binarySearch(String key, int score) {
-            List<Integer> list = map.get(key);  // 점수
-            int start = 0, end = list.size() - 1;   // 시작, 끝
-
-            while (start <= end) {  // 역전이 아니면
-                int mid = (start + end) / 2;    // 중앙값
-                if (list.get(mid) < score)  // 원하는 점수보다 낮으면
-                    start = mid + 1;        // 시작 인덱스 갱신
-                else                        // 같거나 높으면
-                    end = mid - 1;          // 끝 인덱스 갱신
-            }
-            // 역전이 일어나서 시작 > 끝
-            return list.size() - start; // 해당하는 점수의 개수 리턴
-        }
-
-        // info가 포함될 수 있는 문장
-        private static void makeSentence(String[] p, String str, int cnt) {
-            if (cnt == 4) { // 완성되면
-                if (!map.containsKey(str)) {    // 맵에 없는지 확인
-                    List<Integer> list = new ArrayList<Integer>();  // 없으면, 점수 저장할 리스트 생성
-                    map.put(str, list); // 문자열과 빈리스트 저장, 정렬하기위해
-                }
-
-                map.get(str).add(Integer.parseInt(p[4]));   // 빈리스트에 해당하는 모든 점수 저장 => 정렬
-
-                return; // 리턴
-            }
-            // 완성이 아니면
-            makeSentence(p, str + "-", cnt + 1);    // -
-            makeSentence(p, str + p[cnt], cnt + 1); // 해당 문자열 추가
         }
     }
 }
