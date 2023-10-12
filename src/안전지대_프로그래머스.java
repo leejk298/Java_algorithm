8221ad50179b76a7249e703bc3561dbd647ca9d0
leejk298 <5552269@naver.com>
2023-10-12 14:33:20 +0900

    안전지대_프로그래머스

--- src/안전지대_프로그래머스.java
+++ src/안전지대_프로그래머스.java
@@ -6,34 +6,35 @@ public class 안전지대_프로그래머스 {
     }
 
     static class Solution {
-        public int solution(int[][] board) {
-            int[][] copy = new int[board.length][board.length];
-            int answer = 0;
+        public static void checkAround(int x, int y, int[][] copy) {    // BFS
 
-            for(int i = 0; i < board.length; i++)
-                for(int j = 0; j < board.length; j++)
-                    if(board[i][j] == 1)
-                        checkAround(i, j, copy);
-
-            for(int[] arr : copy)
-                for(int i : arr)
-                    if(i == 0)
-                        answer++;
-
-            return answer;
-        }
-
-        static void checkAround(int x, int y, int[][] copy) {
-            int[] dx = {0, -1, -1, 0, 1, 1, 1, 0, -1};
+            int[] dx = {0, -1, -1, 0, 1, 1, 1, 0, -1};  // 9방향
             int[] dy = {0, 0, 1, 1, 1, 0, -1, -1, -1};
 
-            for(int i = 0; i < 9; i++) {
-                int tmpX = dx[i] + x;
+            for(int i = 0; i < 9; i++) {    // 9방향만큼
+                int tmpX = dx[i] + x;   // 다음 좌표
                 int tmpY = dy[i] + y;
 
-                if(tmpX >= 0 && tmpX < copy.length && tmpY >= 0 && tmpY < copy.length)
-                    copy[tmpX][tmpY] = 1;
+                if(tmpX >= 0 && tmpX < copy.length && tmpY >= 0 && tmpY < copy.length)   // 유효하면
+                    copy[tmpX][tmpY] = 1;   // 1
             }
+        }
+        public int solution(int[][] board) {
+
+            int[][] copy = new int[board.length][board.length]; // 복사배열
+            int answer = 0; // 결과값
+
+            for(int i = 0; i < board.length; i++)   // 행
+                for(int j = 0; j < board.length; j++)   // 열
+                    if(board[i][j] == 1)    // 1이면
+                        checkAround(i, j, copy);    // BFS
+
+            for(int[] arr : copy)   // 행
+                for(int i : arr)    // 열
+                    if(i == 0)  // 0이면
+                        answer++;   // 개수 카운트
+
+            return answer;  // 결과값
         }
     }
 }
