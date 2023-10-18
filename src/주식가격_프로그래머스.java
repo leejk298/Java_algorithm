e882b0b200e93e8e4f09c464b6b981cda8d1f7e9
leejk298 <5552269@naver.com>
2023-10-18 19:16:57 +0900

    주식가격_프로그래머스

--- src/주식가격_프로그래머스.java
+++ src/주식가격_프로그래머스.java
@@ -8,8 +8,10 @@ public class 주식가격_프로그래머스 {
 
     static class Solution {
         public int[] solution(int[] prices) {
+
             int len = prices.length;    // 길이
             int[] answer = new int[len];    // 결과배열
+
             Stack<Integer> stack = new Stack<>();   // 스택 자료구조
 
             for(int i = 0; i < len; i++) {  // 길이만큼
@@ -18,7 +20,6 @@ public class 주식가격_프로그래머스 {
                     int p = stack.pop();    // 꺼내어
                     answer[p] = i - p;      // 시간 체크
                 }
-
                 // 비어있거나 상승장이면
                 stack.push(i);   // 삽입
             }
