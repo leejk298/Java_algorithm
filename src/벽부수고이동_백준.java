9fa71533d412bc89e11eb7ed7d7c3cd85a4394cb
leejk298 <5552269@naver.com>
2023-10-23 13:24:00 +0900

    벽부수고이동_백준

--- src/벽부수고이동_백준.java
+++ src/벽부수고이동_백준.java
@@ -2,28 +2,27 @@ import java.util.*;
 import java.io.*;
 
 /*
-6 4
-0100
-1110
+5 4
+0010
 1000
-0000
-0111
-0000
+0100
+0001
+0100
  */
 public class 벽부수고이동_백준 {
     static int N, M;    // 크기
-    static int[][] map, D;  // 입력, 거리배열
-    static boolean[][][] visited;   // 방문배열, 3차원으로 z: 벽 부술 수 있는 횟수
+    static boolean[][][] visited;   // 방문배열
+    static int[][] map; // 입력배열
     static int[] dx = {-1, 1, 0, 0};    // 4방향
     static int[] dy = {0, 0, -1, 1};
 
     static class Node { // 노드 클래스
-        int x, y, z;    // 좌표, 벽 부술 수 있는 횟수
-
-        public Node(int x, int y, int z) {
-            this.x = x;
+        int x, y, z, d;
+        public Node(int x, int y, int z, int d) {   // 파라미터 생성자
+            this.x = x; // 좌표
             this.y = y;
-            this.z = z;
+            this.z = z; // 벽 부술수 있는 찬스
+            this.d = d; // 거리
         }
     }
 
@@ -37,8 +36,7 @@ public class 벽부수고이동_백준 {
 
         // 초기화
         map = new int[N][M];
-        D = new int[N][M];
-        visited = new boolean[N][M][2]; // 한 번만 가능이므로 0, 1로 표시
+        visited = new boolean[2][N][M];
 
         for(int i = 0; i < N; i++) {    // 행
             char[] ch = bf.readLine().toCharArray();    // 문자배열
@@ -52,50 +50,42 @@ public class 벽부수고이동_백준 {
         return (x < 0 || x >= N || y < 0 || y >= M);
     }
 
-    public static boolean BFS(int x, int y, int z) {    // BFS
+    public static int BFS(int x, int y, int z, int d) { // BFS
 
         Queue<Node> queue = new LinkedList<>(); // 큐
 
-        queue.offer(new Node(x, y, z)); // 시작점 삽입
-        D[x][y] = 1;    // 시작점부터 거리 1
-        visited[x][y][z] = true;    // 시작점 방문
+        queue.offer(new Node(x, y, z, d));  // 시작점 큐에 삽입
+        visited[z][x][y] = true;    // 방문여부 갱신
 
         while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
             Node now = queue.poll();    // 하나 꺼내어
 
-            int nowX = now.x, nowY = now.y, nowZ = now.z;   // 현재 좌표, 횟수
-
+            int nowX = now.x, nowY = now.y, nowZ = now.z, nowD = now.d; // 현재 노드값
             if(nowX == N - 1 && nowY == M - 1)  // 도착점에 도달하면
-                return true;    // true
+                return nowD;    // 현재 객체의 거리값 출력
 
             for(int i = 0; i < 4; i++) {    // 4방향
                 int tmpX = nowX + dx[i], tmpY = nowY + dy[i];   // 다음 좌표
-
-                if(isNotValidPos(tmpX, tmpY) || visited[tmpX][tmpY][nowZ])  // 유효한지
+                if(isNotValidPos(tmpX, tmpY) || visited[nowZ][tmpX][tmpY])  // 유효한지
                     continue;
 
-                if(map[tmpX][tmpY] == 0) {  // 벽이 아니면
-                    visited[tmpX][tmpY][nowZ] = true;   // 방문
-                    D[tmpX][tmpY] = D[nowX][nowY] + 1;  // 거리
-                    queue.offer(new Node(tmpX, tmpY, nowZ));    // 큐에 삽입
-                } else if(nowZ == 1) {  // 벽이고 부술 수 있는 횟수가 1이면
-                    visited[tmpX][tmpY][nowZ] = true;   // 방문
-                    D[tmpX][tmpY] = D[nowX][nowY] + 1;  // 거리
-                    queue.offer(new Node(tmpX, tmpY, nowZ - 1));    // 큐에 삽입, 횟수 -1
+                if(map[tmpX][tmpY] == 0) {  // 거리이면
+                    visited[nowZ][tmpX][tmpY] = true;   // 방문 가능
+                    queue.offer(new Node(tmpX, tmpY, nowZ, nowD + 1));  // 거리 + 1
+                } else if(nowZ == 1) {  // 벽이고 허물 수 있으면
+                    visited[nowZ - 1][tmpX][tmpY] = true;   // 방문 가능, 찬스 - 1
+                    queue.offer(new Node(tmpX, tmpY, nowZ - 1, nowD + 1));  // 찬스 - 1, 거리 + 1
                 }
             }
         }
 
-        return false;   // 여기로 오면 도달 불가능이므로 false
+        return -1;  // 여기에 도달하면 도착할 수 없는 것이므로 -1 출력
     }
 
     public static void main(String[] args) throws IOException {
 
-       init();  // 초기화
+        init();  // 초기화
 
-       if(BFS(0, 0, 1)) // BFS, 도달 가능하면
-           System.out.println(D[N - 1][M - 1]); // 도착점 거리 출력
-       else // 도달 불가능하면
-           System.out.println(-1);  // -1 출력
+        System.out.println(BFS(0, 0, 1, 1));    // BFS, 좌표(0, 0), 찬스, 거리
     }
 }
