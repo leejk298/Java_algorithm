4d5e9e952bb883e9b2bbee66c09addd406cb2e22
leejk298 <5552269@naver.com>
2023-10-26 19:16:52 +0900

    올바른괄호갯수_프로그래머스

--- src/올바른괄호갯수_프로그래머스.java
+++ src/올바른괄호갯수_프로그래머스.java
@@ -3,19 +3,18 @@ public class 올바른괄호갯수_프로그래머스 {
         Solution solution = new Solution();
         System.out.println(solution.solution(3));
     }
-
     static class Solution {
-        static int answer;
-
+        static int answer;  // 결과값
         public int solution(int n) {
-            answer = 0;
 
-            dfs(0, 0, n);   // 루트
+            answer = 0; // 초기화
 
-            return answer;
+            DFS(0, 0, n);   // DFS
+
+            return answer;  // 결과값 리턴
         }
+        public void DFS(int l, int r, int N) { // DFS
 
-        public void dfs(int l, int r, int N) {
             if (l < r)  // 여는 괄호 먼저
                 return;
 
@@ -27,10 +26,9 @@ public class 올바른괄호갯수_프로그래머스 {
 
                 return;
             }
-
             // 왼쪽부터 => 여는 괄호 먼저
-            dfs(l + 1, r, N);
-            dfs(l, r + 1, N);
+            DFS(l + 1, r, N);
+            DFS(l, r + 1, N);
         }
     }
 }
