30840d631f680569f7e9498b27e5578aa7267daf
leejk298 <5552269@naver.com>
2023-10-30 19:23:38 +0900

    합승택시요금2_프로그래머스

--- src/합승택시요금2_프로그래머스.java
+++ src/합승택시요금2_프로그래머스.java
@@ -1,17 +1,15 @@
 import java.util.*;
 
 public class 합승택시요금2_프로그래머스 {
-
     public static void main(String[] args) {
         합승택시요금_프로그래머스.Solution solution = new 합승택시요금_프로그래머스.Solution();
         System.out.println(solution.solution(6, 4, 6, 2,
                 new int[][] {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}}));
     }
-
     static class Solution {
         static final int MaxDistance = 200 * 10000 + 1; // 최대 길이
-
         public int solution(int n, int s, int a, int b, int[][] fares) {
+
             int[][] map = new int[n + 1][n + 1];    // 최단경로배열
             for(int i = 0; i <= n; i++) {   // 행
                 for(int j = 0; j <= n; j++) {   // 열
