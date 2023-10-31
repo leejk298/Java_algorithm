6f97bfbf797b3236d05bd7593b8e51497d56c3d5
leejk298 <5552269@naver.com>
2023-10-31 14:15:45 +0900

    신고결과받기_프로그래머스

--- src/신고결과받기_프로그래머스.java
+++ src/신고결과받기_프로그래머스.java
@@ -7,41 +7,36 @@ public class 신고결과받기_프로그래머스 {
                 new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2)));
 
     }
-
     static class Solution {
         Map<String, HashSet<String>> reportMap = new HashMap<>();
         Map<String, Integer> answerMap = new HashMap<>();
         public int[] solution(String[] id_list, String[] report, int k) {
+
             int[] answer = new int[id_list.length];
 
-            // Hash 초기화
-            for(int i = 0; i < id_list.length; i++) {
+            for(int i = 0; i < id_list.length; i++) {   // Hash 초기화
                 HashSet<String> reportId = new HashSet<>();
                 reportMap.put(id_list[i], reportId);
                 answerMap.put(id_list[i], 0);
             }
 
-            // 값 세팅
-            for(String str : report) {
+            for(String str : report) {  // 값 세팅
                 String[] tmp = str.split(" ");
                 reportMap.get(tmp[1]).add(tmp[0]);
             }
 
-            // 처리
-            for(String str : reportMap.keySet()) {
+            for(String str : reportMap.keySet()) {  // 처리
                 HashSet<String> rId = reportMap.get(str);
-
                 if(rId.size() >= k) {
                     for(String id : rId)
                         answerMap.put(id, answerMap.get(id) + 1);
                 }
             }
 
-            // copy
-            for(int i = 0; i < id_list.length; i++)
+            for(int i = 0; i < id_list.length; i++) // copy
                 answer[i] = answerMap.get(id_list[i]);
 
-            return answer;
+            return answer;  // 결과 배열 리턴
         }
     }
 }
