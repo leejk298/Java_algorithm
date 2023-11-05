fc2b6a98fefbf6e83306aeed7847b09af6dfb1f8
leejk298 <5552269@naver.com>
2023-11-05 12:35:16 +0900

    중복순열_DFS

--- src/중복순열_DFS.java
+++ src/중복순열_DFS.java
@@ -1,27 +1,13 @@
 import java.util.Scanner;
 
 public class 중복순열_DFS {
-    public static int N;    // 총 개수
-    public static int M;    // 뽑을 개수
-    public static int A[];  // 주어진 숫자배열
-    public static int res[]; // 결과배열
+    static int N;    // 총 개수
+    static int M;    // 뽑을 개수
+    static int A[];  // 주어진 숫자배열
+    static int res[]; // 결과배열
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void dfs(int v) {   // dfs
 
-        N = sc.nextInt();
-        M = sc.nextInt();
-        A = new int[N];
-        res = new int[M];
-
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
-        }
-
-        dfs(0); // 배열 A index 0번부터 dfs 수행
-    }
-
-    private static void dfs(int v) {   // dfs
         if (v == M) {   // 뽑을 개수와 같으면
             for (int i : res) { // 배열 res 를 i로 순회하면서
                 System.out.print(i);  // 출력
@@ -34,5 +20,20 @@ public class 중복순열_DFS {
                 dfs(v + 1);  // 다음 index 로 dfs 수행
             }
         }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        A = new int[N];
+        res = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        dfs(0); // 배열 A index 0번부터 dfs 수행
     }
 }
