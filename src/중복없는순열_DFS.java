0ca71d2e4e393369f5f3ae2274d6a656047c5b45
leejk298 <5552269@naver.com>
2023-11-06 15:35:42 +0900

    중복없는순열_DFS

--- src/중복없는순열_DFS.java
+++ src/중복없는순열_DFS.java
@@ -1,28 +1,14 @@
 import java.util.Scanner;
 
 public class 중복없는순열_DFS {
-    public static int N;    // 총 개수
-    public static int M;    // 뽑을 개수
-    public static int A[];  // 주어진 숫자배열
-    public static int res[]; // 결과배열
-    public static boolean check[];  // 방문배열
+    static int N;    // 총 개수
+    static int M;    // 뽑을 개수
+    static int A[];  // 주어진 숫자배열
+    static int res[]; // 결과배열
+    static boolean check[];  // 방문배열
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    // 입력
+    public static void BackTracking(int v) {    // dfs
 
-        N = sc.nextInt();   // 크기
-        M = sc.nextInt();   // 뽑을 개수
-        A = new int[N];     // 입력배열
-        res = new int[M];   // 결과배열
-        check = new boolean[N]; // 총 개수만큼
-
-        for (int i = 0; i < N; i++) // 크기만큼
-            A[i] = sc.nextInt();    // 저장
-
-        BackTracking(0); // 배열 A index 0번부터 dfs 수행
-    }
-
-    private static void BackTracking(int v) {    // dfs
         if (v == M) {   // 뽑을 개수와 같으면
             for (int i : res) // 배열 res 를 i로 순회하면서
                 System.out.print(i);    // 출력
@@ -37,5 +23,21 @@ public class 중복없는순열_DFS {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);    // 입력
+
+        N = sc.nextInt();   // 크기
+        M = sc.nextInt();   // 뽑을 개수
+        A = new int[N];     // 입력배열
+        res = new int[M];   // 결과배열
+        check = new boolean[N]; // 총 개수만큼
+
+        for (int i = 0; i < N; i++) // 크기만큼
+            A[i] = sc.nextInt();    // 저장
+
+        BackTracking(0); // 배열 A index 0번부터 dfs 수행
     }
 }
