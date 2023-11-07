e329472110634a165a3acf94aef5f23ab06a3336
leejk298 <5552269@naver.com>
2023-11-08 01:40:20 +0900

    최소값2_SegmentTree_072

--- src/최소값2_SegmentTree_072.java
+++ src/최소값2_SegmentTree_072.java
@@ -2,14 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class 최소값2_SegmentTree_072 {
+    static int N, M;    // 크기
+    static int treeSize, leafNodeStartIndex;    // 크기, 시작인덱스
     static long tree[]; // 트리
 
+    public static void setTree(int i) { // 세그먼트 트리 만들기
+
+        while (i != 1) { // 루트 노드가 아니면
+            if (tree[i / 2] > tree[i]) // 최소값 저장
+                tree[i / 2] = tree[i];
+            i--; // 인덱스 이동
+        }
+    }
+
+    public static long getMin(int S, int E) { // 최소값
+
+        long min = Long.MAX_VALUE; // 최대값으로 초기화 => 최소값 찾기위해
+
+        while (S <= E) { // 역전이 아니면 반복
+            if (S % 2 == 1) { // 오른쪽 노드이면 독립노드 => 부모노드 영향 X
+                min = Math.min(min, tree[S]); // 최소값 저장
+                S++; // 왼쪽이면 오른쪽으로, 오른쪽이면 독립노드 다음 노드로 건너띔
+            }
+
+            if (E % 2 == 0) { // 왼쪽 노드이면 독립노드 => 부모노드 영향 X
+                min = Math.min(min, tree[E]); // 최소값 저장
+                E--; // 오른쪽이면 왼쪽으로, 왼쪽이면 독립노드 다음 노드로 건너띔
+            }
+
+            S /= 2; // 부모노드로 이동
+            E /= 2; // 부모노드로 이동
+        }
+
+        return min; // 최소값 리턴
+    }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
-        int N = Integer.parseInt(st.nextToken()); // 노드 개수
-        int M = Integer.parseInt(st.nextToken()); // 질의 개수
+        N = Integer.parseInt(st.nextToken()); // 노드 개수
+        M = Integer.parseInt(st.nextToken()); // 질의 개수
         int treeHeight = 0, t = N; // 트리높이(k), N
 
         while (t != 0) { // 2^k >= N => k 구하기
@@ -17,8 +51,8 @@ public class 최소값2_SegmentTree_072 {
             treeHeight++;
         }
 
-        int treeSize = (int) Math.pow(2, treeHeight + 1); // 배열크기: 2^k * 2 = 2^(k+1)
-        int leafNodeStartIndex = treeSize / 2; // 원본데이터(리프노드)시작 인덱스
+        treeSize = (int) Math.pow(2, treeHeight + 1); // 배열크기: 2^k * 2 = 2^(k+1)
+        leafNodeStartIndex = treeSize / 2; // 원본데이터(리프노드)시작 인덱스
 
         tree = new long[treeSize]; // 트리 초기화
         for (int i = 0; i < treeSize; i++)
@@ -42,35 +76,5 @@ public class 최소값2_SegmentTree_072 {
         }
 
         bf.close(); // 입력 버퍼 닫기
-    }
-
-    private static long getMin(int S, int E) { // 최소값
-        long min = Long.MAX_VALUE; // 최대값으로 초기화 => 최소값 찾기위해
-
-        while (S <= E) { // 역전이 아니면 반복
-            if (S % 2 == 1) { // 오른쪽 노드이면 독립노드 => 부모노드 영향 X
-                min = Math.min(min, tree[S]); // 최소값 저장
-                S++; // 왼쪽이면 오른쪽으로, 오른쪽이면 독립노드 다음 노드로 건너띔
-            }
-
-            if (E % 2 == 0) { // 왼쪽 노드이면 독립노드 => 부모노드 영향 X
-                min = Math.min(min, tree[E]); // 최소값 저장
-                E--; // 오른쪽이면 왼쪽으로, 왼쪽이면 독립노드 다음 노드로 건너띔
-            }
-
-            S /= 2; // 부모노드로 이동
-            E /= 2; // 부모노드로 이동
-        }
-
-        return min; // 최소값 리턴
-    }
-
-    private static void setTree(int i) { // 세그먼트 트리 만들기
-        while (i != 1) { // 루트 노드가 아니면
-            if (tree[i / 2] > tree[i]) // 최소값 저장
-                tree[i / 2] = tree[i];
-            i--; // 인덱스 이동
-        }
-
     }
 }
