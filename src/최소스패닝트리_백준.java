dcfe2b262246894a04d6ff6f9659f09056e4be1f
leejk298 <5552269@naver.com>
2023-11-11 22:14:46 +0900

    최소스패닝트리_백준

--- src/최소스패닝트리_백준.java
+++ src/최소스패닝트리_백준.java
@@ -11,20 +11,20 @@ import java.io.*;
 public class 최소스패닝트리_백준 {
     static int N, M;    // 크기
     static int[] parent;    // 부모배열
-    static PriorityQueue<Edge> pq;  // 우선순위 큐, 엣지클래스 => 그래프 표현
+    static PriorityQueue<Edge> pq;  // 우선순위 큐
 
-    static class Edge implements Comparable<Edge> { // 엣지클래스, 우선순위 큐 => Comparable 클래스 구현 => compareTo() 메소드 재정의
-        int s, e, w;
+    static class Edge implements Comparable<Edge> { // 엣지리스트
+        int S, E, W;    // 시작, 도착, 가중치
 
-        public Edge(int s, int e, int w) {  // 파라미터 생성자
-            this.s = s;
-            this.e = e;
-            this.w = w;
+        public Edge(int S, int E, int W) {  // 파라미터 생성자
+            this.S = S;
+            this.E = E;
+            this.W = W;
         }
 
         @Override
-        public int compareTo(Edge e) {  // 메소드 오버라이딩
-            return this.w - e.w;    // 가중치 오름차순 정렬
+        public int compareTo(Edge e) {  // 가중치 오름차순 정렬
+            return this.W - e.W;
         }
     }
 
@@ -40,52 +40,52 @@ public class 최소스패닝트리_백준 {
         parent = new int[N + 1];
         pq = new PriorityQueue<>();
 
-        for(int i = 1; i <= N; i++) // 정점 개수만큼
+        for (int i = 1; i <= N; i++)    // 정점 개수만큼
             parent[i] = i;  // 부모배열 저장
 
-        for(int i = 0; i < M; i++) {    // 엣지 개수만큼
+        for (int i = 0; i < M; i++) {   // 엣지 개수만큼
             st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            int S = Integer.parseInt(st.nextToken());   // 시작
-            int E = Integer.parseInt(st.nextToken());   // 도착
-            int W = Integer.parseInt(st.nextToken());   // 가중치
+            int start = Integer.parseInt(st.nextToken());   // 시작
+            int end = Integer.parseInt(st.nextToken());     // 도착
+            int weight = Integer.parseInt(st.nextToken());  // 가중치
 
-            pq.offer(new Edge(S, E, W));    // 우선순위 큐에 삽입, 가중치 오름차순 정렬(최소힙)
+            pq.offer(new Edge(start, end, weight)); // 우선순위 큐에 저장
         }
     }
 
-    public static int find(int a) { // find()
+    public static int find(int a) { // 대표노드 찾기
 
-        if(a == parent[a])  // 부모와 동일하면
-            return a;   // 그대로 리턴
+        if (a == parent[a]) // 부모와 같으면
+            return a;   // 그대로
 
-        // 다르면 부모배열 갱신 후 리턴
-        return parent[a] = find(parent[a]);
+        return parent[a] = find(parent[a]); // 다르면 부모까지 갱신 후 리턴
     }
 
     public static void union(int a, int b) {    // 합집합
 
-        a = find(a);    // a의 부모
-        b = find(b);    // b의 부모
+        a = find(a);    // 대표노드 a, b
+        b = find(b);
 
-        if(a != b)  // 다르면
-            parent[b] = a;  // 합집합
+        if (a != b) // 다르면
+            parent[a] = b;  // 합집합
     }
 
-    public static void Kruskal() {  // 크루스칼, MST 알고리즘
+    public static void Kruskal() {  // 크루스칼 알고리즘 => MST
 
-        int res = 0;    // 최소값
+        int res = 0;    // 결과값
 
-        while(!pq.isEmpty()) {  // 우선순위 큐가 비어있지 않으면
-            Edge now = pq.poll();   // 현재 노드
+        while (!pq.isEmpty()) { // 우선순위 큐가 비어있지 않으면
+            Edge now = pq.poll();   // 하나 꺼내어
 
-            if(find(now.s) != find(now.e)) { // 시작과 도착이 다르면
-                union(now.s, now.e);    // 합집합
-                res += now.w;   // 가중치 합, 최소값 저장
+            int nowS = now.S, nowE = now.E; // 시작, 도착
+            if (find(nowS) != find(nowE)) { // 대표노드가 다르면
+                union(nowS, nowE);  // 합집합
+                res += now.W;   // 결과값 갱신
             }
         }
 
-        System.out.println(res);    // 최소비용 출력
+        System.out.println(res);    // 결과값 리턴
     }
 
     public static void main(String[] args) throws IOException {
