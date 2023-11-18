edef69758f9e6a9520062fdb1af9220dd6e98a04
leejk298 <5552269@naver.com>
2023-11-18 13:54:37 +0900

    이분그래프_Graph_048

--- src/이분그래프_Graph_048.java
+++ src/이분그래프_Graph_048.java
@@ -4,7 +4,24 @@ import java.io.*;
 public class 이분그래프_Graph_048 {
     static boolean isEven; // 이분그래프 판별
 
+    public static void DFS(int v, ArrayList<Integer>[] A, boolean[] visited, int[] check) { // DFS
+
+        visited[v] = true; // 들어온 노드의 방문정보 갱신
+
+        for (int i : A[v]) { // 해당 노드의 인접리스트 탐색
+            if (!visited[i]) { // 인접리스트 중 방문하지않은 노드가 있으면
+                check[i] = (check[v] + 1) % 2; // 원래 노드와 인접 노드를 서로 다른 집합으로
+
+                DFS(i, A, visited, check); // 인접 노드에 대해 DFS - 재귀
+            }
+
+            else if (check[i] == check[v]) // 이미 방문한 배열이면 같은 집합인 지 체크
+                isEven = false; // 같은 집합이면 이분그래프 x
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
@@ -46,22 +63,6 @@ public class 이분그래프_Graph_048 {
 
             if (isEven) // 모든 노드 탐색 후 이분 그래프이면
                 System.out.println("YES"); // YES 출력
-        }
-    }
-
-    private static void DFS(int v, ArrayList<Integer>[] A, boolean[] visited, int[] check) { // DFS
-
-        visited[v] = true; // 들어온 노드의 방문정보 갱신
-
-        for (int i : A[v]) { // 해당 노드의 인접리스트 탐색
-            if (!visited[i]) { // 인접리스트 중 방문하지않은 노드가 있으면
-                check[i] = (check[v] + 1) % 2; // 원래 노드와 인접 노드를 서로 다른 집합으로
-
-                DFS(i, A, visited, check); // 인접 노드에 대해 DFS - 재귀
-            }
-
-            else if (check[i] == check[v]) // 이미 방문한 배열이면 같은 집합인 지 체크
-                isEven = false; // 같은 집합이면 이분그래프 x
         }
     }
 }
