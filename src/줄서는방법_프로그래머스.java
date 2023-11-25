ad967f89875c844a5deb33112d419489a2225405
leejk298 <5552269@naver.com>
2023-11-25 13:06:24 +0900

    줄서는방법_프로그래머스

--- src/줄서는방법_프로그래머스.java
+++ src/줄서는방법_프로그래머스.java
@@ -8,6 +8,8 @@ public class 줄서는방법_프로그래머스 {
 
     static class Solution {
         public int[] solution(int n, long k) {
+
+            // 초기화
             int[] answer = new int[n];  // 결과배열
             long[] F = new long[n + 1]; // 팩토리얼
             boolean[] visited = new boolean[n + 1]; // 방문배열
@@ -20,6 +22,7 @@ public class 줄서는방법_프로그래머스 {
                 for(int j = 1, flag = 1; j <= n; j++) { // 몇 번째 숫자가 들어갈 지
                     if(visited[j])  // 사용하였으면 스킵
                         continue;
+
                     if(k <= flag * F[n - i]) {  // 맞는 숫자 찾을때까지 flag 이용
                         k = k - (F[n - i] * (flag - 1));    // 다음 자릿수 경우의 수 갱신
                         answer[i - 1] = j;  // 결과배열에 저장
@@ -27,8 +30,8 @@ public class 줄서는방법_프로그래머스 {
 
                         break;  // for(i)
                     }
-                    // flag 갱신 => 다음자리의 경우의 수(팩토리얼)를 flag를 증가시키면서 몇 번째 숫자가 들어갈 지 체크
-                    flag++;
+
+                    flag++; // flag 갱신 => 다음자리의 경우의 수(팩토리얼)를 flag를 증가시키면서 몇 번째 숫자가 들어갈 지 체크
                 }
             }
 
