eea0e0373fd465248ecfe91d6437b5c34a3a6da9
leejk298 <5552269@naver.com>
2023-11-26 12:02:19 +0900

    친구관계파악_DFS_025

--- src/친구관계파악_DFS_025.java
+++ src/친구관계파악_DFS_025.java
@@ -4,7 +4,27 @@ import java.util.*;
 public class 친구관계파악_DFS_025 {
     static boolean arrive; // 도착 여부
 
+    public static void DFS(ArrayList<Integer>[] A, boolean[] visited, int v, int depth) {   // 브루트포스, DFS
+
+        // 베이스케이스
+        if (depth == 5) { // 깊이가 5이면 도착한 것이므로
+            arrive = true; // 도착을 true로 설정
+
+            return; // 함수 종료
+        }
+
+        // 재귀케이스: 깊이가 5가 아니고 도착이 없으면
+        visited[v] = true; // 해당 노드 방문 true로 설정
+
+        for (int i : A[v]) // 해당 노드를 순회하면서
+            if (!visited[i]) // 방문 안한 노드가 있으면
+                DFS(A, visited, i, depth + 1); // 해당 노드로 DFS 재귀호출
+
+        visited[v] = false; // 리턴되면 다시 해당 노드의 방문여부 false로 설정
+    }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
@@ -39,22 +59,5 @@ public class 친구관계파악_DFS_025 {
             System.out.println("1"); // 1 출력
         else // 없으면
             System.out.println("0"); // 0 출력
-    }
-
-    private static void DFS(ArrayList<Integer>[] A, boolean[] visited, int v, int depth) {
-        if (depth == 5) { // 깊이가 5이면 도착한 것이므로
-            arrive = true; // 도착을 true로 설정
-
-            return; // 함수 종료
-        }
-
-        // 깊이가 5가 아니고 도착이 없으면
-        visited[v] = true; // 해당 노드 방문 true로 설정
-
-        for (int i : A[v]) // 해당 노드를 순회하면서
-            if (!visited[i]) // 방문 안한 노드가 있으면
-                DFS(A, visited, i, depth + 1); // 해당 노드로 DFS 재귀호출
-
-        visited[v] = false; // 리턴되면 다시 해당 노드의 방문여부 false로 설정
     }
 }
