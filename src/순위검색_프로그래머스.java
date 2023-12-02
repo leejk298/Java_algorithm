3db0da4d7693e36b484b371abdf96c20c00b52ae
leejk298 <5552269@naver.com>
2023-12-02 12:14:45 +0900

    순위검색_프로그래머스

--- src/순위검색_프로그래머스.java
+++ src/순위검색_프로그래머스.java
@@ -10,25 +10,27 @@ public class 순위검색_프로그래머스 {
                         "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"})));
     }
     static class Solution {
-        static HashMap<String, List<Integer>> map;  // info
-        // 이분 탐색
-        public static int binarySearch(String key, int score) {
+        static HashMap<String, List<Integer>> map;  // info, 해시맵
+
+        public static int binarySearch(String key, int score) { // 이분 탐색
 
             List<Integer> list = map.get(key);  // 점수
 
             int start = 0, end = list.size() - 1;   // 시작, 끝
             while (start <= end) {  // 역전이 아니면
                 int mid = (start + end) / 2;    // 중앙값
+
                 if (list.get(mid) < score)  // 원하는 점수보다 낮으면
                     start = mid + 1;        // 시작 인덱스 갱신
                 else                        // 같거나 높으면
                     end = mid - 1;          // 끝 인덱스 갱신
             }
-            // 역전이 일어나서 시작 > 끝
-            return list.size() - start; // 해당하는 점수의 개수 리턴
+
+            return list.size() - start;  // 역전이 일어나서 시작 > 끝, 해당하는 점수의 개수 리턴
         }
-        // info가 포함될 수 있는 문장
-        public static void makeSentence(String[] p, String str, int cnt) {
+
+        public static void makeSentence(String[] p, String str, int cnt) {  // info가 포함될 수 있는 문장
+
             // 베이스케이스
             if (cnt == 4) { // 완성되면
                 if (!map.containsKey(str)) {    // 맵에 없는지 확인
@@ -44,6 +46,7 @@ public class 순위검색_프로그래머스 {
             makeSentence(p, str + "-", cnt + 1);    // -
             makeSentence(p, str + p[cnt], cnt + 1); // 해당 문자열 추가
         }
+
         public static int[] solution(String[] info, String[] query) {
 
             int[] answer = new int[query.length];   // 결과배열
