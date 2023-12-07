2fb0fd5a6b832c655c44d7b953f6f3a03280ca8b
leejk298 <5552269@naver.com>
2023-12-07 15:27:27 +0900

    기능개발_프로그래머스

--- src/기능개발_프로그래머스.java
+++ src/기능개발_프로그래머스.java
@@ -7,33 +7,37 @@ public class 기능개발_프로그래머스 {
 
     static class Solution {
         public int[] solution(int[] progresses, int[] speeds) {
-            Queue<Integer> queue = new LinkedList<>();
-            for(int i = 0; i < progresses.length; i++) {
-                int num = 100 - progresses[i];
-                if(num % speeds[i] == 0)
-                    queue.offer(num / speeds[i]);
-                else
-                    queue.offer(num / speeds[i] + 1);
+
+            Queue<Integer> queue = new LinkedList<>();  // 큐
+
+            for(int i = 0; i < progresses.length; i++) {    // 크기만큼
+                int num = 100 - progresses[i];  // 숫자
+
+                if(num % speeds[i] == 0)    // 나누어 떨어지면
+                    queue.offer(num / speeds[i]);   // 개수만큼
+                else    // 아니면
+                    queue.offer(num / speeds[i] + 1);   // 개수 + 1
             }
 
-            List<Integer> arr = new ArrayList<>();
+            List<Integer> arr = new ArrayList<>();  // 결과리스트
+            int now = queue.peek(), count = 0;  // 초기화
 
-            int now = queue.peek(), count = 0;
-            while(!queue.isEmpty()) {
-                int next = queue.poll();
+            while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
+                int next = queue.poll();    // 하나 꺼내어
 
-                if(now < next) {
-                    arr.add(count);
-                    count = 0;
-                    now = next;
+                if(now < next) {    // 현재 값보다 크면
+                    arr.add(count); // 리스트에 추가
+
+                    count = 0;  // 개수 초기화
+                    now = next; // 현재값 갱신
                 }
 
-                count++;
+                count++;    // 개수 카운트
             }
 
-            arr.add(count);
+            arr.add(count); // 개수 삽입
 
-            return arr.stream().mapToInt(i -> i).toArray();
+            return arr.stream().mapToInt(i -> i).toArray(); // 정수형 배열 리턴
         }
     }
 }
