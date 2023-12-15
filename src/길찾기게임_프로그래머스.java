ef90183eab2017d23eaa08f599b7ad087f2d8c04
leejk298 <5552269@naver.com>
2023-12-15 13:18:20 +0900

    길찾기게임_프로그래머스

--- src/길찾기게임_프로그래머스.java
+++ src/길찾기게임_프로그래머스.java
@@ -11,35 +11,19 @@ public class 길찾기게임_프로그래머스 {
         static int index;   // 노드 인덱스
         static int[][] answer;  // 결과배열
 
-        public int[][] solution(int[][] nodeinfo) {
+        public class Node { // Node 클래스
+            // 멤버 변수
+            int x, y, w;    // 좌표, 인덱스
+            Node l, r;  // 자식
 
-            answer = new int[2][nodeinfo.length];   // 결과배열 선언
-            Node[] node = new Node[nodeinfo.length];    // 노드
-
-            for(int i = 0; i < nodeinfo.length; i++)    // 크기만큼
-                node[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1, null, null);  // 초기화
-
-            Arrays.sort(node, new Comparator<Node>() {  // 정렬
-                @Override
-                public int compare(Node o1, Node o2) {
-                    if(o1.y == o2.y)    // y값이 같으면
-                        return o1.x - o2.x; // x값에 대해 오름차순
-
-                    return o2.y - o1.y; // y값이 다르면 y값에 대해 내림차순
-                }
-            });
-
-            Node root = node[0];    // 루트노드 => y값이 가장 큰 값
-            for(int i = 1; i < node.length; i++)    // 트리 만들기
-                insertNode(root, node[i]);
-
-            index = 0;  // 노드 인덱스 초기화
-            preOrder(root); // 전위순회
-
-            index = 0;  // 초기화
-            postOrder(root);    // 후위순회
-
-            return answer;  // 결과배열 리턴
+            // 멤버 함수
+            public Node(int x, int y, int w, Node l, Node r) {  // 생성자
+                this.x = x;
+                this.y = y;
+                this.w = w;
+                this.l = l;
+                this.r = r;
+            }
         }
 
         public void preOrder(Node root) {   // 전위 순회: 자 왼 오
@@ -79,19 +63,35 @@ public class 길찾기게임_프로그래머스 {
             }
         }
 
-        public class Node { // Node 클래스
-            // 멤버 변수
-            int x, y, w;    // 좌표, 인덱스
-            Node l, r;  // 자식
+        public int[][] solution(int[][] nodeinfo) {
 
-            // 멤버 함수
-            public Node(int x, int y, int w, Node l, Node r) {  // 생성자
-                this.x = x;
-                this.y = y;
-                this.w = w;
-                this.l = l;
-                this.r = r;
-            }
+            answer = new int[2][nodeinfo.length];   // 결과배열 선언
+            Node[] node = new Node[nodeinfo.length];    // 노드
+
+            for(int i = 0; i < nodeinfo.length; i++)    // 크기만큼
+                node[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1, null, null);  // 초기화
+
+            Arrays.sort(node, new Comparator<Node>() {  // 정렬
+                @Override
+                public int compare(Node o1, Node o2) {
+                    if(o1.y == o2.y)    // y값이 같으면
+                        return o1.x - o2.x; // x값에 대해 오름차순
+
+                    return o2.y - o1.y; // y값이 다르면 y값에 대해 내림차순
+                }
+            });
+
+            Node root = node[0];    // 루트노드 => y값이 가장 큰 값
+            for(int i = 1; i < node.length; i++)    // 트리 만들기
+                insertNode(root, node[i]);
+
+            index = 0;  // 노드 인덱스 초기화
+            preOrder(root); // 전위순회
+
+            index = 0;  // 초기화
+            postOrder(root);    // 후위순회
+
+            return answer;  // 결과배열 리턴
         }
     }
 }
