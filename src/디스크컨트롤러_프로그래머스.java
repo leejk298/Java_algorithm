7d8c20bc3f785397003154b478ac84836fc03d01
leejk298 <5552269@naver.com>
2023-12-16 18:35:09 +0900

    디스크컨트롤러_프로그래머스

--- src/디스크컨트롤러_프로그래머스.java
+++ src/디스크컨트롤러_프로그래머스.java
@@ -8,15 +8,15 @@ public class 디스크컨트롤러_프로그래머스 {
 
     static class Solution {
         public int solution(int[][] jobs) {
-            int len = jobs.length;
-            int index = 0, count = 0, end = 0, sum = 0;
+
+            int len = jobs.length;  // 길이
+            int index = 0, count = 0, end = 0, sum = 0; // 초기화
 
             Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);   // 요청시간 오름차순
 
             PriorityQueue<int []> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);  // 소요시간 오름차순
 
             while(count < len) {    // 길이만큼
-
                 while(index < len && jobs[index][0] <= end)     // 유효하면
                     pq.offer(jobs[index++]);
 
@@ -24,6 +24,7 @@ public class 디스크컨트롤러_프로그래머스 {
                     end = jobs[index][0];   // 해당 작업 요청시간
                 else {  // 비어있지않으면
                     int[] now = pq.poll();  // 하나 꺼내어 -> minHeap (소요시간)
+
                     sum += now[1] + end - now[0];   // 총 시간 갱신
                     end += now[1];  // 종료시간
                     count++;    // 횟수
