d188c837ed3633c1a5126c403ce0ff5aefe1b69b
leejk298 <5552269@naver.com>
2023-12-20 14:46:52 +0900

    괄호변환_프로그래머스

--- src/괄호변환_프로그래머스.java
+++ src/괄호변환_프로그래머스.java
@@ -7,69 +7,72 @@ public class 괄호변환_프로그래머스 {
     }
 
     static class Solution {
-        public String solution(String p) {
-            if(check(p))    // 올바른 괄호
-                return p;
+        public static boolean check(String s) { // 괄호 개수
 
-            return DFS(p);  // 균형잡힌 괄호
-        }
+            Stack<Character> stack = new Stack<>(); // 스택
 
-        static boolean check(String s) {
-            Stack<Character> stack = new Stack<>();
-
-            for(char c : s.toCharArray()) {
-                if(c == '(')
-                    stack.push(c);
-                else {
-                    if(stack.isEmpty())
-                        return false;
-                    else
-                        stack.pop();
+            for(char c : s.toCharArray()) { // 문자열 순회
+                if(c == '(')    // 여는 괄호이면
+                    stack.push(c);  // push
+                else {  // 아니면
+                    if(stack.isEmpty()) // 스택이 비어있으면
+                        return false;   // false
+                    else    // 아니면
+                        stack.pop();    // pop
                 }
             }
 
-            return stack.isEmpty();
+            return stack.isEmpty(); // 스택이 비었는 지 유무를 리턴
         }
 
-        static String DFS(String s) {
-            int count = 0;
-            String result = "";
+        public static String DFS(String s) {    // DFS
 
-            if(s.equals(""))
-                return s;
+            int count = 0;  // 개수
+            String result = ""; // 결과 문자열
 
-            int index = 0;
-            while(index < s.length()) {
-                char c = s.charAt(index++);
+            if(s.equals(""))    // 공백이면
+                return s;   // 그대로 리턴
 
-                if(c == '(')
-                    count++;
+            int index = 0;  // 인덱스
+            while(index < s.length()) { // 문자열 길이만큼
+                char c = s.charAt(index++); // 해당 문자
+
+                if(c == '(')    // 여는 괄호이면
+                    count++;    // 개수 카운트
                 else
                     count--;
 
-                if(count == 0)
-                    break;
+                if(count == 0)  // 0이면
+                    break;  // while 종료
             }
 
-            String u = s.substring(0, index);
-            String v = s.substring(index, s.length());
+            String u = s.substring(0, index);   // 문자열1
+            String v = s.substring(index, s.length());  // 문자열2
 
-            if(check(u))
+            if(check(u))   // 개수가 맞으면
                 result = u + DFS(v);
-            else {
-                result = "(" + DFS(v) + ")";
+            else {  // 아니면
+                result = "(" + DFS(v) + ")";    // 괄호 추가
 
-                for(int j = 1; j < u.length() - 1; j++) {
-                    char c = u.charAt(j);
+                for(int j = 1; j < u.length() - 1; j++) {   // 문자열1 길이만큼
+                    char c = u.charAt(j);   // 해당 문자
 
-                    if(c == '(')
-                        result += ')';
+                    if(c == '(')    // 여는 괄호이면
+                        result += ')';  // 닫는 괄호 추가
                     else
                         result += '(';
                 }
             }
 
-            return result;
+            return result;  // 결과 문자열 리턴
+        }
+
+        public String solution(String p) {
+
+            if(check(p))    // 올바른 괄호
+                return p;
+
+            return DFS(p);  // 균형잡힌 괄호
         }
     }
 }
