42c959c6244c8e7d8c02c0e2696a7ee41a7580c8
leejk298 <5552269@naver.com>
2023-12-31 14:18:17 +0900

    DFS_BFS_026

--- src/DFS_BFS_026.java
+++ src/DFS_BFS_026.java
@@ -5,7 +5,41 @@ public class DFS_BFS_026 {
     static ArrayList<Integer> A[];
     static boolean visited[];
 
+    public static void DFS(int start) { // DFS
+
+        System.out.print(start + " "); // 노드 출력
+        visited[start] = true; // 방문배열 갱신
+
+        for (int i : A[start]) { // 해당 노드의 인접리스트들 순회하면서
+            if (!visited[i]) { // 인접리스트들이 방문되지 않았으면
+                DFS(i); // DFS 실행
+            }
+        }
+    }
+
+    public static void BFS(int start) { // BFS
+
+        Queue<Integer> queue = new LinkedList<>(); // 큐 구현
+
+        queue.add(start); // 해당 노드 큐에 삽입
+        visited[start] = true; // 방문배열 갱신
+
+        while (!queue.isEmpty()) { // 비어있지않으면 반복
+            int now = queue.poll(); // 하나 꺼내어
+
+            System.out.print(now + " "); // 노드 출력
+
+            for (int i : A[now]) { // 해당 노드의 인접리스트들 순회하면서
+                if (!visited[i]) { // 인접리스트들이 방문되지 않았으면
+                    visited[i] = true; // 방문배열 갱신
+                    queue.add(i); // 큐에 삽입 => FIFO
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
@@ -34,34 +68,5 @@ public class DFS_BFS_026 {
         visited = new boolean[N + 1]; // 방문배열 초기화
         BFS(start); // BFS
         System.out.println();
-    }
-
-    private static void DFS(int start) { // DFS
-        System.out.print(start + " "); // 노드 출력
-
-        visited[start] = true; // 방문배열 갱신
-        for (int i : A[start]) { // 해당 노드의 인접리스트들 순회하면서
-            if (!visited[i]) { // 인접리스트들이 방문되지 않았으면
-                DFS(i); // DFS 실행
-            }
-        }
-    }
-
-    private static void BFS(int start) { // BFS
-        Queue<Integer> queue = new LinkedList<>(); // 큐 구현
-        queue.add(start); // 해당 노드 큐에 삽입
-        visited[start] = true; // 방문배열 갱신
-
-        while (!queue.isEmpty()) { // 비어있지않으면 반복
-            int now = queue.poll(); // 하나 꺼내어
-            System.out.print(now + " "); // 노드 출력
-            for (int i : A[now]) { // 해당 노드의 인접리스트들 순회하면서
-                if (!visited[i]) { // 인접리스트들이 방문되지 않았으면
-                    visited[i] = true; // 방문배열 갱신
-                    queue.add(i); // 큐에 삽입 => FIFO
-                }
-            }
-        }
-
     }
 }
