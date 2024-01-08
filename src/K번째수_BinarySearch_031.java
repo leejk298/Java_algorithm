10b3db748cd64547272b27aea58ce4f2beb3767b
leejk298 <5552269@naver.com>
2024-01-08 13:24:27 +0900

    K번째수_BinarySearch_031

--- src/K번째수_BinarySearch_031.java
+++ src/K번째수_BinarySearch_031.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class K번째수_BinarySearch_031 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in); // 입력
 
         int N = sc.nextInt(); // 크기
