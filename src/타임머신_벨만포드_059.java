e2cf07a1504b0565aa00a0c9f3fdeb078bad84b0
leejk298 <5552269@naver.com>
2024-01-16 14:04:17 +0900

    타임머신_벨만포드_059

--- src/타임머신_벨만포드_059.java
+++ src/타임머신_벨만포드_059.java
@@ -2,15 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class 타임머신_벨만포드_059 {
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static vEdge[] edges;
+    static long[] D;
+
+    static class vEdge { // 엣지 클래스
+        int S, E, W; // 멤버 변수
+
+        // 멤버 함수
+        public vEdge(int S, int E, int W) { // 파라미터 생성자
+            this.S = S;
+            this.E = E;
+            this.W = W;
+        }
+    }
+
+    public static void init() throws IOException {  // 초기화
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
-        int N = Integer.parseInt(st.nextToken()); // 노드
-        int M = Integer.parseInt(st.nextToken()); // 엣지
+        N = Integer.parseInt(st.nextToken()); // 노드
+        M = Integer.parseInt(st.nextToken()); // 엣지
 
-        vEdge edges[] = new vEdge[M]; // 벨만포드 => 엣지리스트 필요, 크기: 엣지 개수
-        long D[] = new long[N + 1]; // 최단경로 배열, 크기: 노드 개수
+        edges = new vEdge[M]; // 벨만포드 => 엣지리스트 필요, 크기: 엣지 개수
+        D = new long[N + 1]; // 최단경로 배열, 크기: 노드 개수
 
         for (int i = 0; i <= N; i++) // 노드 개수만큼
             D[i] = Integer.MAX_VALUE; // 최단경로 무한대로 초기화
@@ -24,8 +40,12 @@ public class 타임머신_벨만포드_059 {
 
             edges[i] = new vEdge(S, E, W); // 엣지리스트 저장
         }
+    }
+
+    public static void BellmanFord() {  // 벨만포드
 
         D[1] = 0; // 시작노드 최단경로값 0으로 설정
+
         for (int i = 1; i < N; i++) { // N-1 번 실행 => 최단경로
             for (int j = 0; j < M; j++) { // 엣지 개수만큼
                 vEdge now = edges[j]; // 엣지 정보 하나 꺼내어
@@ -55,15 +75,11 @@ public class 타임머신_벨만포드_059 {
         else // 음수사이클이 있으면
             System.out.println("-1"); // -1 출력
     }
-}
 
-class vEdge { // 엣지 클래스
-    int S, E, W; // 멤버 변수
+    public static void main(String[] args) throws IOException {
 
-    // 멤버 함수
-    public vEdge(int S, int E, int W) { // 파라미터 생성자
-        this.S = S;
-        this.E = E;
-        this.W = W;
+        init(); // 초기화
+
+        BellmanFord();  // 벨만포드
     }
 }
