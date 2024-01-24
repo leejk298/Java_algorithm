cd88c5556e744de916bec724e39e944c33e180ce
leejk298 <5552269@naver.com>
2024-01-24 14:38:22 +0900

    JadenCase문자열_프로그래머스

--- src/JadenCase문자열_프로그래머스.java
+++ src/JadenCase문자열_프로그래머스.java
@@ -1,27 +1,34 @@
 public class JadenCase문자열_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution("3people unFollowed me"));
+        System.out.println(solution.solution("aBC DEF  3A2b "));
     }
 
     static class Solution {
         public String solution(String s) {
-            String answer = "";
-            String[] str= s.split(" ");
 
-            for(int i = 0; i < str.length; i++) {
+            String answer = ""; // 결과문자열
 
-                answer += str[i].substring(0, 1).toUpperCase();
-                answer += str[i].substring(1, str[i].length()).toLowerCase();
+            String[] str = s.split(" "); // 공백기준으로 문자열 나누기
+            for (int i = 0; i < str.length; i++) {  // 배열 크기만큼
+                if (str[i].equals("")) {    // 공백이 연속인 경우
+                    answer += " ";  // 공백 하나만 추가 후
 
-                answer += " ";
+                    continue;   // 건너뛰기
+                }
 
+                // 공백이 아닌경우
+                answer += str[i].substring(0, 1).toUpperCase(); // 처음만 대문자
+                answer += str[i].substring(1, str[i].length()).toLowerCase();   // 나머진 소문자
+
+                answer += " ";  // 공백 추가
             }
 
-            if(s.substring(s.length() - 1, s.length()).equals(" "))
-                return answer;
+            if (s.charAt(s.length() - 1) == ' ')    // 입력문자열 마지막이 공백이면
+                return answer;  // 그대로 리턴
 
-            return answer.substring(0, answer.length() - 1);
+            // 아니면
+            return answer.substring(0, answer.length() - 1);    // 공백 제거 후 리턴
         }
     }
 }
