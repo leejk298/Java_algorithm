8a1317d69ce584b05592620f596090463b89a9f8
leejk298 <5552269@naver.com>
2024-01-25 14:38:00 +0900

    귤고르기_프로그래머스

--- src/귤고르기_프로그래머스.java
+++ src/귤고르기_프로그래머스.java
@@ -9,28 +9,27 @@ public class 귤고르기_프로그래머스 {
     static class Solution {
         public int solution(int k, int[] tangerine) {
 
-            int answer = 0; // 결과값
+            Map<Integer, Integer> hashMap = new HashMap<>();    // 해시맵
 
-            Map<Integer, Integer> map = new HashMap<>();    // 해시맵
-            for(int i : tangerine)  // 입력배열 순회
-                map.put(i, map.getOrDefault(i, 0) + 1); // 해시맵 저장
+            for(int t : tangerine)  // 입력배열 순회
+                hashMap.put(t, hashMap.getOrDefault(t, 0) + 1); // 해시맵 저장
 
-            List<Integer> arr = new ArrayList<>();  // 결과리스트
-            for(int i : map.keySet())   // map의 key를 가져와서
-                arr.add(map.get(i));    // key에 해당하는 value 삽입
+            List<Integer> list = new ArrayList<>(); // 리스트
+            for(int key : hashMap.keySet()) // 키 값 순회
+                list.add(hashMap.get(key)); // 밸류 값 리스트에 저장
 
-            Collections.sort(arr, Collections.reverseOrder());  // 내림차순 정렬
+            Collections.sort(list, Collections.reverseOrder()); // 내림차순 정렬
 
-            int sum = 0;    // 총 합
-            for(int i : arr) {  // 리스트 순회
-                sum += i;   // 합 갱신
-                answer++;   // 개수
+            int sum = 0, answer = 0;    // 총 합, 결과값
+            for(int i : list) { // 리스트 순회
+                sum += i;   // 총합 갱신
+                answer++;   // 가짓수
 
-                if(sum >= k)    // 크면
+                if(sum >= k)    // 필요 개수 이상이면
                     break;  // for 종료
             }
 
-            return answer;  // 총 개수 리턴
+            return answer;  // 결과값 리턴
         }
     }
 }
