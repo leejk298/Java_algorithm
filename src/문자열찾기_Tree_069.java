334930e90f12ef6b28b7c0dbd7d63f51fa745be8
leejk298 <5552269@naver.com>
2024-02-05 14:43:10 +0900

    문자열찾기_Tree_069

--- src/문자열찾기_Tree_069.java
+++ src/문자열찾기_Tree_069.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
 public class 문자열찾기_Tree_069 {
+    static class tNode {
+        tNode[] next = new tNode[26]; // 알파벳 개수만큼
+        boolean isEnd; // 배열에 끝인 지 체크
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in); // 입력
 
         int N = sc.nextInt(); // 입력개수
@@ -11,7 +17,9 @@ public class 문자열찾기_Tree_069 {
 
         while (N > 0) { // 입력 개수만큼
             String str = sc.next(); // 한 줄 스트링
+
             tNode now = root; // 루트노드 설정
+
             for (int i = 0; i < str.length(); i++) { // 스트링 길이만큼
                 char ch = str.charAt(i); // 문자 하나씩 검사
                 if (now.next[ch - 'a'] == null) // 해당 문자가 없으면
@@ -26,11 +34,15 @@ public class 문자열찾기_Tree_069 {
         }
 
         int cnt = 0; // 포함돼있는 문자열 개수
+
         while (M > 0) { // 체크할 개수만큼
             String text = sc.next(); // 한 줄 스트링
+
             tNode now = root; // 루트노드로 설정
+
             for (int i = 0; i < text.length(); i++) { // 스트링 길이만큼
                 char ch = text.charAt(i); // 문자 하나씩 검사
+
                 if (now.next[ch - 'a'] == null) // 문자열이 끝나면
                     break; // 해당 문자열 검사 종료
 
@@ -44,9 +56,4 @@ public class 문자열찾기_Tree_069 {
 
         System.out.println(cnt); // 개수 출력
     }
-}
-
-class tNode {
-    tNode[] next = new tNode[26]; // 알파벳 개수만큼
-    boolean isEnd; // 배열에 끝인 지 체크
 }
