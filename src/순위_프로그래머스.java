abcd1c71ba4e2537e4747928ce4f4cf968220f6f
leejk298 <5552269@naver.com>
2024-02-07 14:03:56 +0900

    순위_프로그래머스

--- src/순위_프로그래머스.java
+++ src/순위_프로그래머스.java
@@ -8,29 +8,28 @@ public class 순위_프로그래머스 {
     static class Solution {
         public int solution(int n, int[][] results) {
 
-            // 초기화
-            int answer = 0;
-            int[][] dp = new int[n + 1][n + 1];
+            int answer = 0; // 결과값
+            int[][] dp = new int[n + 1][n + 1]; // dp 배열
 
-            for(int i = 0; i < results.length; i++) {   // 크기만큼
-                int S = results[i][0];  // 시작
-                int E = results[i][1];  // 끝
+            for(int[] result : results) {   // 입력배열 순회
+                int s = result[0], e = result[1];   // 이긴사람 s, 진 사람 e
 
-                dp[S][E] = 1;   // 이기면 1
-                dp[E][S] = -1;  // 지면 -1
+                dp[s][e] = 1;   // s -> e: 1
+                dp[e][s] = -1;  // e -> s: -1
             }
 
-            for(int k = 1; k <= n; k++) {   // 경유지 k
-                for(int i = 1; i <= n; i++) {   // 시작점부터
-                    for(int j = 1; j <= n; j++) {   // 도착점까지
-                        if(dp[i][k] == 1 && dp[k][j] == 1) {    // 경유지를 거쳐서 갈 수 있으면 => 둘 다 이기면
-                            dp[i][j] = 1;   // 순위가 정해지므로 1
-                            dp[j][i] = -1;  // 반대는 -1
+            // 벨만포드 알고리즘
+            for(int k = 1; k <= n; k++) {   // 경유지 k에 대해
+                for(int i = 1; i <= n; i++) {   // 시작점에서
+                    for(int j = 1; j <= n; j++) {   // 도착점으로
+                        if(dp[i][k] == 1 && dp[k][j] == 1) {    // 도달 가능한 경우 1번 => i가 k를 이기고, k가 j를 이기면 i가 j를 이김
+                            dp[i][j] = 1;   // i -> j: 1
+                            dp[j][i] = -1;  // j -> i: -1
                         }
 
-                        if(dp[i][k] == -1 && dp[k][j] == -1) {  // 둘 다 졌으면
-                            dp[i][j] = -1;  // 순위가 정해지므로 -1
-                            dp[j][i] = 1;   // 반대는 1
+                        if(dp[i][k] == -1 && dp[k][j] == -1) {  // 도달 가능한 경우 2번 => i가 k에게 지고, k가 j에게 지면 i가 j에게 짐
+                            dp[i][j] = -1;  // i -> j: -1
+                            dp[j][i] = 1;   // j -> i: 1
                         }
                     }
                 }
@@ -39,16 +38,15 @@ public class 순위_프로그래머스 {
             for(int i = 1; i <= n; i++) {   // 행
                 int count = 0;  // 개수
 
-                for(int j = 1; j <= n; j++) {   // 열
-                    if(dp[i][j] != 0)   // 0이 아니면 이기거나 진 거이므로
+                for(int j = 1; j <= n; j++) // 열
+                    if(dp[i][j] != 0)   // 0이 아니면 => 승부를 낸 경우
                         count++;    // 개수 카운트
-                }
 
-                if(count == n - 1)  // 자신을 제외한 n - 1개이면 순위가 정해지므로
-                    answer++;   // 결과값 카운트
+                if(count == n - 1)  // 자신을 제외한 개수가 같으면
+                    answer++;   // 순위를 정할 수 있으므로 결과값 카운트
             }
 
-            return answer;  // 총 개수 리턴
+            return answer;  // 결과값 리턴
         }
     }
 }
