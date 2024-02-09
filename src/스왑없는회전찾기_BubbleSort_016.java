8931168cf728d25e5a0f09be01b6798aad1f47e2
leejk298 <5552269@naver.com>
2024-02-09 20:09:46 +0900

    스왑없는회전찾기_BubbleSort_016

--- src/스왑없는회전찾기_BubbleSort_016.java
+++ src/스왑없는회전찾기_BubbleSort_016.java
@@ -4,7 +4,26 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class 스왑없는회전찾기_BubbleSort_016 {
+    static class mData implements Comparable<mData> { // 1. Comparable 클래스 구현 -> 2. compareTo 메소드 오버라이딩(재정의)
+        // 클래스 = 멤버변수 + 멤버함수
+        int value; // 멤버변수
+        int index;
+
+        public mData(int value, int index) { // 멤버함수: 생성자
+            super(); // Comparable 클래스 호출
+            this.value = value;
+            this.index = index;
+        }
+
+        // compareTo 메소드 오버라이딩(재정의)
+        @Override
+        public int compareTo(mData o) { // 값 비교하여 정렬
+            return this.value - o.value; // 부호로 값 비교하여 리턴(양수 - 오름차순, 음수, 0 - 내림차순)
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
 
         int N = Integer.parseInt(bf.readLine()); // 크기, 한 줄 스트링 정수로 파싱
@@ -27,20 +46,3 @@ public class 스왑없는회전찾기_BubbleSort_016 {
     }
 }
 
-class mData implements Comparable<mData> { // 1. Comparable 클래스 구현 -> 2. compareTo 메소드 오버라이딩(재정의)
-    // 클래스 = 멤버변수 + 멤버함수
-    int value; // 멤버변수
-    int index;
-
-    public mData(int value, int index) { // 멤버함수: 생성자
-        super(); // Comparable 클래스 호출
-        this.value = value;
-        this.index = index;
-    }
-
-    // compareTo 메소드 오버라이딩(재정의)
-    @Override
-    public int compareTo(mData o) { // 값 비교하여 정렬
-        return this.value - o.value; // 부호로 값 비교하여 리턴(양수 - 오름차순, 음수, 0 - 내림차순)
-    }
-}
