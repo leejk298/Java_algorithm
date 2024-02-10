0f93be60834aeafbdad33fe8c10a26f371141a92
leejk298 <5552269@naver.com>
2024-02-10 18:21:12 +0900

    괄호채우기_Greedy_036

--- src/괄호채우기_Greedy_036.java
+++ src/괄호채우기_Greedy_036.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class 괄호채우기_Greedy_036 {
+    public static int mySum(String str) {
+
+        int sum = 0;
+        String tmp[] = str.split("[+]"); // [+] 하는 이유: split() 에서 +를 잘 못읽어서 []를 붙여줌
+
+        for (int i = 0; i < tmp.length; i++)	// 배열 크기 만큼
+            sum = sum + Integer.parseInt(tmp[i]);	// 문자열을 정수로 고쳐서 더해줌
+
+        return sum;
+    }
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);	// 입력
@@ -19,16 +29,5 @@ public class 괄호채우기_Greedy_036 {
         }
 
         System.out.println(res);	// 출력
-    }
-
-    private static int mySum(String str) {
-
-        int sum = 0;
-        String tmp[] = str.split("[+]"); // [+] 하는 이유: split() 에서 +를 잘 못읽어서 []를 붙여줌
-
-        for (int i = 0; i < tmp.length; i++)	// 배열 크기 만큼
-            sum = sum + Integer.parseInt(tmp[i]);	// 문자열을 정수로 고쳐서 더해줌
-
-        return sum;
     }
 }
