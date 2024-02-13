7b1b65616b3b6c2cead9d5f24eb3898312dcab67
leejk298 <5552269@naver.com>
2024-02-13 13:19:06 +0900

    트리의지름_BFS_028

--- src/트리의지름_BFS_028.java
+++ src/트리의지름_BFS_028.java
@@ -1,14 +1,31 @@
 import java.util.*;
 
 public class 트리의지름_BFS_028 {
-    public static void main(String[] args) {
+    static int N;
+    static int[] distance;
+    static boolean[] visited;
+    static ArrayList<Edge>[] A;
+
+    static class Edge { // Edge 클래스 - 멤버변수 + 멤버함수
+        // 멤버변수
+        int e, w;
+
+        // 멤버함수
+        public Edge(int e, int w) { // 파라미터 생성자
+            this.e = e;
+            this.w = w;
+        }
+    }
+
+    public static void init() { // 초기화
 
         Scanner sc = new Scanner(System.in); // 입력
-        int N = sc.nextInt(); // 노드 개수
 
-        boolean visited[] = new boolean[N + 1]; // 방문배열
-        int distance[] = new int[N + 1]; // 거리배열
-        ArrayList<Edge>[] A = new ArrayList[N + 1]; // 노드마다 인접리스트 구현
+        N = sc.nextInt(); // 노드 개수
+
+        visited = new boolean[N + 1]; // 방문배열
+        distance = new int[N + 1]; // 거리배열
+        A = new ArrayList[N + 1]; // 노드마다 인접리스트 구현
 
         for (int i = 1; i <= N; i++) // 노드 1부터 시작 N개 만큼
             A[i] = new ArrayList<Edge>(); // 인접리스트 부착
@@ -27,23 +44,10 @@ public class 트리의지름_BFS_028 {
                 A[S].add(new Edge(E, V));
             }
         }
-
-        BFS(1, visited, distance, A); // BFS
-        int max = 1; // 1 부터 시작 -> 가장 긴 지름 찾으면 다시 BFS
-
-        for (int i = 2; i <= N; i++) // 2 부터 N 까지
-            if (distance[max] < distance[i]) // 가장 긴 지름에 해당하는 index 찾아서
-                max = i; // max로 설정
-
-        distance = new int[N + 1]; // 새로 초기화
-        visited = new boolean[N + 1];
-        BFS(max, visited, distance, A); // 가장 긴 index로 BFS 다시
-
-        Arrays.sort(distance); // 오름차순 정렬
-        System.out.println(distance[N]); // 가장 끝 index => 가장 긴 지름 출력
     }
 
-    private static void BFS(int v, boolean[] visited, int[] distance, ArrayList<Edge>[] A) {
+    public static void BFS(int v) { // BFS
+
         Queue<Integer> queue = new LinkedList<Integer>(); // BFS는 Queue로 구현 -> FIFO
 
         queue.add(v); // 처음에 들어온 노드 삽입
@@ -64,15 +68,26 @@ public class 트리의지름_BFS_028 {
             }
         }
     }
-}
 
-class Edge { // Edge 클래스 - 멤버변수 + 멤버함수
-    // 멤버변수
-    int e, w;
+    public static void main(String[] args) {
 
-    // 멤버함수
-    public Edge(int e, int w) { // 파라미터 생성자
-        this.e = e;
-        this.w = w;
+        init(); // 초기화
+
+        BFS(1); // BFS
+
+        int max = 1; // 1 부터 시작 -> 가장 긴 지름 찾으면 다시 BFS
+
+        for (int i = 2; i <= N; i++) // 2 부터 N 까지
+            if (distance[max] < distance[i]) // 가장 긴 지름에 해당하는 index 찾아서
+                max = i; // max로 설정
+
+        distance = new int[N + 1]; // 새로 초기화
+        visited = new boolean[N + 1];
+
+        BFS(max); // 가장 긴 index로 BFS 다시
+
+        Arrays.sort(distance); // 오름차순 정렬
+
+        System.out.println(distance[N]); // 가장 끝 index => 가장 긴 지름 출력
     }
 }
