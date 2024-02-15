16ccee7a5c0d2c0a8af8f6f79d0e9f8a782715f4
leejk298 <5552269@naver.com>
2024-02-15 13:34:07 +0900

    임계경로_위상정렬_055

--- src/임계경로_위상정렬_055.java
+++ src/임계경로_위상정렬_055.java
@@ -2,6 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class 임계경로_위상정렬_055 {
+    static int N, M;
+    static int start, end;
+    static int[] inDegree, res;
+    static List<wNode>[] A, reverseA;
+
     static class wNode { // wNode 클래스
         int node; // 노드
         int w; // 가중치
@@ -12,17 +17,17 @@ public class 임계경로_위상정렬_055 {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void init() throws IOException {  // 초기화
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력버퍼
 
-        int N = Integer.parseInt(bf.readLine()); // 노드
-        int M = Integer.parseInt(bf.readLine()); // 엣지
+        N = Integer.parseInt(bf.readLine()); // 노드
+        M = Integer.parseInt(bf.readLine()); // 엣지
 
-        int indegree[] = new int[N + 1]; // 진입차수
-        int res[] = new int[N + 1]; // 임계경로
-        ArrayList<wNode>[] A = new ArrayList[N + 1]; // 인접리스트
-        ArrayList<wNode>[] reverseA = new ArrayList[N + 1]; // 역방향 인접리스트
+        inDegree = new int[N + 1]; // 진입차수
+        res = new int[N + 1]; // 임계경로
+        A = new ArrayList[N + 1]; // 인접리스트
+        reverseA = new ArrayList[N + 1]; // 역방향 인접리스트
 
         for (int i = 1; i <= N; i++) { // 노드 개수만큼
             A[i] = new ArrayList<wNode>(); // 인접리스트 구현
@@ -38,33 +43,40 @@ public class 임계경로_위상정렬_055 {
 
             A[S].add(new wNode(E, W)); // 인접리스트 구현
             reverseA[E].add(new wNode(S, W));
-            indegree[E]++; // 진입차수 갱신
+            inDegree[E]++; // 진입차수 갱신
         }
 
         StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
-        int start = Integer.parseInt(st.nextToken()); // 출발 도시
-        int end = Integer.parseInt(st.nextToken()); // 도착 도시
+        start = Integer.parseInt(st.nextToken()); // 출발 도시
+        end = Integer.parseInt(st.nextToken()); // 도착 도시
+    }
+
+    public static void BFS() {
 
         Queue<Integer> queue = new LinkedList<>(); // 큐
+
         queue.add(start); // 출발 도시부터 시작하여
 
         while (!queue.isEmpty()) { // 큐가 비어있지않으면
             int now = queue.poll(); // 하나 꺼내어
 
             for (wNode next : A[now]) { // 해당 노드의 인접리스트들 순회
-                indegree[next.node]--; // 인접리스트 전부 진입차수 갱신
+                inDegree[next.node]--; // 인접리스트 전부 진입차수 갱신
                 res[next.node] = Math.max(res[next.node], res[now] + next.w); // 임계경로 갱신 => 최대값으로
 
-                if (indegree[next.node] == 0) // 진입차수가 0 이면
+                if (inDegree[next.node] == 0) // 진입차수가 0 이면
                     queue.add(next.node); // 큐에 저장
             }
         }
+    }
+
+    public static void reverseBFS() {
 
         int cnt = 0; // 도시 방문 횟수
         boolean visited[] = new boolean[N + 1]; // 방문배열
 
-        queue = new LinkedList<>(); // 큐
+        Queue<Integer> queue = new LinkedList<>(); // 큐
         queue.add(end); // 도착 도시부터 시작하여 역방향으로
         visited[end] = true; // 방문여부 갱신
         while (!queue.isEmpty()) { // 큐가 비어있지않으면
@@ -86,5 +98,14 @@ public class 임계경로_위상정렬_055 {
 
         System.out.println(res[end]); // 임계경로 값 출력
         System.out.println(cnt); // 도로 개수 출력
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init(); // 초기화
+
+        BFS();  // 시작점으로 BFS
+
+        reverseBFS();   // 도착점으로 BFS
     }
 }
