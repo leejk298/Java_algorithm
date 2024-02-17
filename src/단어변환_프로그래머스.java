7d57cb141337ea4e9e934af4c81558b5cb8d5174
leejk298 <5552269@naver.com>
2024-02-17 15:42:12 +0900

    단어변환_프로그래머스

--- src/단어변환_프로그래머스.java
+++ src/단어변환_프로그래머스.java
@@ -9,14 +9,14 @@ public class 단어변환_프로그래머스 {
         static int answer;  // 결과값, 백트래킹 후 저장해야 하므로 전역
         static boolean[] visited;   // 방문배열
         public static void DFS(int depth, String b, String t, String[] w) { // 백트래킹
-            // 베이스 케이스
-            if(b.equals(t)) {   // 같으면
+
+            if(b.equals(t)) {   // 베이스케이스: 같으면
                 answer = Math.min(answer, depth);   // 최소값 저장
 
                 return; // 함수 리턴, 완전 탐색 위해
             }
 
-            // 재귀 케이스
+            // 재귀 케이스: 같지 않으면
             for(int i = 0; i < w.length; i++) { // 문자열 크기만큼
 
                 if(!visited[i]) {   // 방문한 적이 없으면
@@ -39,8 +39,8 @@ public class 단어변환_프로그래머스 {
         public int solution(String begin, String target, String[] words) {
 
             answer = Integer.MAX_VALUE; // 최대값으로 초기화, 최소값 찾아야하므로
-
             boolean flag = false;   // 비교하기 위해
+
             for(String w : words)   // 입력 배열 순회
                 if(w.equals(target))    // 같은게 있으면
                     flag = true;    // true
@@ -50,7 +50,6 @@ public class 단어변환_프로그래머스 {
 
             // true 인 경우
             visited = new boolean[words.length];    // 방문배열 선언
-
             DFS(0, begin, target, words);   // DFS
 
             return answer;  // 최소 변환 횟수 출력
