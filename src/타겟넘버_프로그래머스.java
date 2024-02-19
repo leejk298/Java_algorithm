7875d85dad7e6e814a2b186456499d64a699fbaa
leejk298 <5552269@naver.com>
2024-02-19 14:09:20 +0900

    타겟넘버_프로그래머스

--- src/타겟넘버_프로그래머스.java
+++ src/타겟넘버_프로그래머스.java
@@ -5,24 +5,29 @@ public class 타겟넘버_프로그래머스 {
     }
 
     static class Solution {
-        static int answer = 0;  // 초기화, 전역변수로 => DFS 안에서 처리 필요
-        public int solution(int[] numbers, int target) {
+        static int answer;  // 결과값
 
-            DFS(numbers, 0, target, 0); // 백트래킹
+        public static void DFS(int[] numbers, int depth, int target, int sum) {
 
-            return answer;  // 결과값 리턴
-        }
-        static void DFS(int[] numbers, int depth, int target, int sum) {    // 백트래킹
-            // 베이스케이스
-            if (depth == numbers.length) {   // 마지막 노드, 깊이 + 1 이 배열의 크기가 되면
-                if (target == sum)   // 조건이 맞으면
+            if (depth == numbers.length) {   // 베이스케이스: 깊이가 길이와 같아지면
+                if (sum == target)   // 총합이 타겟넘버와 같으면
                     answer++;   // 개수 카운트
 
-                return;
+                return; // 완전 탐색하기 위해 리턴
             }
-            // 재귀케이스
-            DFS(numbers, depth + 1, target, sum + numbers[depth]);  // 덧셈, 깊이를 인덱스로
-            DFS(numbers, depth + 1, target, sum - numbers[depth]);  // 뺄셈
+
+            // 재귀케이스: 깊이가 길이와 다르면
+            DFS(numbers, depth + 1, target, sum + numbers[depth]);  // 재귀콜, +
+            DFS(numbers, depth + 1, target, sum - numbers[depth]);  // 재귀콜, -
+        }
+
+        public int solution(int[] numbers, int target) {
+
+            answer = 0; // 결과값 초기화
+
+            DFS(numbers, 0, target, 0); // DFS
+
+            return answer;  // 결과값 리턴
         }
     }
 }
