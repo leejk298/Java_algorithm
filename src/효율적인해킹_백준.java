e68f8c141425be821cb24e177fcd97b83135a75c
leejk298 <5552269@naver.com>
2024-02-27 14:37:29 +0900

    효율적인해킹_백준

--- src/효율적인해킹_백준.java
+++ src/효율적인해킹_백준.java
@@ -11,10 +11,9 @@ import java.io.*;
 
 public class 효율적인해킹_백준 {
     static int N, M;    // 크기
-    static int[] arr;   // 해킹 가능 배열
-    static ArrayList<Integer>[] A;  // 인접리스트
+    static int[] arr;   // 결과배열
+    static List<Integer>[] A;   // 인접리스트
     static boolean[] visited;   // 방문배열
-    static StringBuilder sb;    // 결과문자열
 
     public static void init() throws IOException {  // 초기화
 
@@ -25,64 +24,65 @@ public class 효율적인해킹_백준 {
         M = Integer.parseInt(st.nextToken());   // 엣지 개수
 
         // 초기화
+        arr = new int[N + 1];
         A = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++)
-            A[i] = new ArrayList<>();
 
-        for(int i = 0; i < M; i++) {    // 엣지 개수만큼
+        for (int i = 1; i <= N; i++)    // 정점 개수만큼
+            A[i] = new ArrayList<>();   // 인접리스트 구현
+
+        for (int i = 0; i < M; i++) {   // 엣지 개수만큼
             st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            int S = Integer.parseInt(st.nextToken());   // 시작
-            int E = Integer.parseInt(st.nextToken());   // 도착
+            int s = Integer.parseInt(st.nextToken());   // 시작
+            int e = Integer.parseInt(st.nextToken());   // 도착
 
-            A[S].add(E);    // 단반향
+            A[s].add(e);    // 단방향
         }
     }
 
-    public static void DFS(int v) { // DFS
+    public static void BFS(int v) { // BFS
 
-        if(visited[v])  // 방문한 적이 있으면
-            return; // 건너뛰기
+        Queue<Integer> queue = new LinkedList<>();  // 큐
 
-        // 방문한 적이 없으면
+        queue.offer(v); // 시작점 큐에 삽입
         visited[v] = true;  // 방문
 
-        for(int i = 0; i < A[v].size(); i++) {  // 인접리스트 크기만큼
-            int next = A[v].get(i); // 다음 노드
+        while (!queue.isEmpty()) {  // 큐가 비어있지 않으면
+            int now = queue.poll(); // 하나 꺼내어
 
-            if(!visited[next]) {    // 방문한 적이 없으면
-                arr[next]++;    // 해킹가능 카운트
-                DFS(next);  // 재귀콜, DFS
+            for (int next : A[now]) {   // 해당 정점의 인접리스트 순회
+                if (!visited[next]) {   // 다음 정점을 방문한 적이 없으면
+                    visited[next] = true;   // 방문
+                    arr[next]++;    // 카운트
+                    queue.offer(next);  // 큐에 삽입
+                }
             }
         }
     }
 
-    public static void findMaxCount() { // 가능한 최대 해킹 개수
+    public static void findMaxCount() { // 최대 개수 찾기
 
-        arr = new int[N + 1];   // 초기화
-
-        for(int i = 1; i <= N; i++) {   // 정점 개수만큼
+        for (int i = 1; i <= N; i++) {  // 정점 개수만큼
             visited = new boolean[N + 1];   // 방문배열 초기화
-            DFS(i); // DFS
+            BFS(i); // BFS
         }
 
         int max = 0;    // 최대값
-        for(int i = 1; i <= N; i++) // 정점 개수만큼
-            if(max < arr[i])    // 최대값 구하기
-                max = arr[i];
+        for (int i = 1; i <= N; i++)    // 정점 개수만큼
+            max = Math.max(max, arr[i]);    // 최대값 찾기
 
-        sb = new StringBuilder();   // 결과 문자열 만들기
-        for(int i = 1; i <= N; i++) // 정점 개수만큼
-            if(arr[i] == max)   // 최대값과 같으면
-                sb.append(i + " "); // 결과 저장
+        StringBuilder sb = new StringBuilder(); // 결과 문자열
+        for (int i = 1; i <= N; i++)    // 정점 개수만큼
+            if (max == arr[i])  // 최대값과 같으면
+                sb.append(i + " "); // 문자열에 추가
+
+        System.out.println(sb); // 결과 문자열 출력
     }
 
     public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
-        findMaxCount(); // 최대 개수 찾기
-
-        System.out.println(sb); // 결과 출력
+        findMaxCount(); // 가장 많은 컴퓨터를 해킹할 수 있는 번호
     }
 }
