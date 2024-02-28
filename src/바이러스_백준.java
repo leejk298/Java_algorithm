187e03b90a50a2417964234f367a4778e588e675
leejk298 <5552269@naver.com>
2024-02-28 14:05:04 +0900

    바이러스_백준

--- src/바이러스_백준.java
+++ src/바이러스_백준.java
@@ -17,6 +17,7 @@ public class 바이러스_백준 {
     static boolean[] visited;   // 방문배열
 
     public static void init() { // 초기화
+
         Scanner sc = new Scanner(System.in);    // 입력
 
         N = sc.nextInt();   // 노드
@@ -48,6 +49,7 @@ public class 바이러스_백준 {
     }
 
     public static int countVirus() {   // 바이러스 개수
+
         int count = 0;  // 개수
 
         for(int i = 2; i <= N; i++) // 2부터
