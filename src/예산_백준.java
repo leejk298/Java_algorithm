c9011d7549f81a30222182d6470e22a3d8fbb1c6
leejk298 <5552269@naver.com>
2024-03-05 14:44:48 +0900

    예산_백준

--- src/예산_백준.java
+++ src/예산_백준.java
@@ -14,41 +14,41 @@ public class 예산_백준 {
     public static void init() throws IOException {  // 초기화
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());     // 한 줄 스트링
 
-        N = Integer.parseInt(bf.readLine());    // 크기
+        N = Integer.parseInt(st.nextToken());   // 크기
 
-        // 초기화
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+        arr = new int[N];   // 입력배열
+
+        st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
         for(int i = 0; i < N; i++)  // 크기만큼
             arr[i] = Integer.parseInt(st.nextToken());  // 입력배열 저장
 
-        M = Integer.parseInt(bf.readLine());    // 예산
+        st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+        M = Integer.parseInt(st.nextToken());   // 예산
     }
 
-    public static void BinarySearch() { // 이분 탐색
+    public static void BinarySearch() { // 이분탐색
 
-        Arrays.sort(arr);   // 오름차순 정렬, 최악의 경우를 구하기 위해
+        Arrays.sort(arr);   // 오름차순 정렬
 
-        int S = 1, E = arr[N - 1];  // 인덱스, 최선의 경우: 1, 최악의 경우
-        int res = 0;    // 결과값
-
-        while(S <= E) { // 역전이 아니면 반복
-            int mid = (S + E) / 2;  // 중앙값
+        int s = 1, e = arr[N - 1], res = 0; // 인덱스, 결과값
+        while(s <= e) { // 역전이 아니면
+            int mid = (s + e) / 2;  // 중앙값
 
             int sum = 0;    // 총 합
-            for(int i : arr) {  // 배열 순회
-                if (mid < i)    // 크면
-                    sum += mid; // 상한가 더하고
-                else    // 작거나 같으면
-                    sum += i;   // 그대로 더함
+            for(int num : arr) {    // 입력배열 순회
+                if(mid < num)   // 중앙값보다 크면
+                    sum += mid; // 중앙값을 더해주고
+                else    // 아니면
+                    sum += num; // 해당값을 더해줌
             }
 
-            if(sum <= M) {  // 기준값보다 작거나 같으면
-                res = Math.max(res, mid);   // 최대값을 저장
-                S = mid + 1;    // 더 커질 수 있게 시작 인덱스 갱신
-            } else  // 크면
-                E = mid - 1;    // 더 작아져야 하므로 끝 인덱스 갱신
+            if(sum <= M) {  // 총 합이 예산보다 작거나 같으면
+                res = Math.max(res, mid);   // 중앙값 중 최대값 저장
+                s = mid + 1;    // 시작인덱스 갱신
+            } else  // 아니면
+                e = mid - 1;    // 끝인덱스 갱신
         }
 
         System.out.println(res);    // 결과값 출력
