6a4fe75289a5baf7feac44e631ca87edff49d45d
leejk298 <5552269@naver.com>
2024-03-06 12:18:08 +0900

    최소힙_백준

--- src/최소힙_백준.java
+++ src/최소힙_백준.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*
@@ -14,25 +15,31 @@ import java.util.*;
  */
 
 public class 최소힙_백준 {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        int N = Integer.parseInt(st.nextToken());    // 크기
 
-        int N = sc.nextInt();   // 크기
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  // 우선순위 큐
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();    // 우선순위 큐, 오름차순 -> 최소힙
+        StringBuilder sb = new StringBuilder(); // 결과 문자열
 
         for(int i = 0; i < N; i++) {    // 크기만큼
-            int num = sc.nextInt(); // 숫자
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            if(num == 0) {  // 0 이면
-                if(pq.isEmpty())    // 우선순위 큐가 비어있으면
-                    System.out.println(0);  // 0 출력
-                else    // 비어있지 않으면
-                    System.out.println(pq.poll());  // 최소값 -> 루트값 출력
+            int num = Integer.parseInt(st.nextToken()); // 숫자
+
+            if(num == 0) {  // 0이면
+                if(!pq.isEmpty())   // 큐가 비어있지 않으면
+                    sb.append(pq.poll() + "\n");    // 최소값 하나 꺼내서 결과 문자열에 저장
+                else    // 비어있으면
+                    sb.append(0 + "\n");    // 0 저장
             } else  // 0이 아니면
-                pq.offer(num);  // 우선순위 큐에 추가 -> 오름차순 정렬
+                pq.offer(num);  // 큐에 숫자 추가
         }
+
+        System.out.print(sb);   // 결과 문자열 출력
     }
 }
