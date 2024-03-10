de60d2b7275dfb1fdddc2e00c0a6aef1f60a32e9
leejk298 <5552269@naver.com>
2024-03-10 13:04:48 +0900

    빗물_백준

--- src/빗물_백준.java
+++ src/빗물_백준.java
@@ -7,35 +7,36 @@ import java.io.*;
  */
 
 public class 빗물_백준 {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        int H = Integer.parseInt(st.nextToken());   // 높이
-        int W = Integer.parseInt(st.nextToken());   // 너비
+        int N = Integer.parseInt(st.nextToken());   // 세로
+        int M = Integer.parseInt(st.nextToken());   // 가로
 
-        int[] arr = new int[W]; // 입력배열
+        int[] arr = new int[M]; // 입력배열 초기화
 
         st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
-        for(int i = 0; i < W; i++)  // 너비만큼
+
+        for(int i = 0; i < M; i++)  // 가로
             arr[i] = Integer.parseInt(st.nextToken());  // 입력배열 저장
 
-        int sum = 0;    // 빗물
-        for(int i = 1; i < W - 1; i++) {    // 처음과 마지막은 빗물이 찰 수 없음
-            int l = 0, r = 0;   // 왼, 오른쪽 인덱스
+        int res = 0;    // 결과값 초기화
+
+        for(int i = 1; i < M - 1; i++) {    // 가장자리 제외한 구간에 대해
+            int left = 0, right = 0;    // 각 구간의 왼, 오른쪽 값 초기화
 
             for(int j = 0; j < i; j++)  // 왼쪽
-                l = Math.max(l, arr[j]);    // 최대값
+                left = Math.max(left, arr[j]);  // 최대값
 
-            for(int j = i + 1; j < W; j++)  // 오른쪽
-                r = Math.max(r, arr[j]);    // 최대값
+            for(int j = i + 1; j < M; j++)  // 오른쪽
+                right = Math.max(right, arr[j]);    // 최대값
 
-            if(arr[i] < l && arr[i] < r)    // 잠기면
-                sum += Math.min(l, r) - arr[i]; // 잠기는 양 = 최소값 - 해당값
+            if(arr[i] < left && arr[i] < right) // 해당 값이 왼, 오른쪽 값보다 작으면
+                res += Math.min(left, right) - arr[i];  //  빗물의 양 갱신
         }
 
-        System.out.println(sum);    // 총량 출력
+        System.out.println(res);    // 결과값 출력
     }
 }
