5b3b2ceed423cd0419935fd52f7854b6cba5fa6f
leejk298 <5552269@naver.com>
2024-03-10 12:50:53 +0900

    숨바꼭질3_백준

--- src/숨바꼭질3_백준.java
+++ src/숨바꼭질3_백준.java
@@ -1,58 +1,63 @@
 import java.util.*;
+import java.io.*;
 
 /*
 5 17
  */
 
 public class 숨바꼭질3_백준 {
-    static int N, T, res;   // 크기, 결과값
-    static final int max = 100000;  // 최대 100,000
+    static int N, K, res;   // 입력값, 결과값
+    static final int max = 100001;  // 최대 구간값
     static boolean[] visited;   // 방문배열
 
     static class Node { // 노드 클래스
-        int data, count;    // 숫자, 횟수
+        int num, count; // 값, 횟수
 
-        public Node(int data, int count) {  // 생성자
-            this.data = data;
+        public Node(int num, int count) {   // 파라미터 생성자
+            this.num = num;
             this.count = count;
         }
     }
 
-    public static void init() { // 초기화
+    public static void init() throws IOException {  // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        N = sc.nextInt();   // 주어진 숫자
-        T = sc.nextInt();   // 타겟 숫자
-        res = Integer.MAX_VALUE;    // 최소값 찾기위해 최대로 초기화
+        N = Integer.parseInt(st.nextToken());   // 수빈
+        K = Integer.parseInt(st.nextToken());   // 동생
 
-        visited = new boolean[max + 1]; // 초기화
+        visited = new boolean[max]; // 방문배열 초기화
+        res = Integer.MAX_VALUE;    // 결과값을 최대값으로 초기화
     }
 
     public static void BFS() {  // BFS
 
         Queue<Node> queue = new LinkedList<>(); // 큐
-        queue.offer(new Node(N, 0));    // 시작점 큐에 삽입
+
+        queue.offer(new Node(N, 0));    // 큐에 시작점 삽입
 
         while(!queue.isEmpty()) {   // 큐가 비어있지 않으면
             Node now = queue.poll();    // 하나 꺼내어
-            visited[now.data] = true;   // 방문여부 갱신
 
-            if(now.data == T)   // 찾으면
-                res = Math.min(res, now.count); // 최소값으로 설정
+            int nowNum = now.num, nowCount = now.count; // 현재 값과 횟수
+            visited[nowNum] = true; // 방문 여부 갱신
 
-            if(now.data * 2 <= max && !visited[now.data * 2])   // 2배
-                queue.offer(new Node(now.data * 2, now.count)); // 순간이동
+            if(nowNum == K) // 동생을 찾으면
+                res = Math.min(res, nowCount);  // 최소값 저장
 
-            if(now.data + 1 <= max && !visited[now.data + 1])   // + 1
-                queue.offer(new Node(now.data + 1, now.count + 1)); // 횟수 + 1
+            if(nowNum * 2 < max && !visited[nowNum * 2])    // 순간이동
+                queue.offer(new Node(nowNum * 2, nowCount));    // 2배, 횟수는 그대로
 
-            if(now.data - 1 >= 0 && !visited[now.data - 1]) // - 1
-                queue.offer(new Node(now.data - 1, now.count + 1));
+            if(nowNum + 1 < max && !visited[nowNum + 1])    // 걷기 + 1
+                queue.offer(new Node(nowNum + 1, nowCount + 1));
+
+            if(nowNum - 1 >= 0 && !visited[nowNum - 1]) // 걷기 - 1
+                queue.offer(new Node(nowNum - 1, nowCount + 1));
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
