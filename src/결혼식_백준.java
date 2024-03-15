7d341dc956dc4f9263e18101dd46db877858e623
leejk298 <5552269@naver.com>
2024-03-15 15:14:27 +0900

    결혼식_백준

--- src/결혼식_백준.java
+++ src/결혼식_백준.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*
@@ -15,25 +16,30 @@ public class 결혼식_백준 {
     static List<Integer>[] A;   // 인접리스트
     static boolean[] visited;   // 방문배열
 
-    public static void init() { // 초기화
+    public static void init() throws IOException { // 초기화
 
-        Scanner sc = new Scanner(System.in);    // 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        N = sc.nextInt();   // 노드
-        M = sc.nextInt();   // 엣지
+        N = Integer.parseInt(st.nextToken());   // 정점 개수
 
-        // 초기화
-        A = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
+        st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-        for(int i = 1; i <= N; i++)
+        M = Integer.parseInt(st.nextToken());   // 엣지 개수
+
+        A = new ArrayList[N + 1];   // 인접리스트
+        visited = new boolean[N + 1];   // 방문배열
+
+        for (int i = 1; i <= N; i++)    // 정점 개수만큼
             A[i] = new ArrayList<>();   // 인접리스트 구현
 
-        for(int i = 0; i < M; i++) {
-            int s = sc.nextInt();   // 시작
-            int e = sc.nextInt();   // 끝
+        for (int i = 0; i < M; i++) {   // 엣지 개수만큼
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
-            A[s].add(e);    // 무방향
+            int s = Integer.parseInt(st.nextToken());   // 시작
+            int e = Integer.parseInt(st.nextToken());   // 도착
+
+            A[s].add(e);    // 양방향
             A[e].add(s);
         }
     }
@@ -42,27 +48,26 @@ public class 결혼식_백준 {
 
         visited[v] = true;  // 방문여부 갱신
 
-        if(depth == 2)  // 친구의 친구까지
+        if (depth == 2)  // 친구의 친구까지
             return;
 
-        for(int i = 0; i < A[v].size(); i++) {  // 인접리스트 개수만큼
-            int next = A[v].get(i); // 다음 노드
-            DFS(next, depth + 1);   // DFS
-        }
+        for (int next : A[v])   // 인접리스트 순회
+            if (!visited[next]) // 방문한 적이 없으면
+                DFS(next, depth + 1);   // DFS
     }
 
     public static void printIsInvite() {    // 초대가능한 친구
 
         int count = 0;  // 개수
 
-        for(int i = 2; i <= N; i++) // 2번부터
-            if(visited[i])  // 방문했으면
+        for (int i = 2; i <= N; i++) // 2번부터
+            if (visited[i])  // 방문했으면
                 count++;    // 개수 카운트
 
         System.out.println(count);  // 출력
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         init(); // 초기화
 
