0a46d2cfc32e1176452a727a288e6c6a68ea567b
leejk298 <5552269@naver.com>
2024-03-16 15:33:47 +0900

    회사에있는사람_백준

--- src/회사에있는사람_백준.java
+++ src/회사에있는사람_백준.java
@@ -10,16 +10,16 @@ Artem enter
  */
 
 public class 회사에있는사람_백준 {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+
         int N = Integer.parseInt(bf.readLine());    // 크기
+
         Set<String> hashSet = new HashSet<>();  // 해시셋
 
         for(int i = 0; i < N; i++) {    // 크기만큼
             String[] str = bf.readLine().split(" ");    // 입력 문자열
-
             String name = str[0];   // 이름
 
             if(!hashSet.contains(name)) // 해시셋에 포함되어있지 않으면
