00b35cff6f22e5cea653b3dcb87bd8750ea23394
leejk298 <5552269@naver.com>
2024-03-30 16:39:40 +0900

    최소비용_플로이드워셜_061

--- src/최소비용_플로이드워셜_061.java
+++ src/최소비용_플로이드워셜_061.java
@@ -2,14 +2,17 @@ import java.util.*;
 import java.io.*;
 
 public class 최소비용_플로이드워셜_061 {
-    public static void main(String[] args) throws IOException {
+    static int N, M;    // 크기
+    static int[][] A;   // 입력배열
+
+    public static void init() throws IOException {  // 초기화
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
-        StringTokenizer st;
 
-        int N = Integer.parseInt(bf.readLine()); // 노드
-        int M = Integer.parseInt(bf.readLine()); // 엣지
-        int A[][] = new int[N + 1][N + 1]; // 인접행렬 => 플로이드워셜은 N 이 작아야 쓸 수 있으므로
+        N = Integer.parseInt(bf.readLine()); // 노드
+        M = Integer.parseInt(bf.readLine()); // 엣지
+
+        A = new int[N + 1][N + 1]; // 인접행렬 => 플로이드워셜은 N 이 작아야 쓸 수 있으므로
 
         for (int i = 1; i <= N; i++) { // 노드 개수만큼
             for (int j = 1; j <= N; j++) { // 2차 배열
@@ -19,6 +22,8 @@ public class 최소비용_플로이드워셜_061 {
                     A[i][j] = 1000001;
             }
         }
+
+        StringTokenizer st;
 
         for (int i = 0; i < M; i++) { // 엣지 개수만큼
             st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
@@ -30,8 +35,10 @@ public class 최소비용_플로이드워셜_061 {
             if (A[S][E] > W) // 이미 가중치가 있다면 작은 수로 설정
                 A[S][E] = W;
         }
+    }
 
-        // 플로이드워셜 알고리즘 로직, 성능: O(V^3) => V: 노드 개수
+    public static void FloydWarshall() { // 플로이드워셜 알고리즘 로직, 성능: O(V^3) => V: 노드 개수
+
         for (int k = 1; k <= N; k++) // 경유지 K에 대해
             for (int i = 1; i <= N; i++) // 출발노드 S에 대해
                 for (int j = 1; j <= N; j++) // 도착노드 E에 대해
@@ -48,5 +55,12 @@ public class 최소비용_플로이드워셜_061 {
 
             System.out.println(); // 개행문자 출력
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init(); // 초기화
+
+        FloydWarshall();    // 플로이드워셜 알고리즘
     }
 }
