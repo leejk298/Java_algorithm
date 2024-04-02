13a24433fdda4a266c559d6c40274304df153ea6
leejk298 <5552269@naver.com>
2024-04-02 21:21:02 +0900

    중복조합

--- src/중복조합.java
+++ src/중복조합.java
@@ -1,10 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class 중복조합 {
-    public static int N;    // 총 개수
-    public static int M;    // 뽑을 개수
-    public static int A[];  // 주어진 숫자배열
-    public static int res[]; // 결과배열
+    static int N;    // 총 개수
+    static int M;    // 뽑을 개수
+    static int A[];  // 주어진 숫자배열
+    static int res[]; // 결과배열
+
+    public static void dfs(int v, int w) {   // dfs
+
+        if (v == M) {   // 뽑을 개수와 같으면
+            for (int i : res) // 배열 res 를 i로 순회하면서
+                System.out.print(i);  // 출력
+            System.out.println();   // 개행문자 출력
+        } else {    // 뽑을 개수보다 작으면
+            for (int i = w; i < N; i++) {   // 주어진 숫자들 개수만큼
+                res[v] = A[i];   // 결과배열에 저장
+                dfs(v + 1, i); // 개수 + 1, 다음 뽑을 숫자 - 중복 가능하므로 그대로
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -19,21 +33,5 @@ public class 중복조합 {
             A[i] = sc.nextInt();
 
         dfs(0, 0); // v: 뽑을 개수 count, w: 뽑을 숫자 => A[index]
-    }
-
-    private static void dfs(int v, int w) {   // dfs
-
-        if (v == M) {   // 뽑을 개수와 같으면
-            for (int i : res) { // 배열 res 를 i로 순회하면서
-                System.out.print(i);  // 출력
-            }
-
-            System.out.println();   // 개행문자 출력
-        } else {    // 뽑을 개수보다 작으면
-            for (int i = w; i < N; i++) {   // 주어진 숫자들 개수만큼
-                res[v] = A[i];   // 결과배열에 저장
-                dfs(v + 1, i); // 개수 + 1, 다음 뽑을 숫자 - 중복 가능하므로 그대로
-            }
-        }
     }
 }
