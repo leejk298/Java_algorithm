f2623b2556e3cb844701cb79d1eba7c4984457e9
leejk298 <5552269@naver.com>
2024-04-06 15:55:35 +0900

    소수찾기_DFS_024

--- src/소수찾기_DFS_024.java
+++ src/소수찾기_DFS_024.java
@@ -1,25 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class 소수찾기_DFS_024 {
     static int N;	// 정적 변수
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);	// 입력
+    public static void DFS(int num, int jarisu) {
 
-        N = sc.nextInt();	// 자릿수
-
-        // 2, 3, 5, 7은 1의 자리에서 소수이므로
-        DFS(2, 1);	// 2 -> 2x -> 2xx -> 2xxx 전부 소수인 숫자 찾기
-        DFS(3, 1);
-        DFS(5, 1);
-        DFS(7, 1);
-    }
-
-    private static void DFS(int num, int jarisu) {
         if (jarisu == N) {	// 자릿수가 같으면
-            if (is_prime(num)) {	// 소수이면
+            if (is_prime(num))	// 소수이면
                 System.out.println(num);	// 출력
-            }
 
             return;	// 소수가 아니면 리턴
         }
@@ -33,11 +21,25 @@ public class 소수찾기_DFS_024 {
         }
     }
 
-    private static boolean is_prime(int num) {	// 소수 체크
+    public static boolean is_prime(int num) {	// 소수 체크
+
         for (int i = 2; i <= num / 2; i++)
             if (num % i == 0)
                 return false;
 
         return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);	// 입력
+
+        N = sc.nextInt();	// 자릿수
+
+        // 2, 3, 5, 7은 1의 자리에서 소수이므로
+        DFS(2, 1);	// 2 -> 2x -> 2xx -> 2xxx 전부 소수인 숫자 찾기
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
     }
 }
