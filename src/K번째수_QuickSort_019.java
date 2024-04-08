8c71fcc805a0df9a5b3dc6f0df1bb5f47b66d1d0
leejk298 <5552269@naver.com>
2024-04-08 21:52:19 +0900

    K번째수_QuickSort_019

--- src/K번째수_QuickSort_019.java
+++ src/K번째수_QuickSort_019.java
@@ -4,19 +4,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class K번째수_QuickSort_019 {
+    static int N, K;    // 크기, K번째 수
+    static int[] A; // 입력배열
 
-    public static void quickSort(int[] A, int S, int E, int K) { // 퀵 정렬
+    public static void init() throws IOException {  // 초기화
 
-        if (S < E) {
-            int pivot = partition(A, S, E); // 분할
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
 
-            if (pivot == K) // K번째 수 출력
-                return;
-            else if (pivot < K) // LT
-                quickSort(A, pivot + 1, E, K);
-            else // GT
-                quickSort(A, S, pivot - 1, K);
-        }
+        N = Integer.parseInt(st.nextToken()); // 크기
+        K = Integer.parseInt(st.nextToken()); // K번째 수
+        A = new int[N]; // 입력배열
+
+        st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
+
+        for (int i = 0; i < N; i++) // 크기만큼
+            A[i] = Integer.parseInt(st.nextToken());    // 입력배열 저장
+    }
+
+    public static void swap(int[] A, int S, int E) { // 스왑
+
+        int tmp = A[S]; // 스왑변수 tmp
+        A[S] = A[E];
+        A[E] = tmp;
     }
 
     public static int partition(int[] A, int S, int E) { // 분할
@@ -29,8 +39,9 @@ public class K번째수_QuickSort_019 {
         }
 
         // 세 개 이상인 경우
-        int mid = (S + E) / 2; // 중앙값을 pivot으로
-        swap(A, S, mid); // pivot을 숨기기 위해 맨 앞으로 이동
+        int mid = (S + E) / 2; // 중앙값을 피벗으로
+
+        swap(A, S, mid); // 피벗을 숨기기 위해 맨 앞으로 이동
 
         int pivot = A[S];
         int i = S + 1, j = E; // i, j 인덱스 설정
@@ -46,31 +57,29 @@ public class K번째수_QuickSort_019 {
                 swap(A, i++, j--); // 스왑
         }
 
-        A[S] = A[j]; // pivot을 맞는 위치로 이동
+        A[S] = A[j]; // 피벗을 맞는 위치로 이동
         A[j] = pivot;
 
         return j; // pivot 값 리턴
     }
 
-    public static void swap(int[] A, int S, int E) { // 스왑
+    public static void quickSort(int[] A, int S, int E, int K) { // 퀵 정렬
 
-        int tmp = A[S];
-        A[S] = A[E];
-        A[E] = tmp;
+        if (S < E) {
+            int pivot = partition(A, S, E); // 분할
+
+            if (pivot == K) // K번째 수 출력
+                return;
+            else if (pivot < K) // LT
+                quickSort(A, pivot + 1, E, K);
+            else // GT
+                quickSort(A, S, pivot - 1, K);
+        }
     }
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입력 버퍼
-        StringTokenizer st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
-
-        int N = Integer.parseInt(st.nextToken()); // 크기
-        int K = Integer.parseInt(st.nextToken()); // K번째 수
-        int A[] = new int[N]; // 배열
-
-        st = new StringTokenizer(bf.readLine()); // 한 줄 스트링
-        for (int i = 0; i < N; i++) // 배열 저장
-            A[i] = Integer.parseInt(st.nextToken());
+        init(); // 초기화
 
         quickSort(A, 0, N - 1, K - 1); // 퀵 정렬, 기대실행시간: O(NlogN), 최악실행시간: O(N^2)
 
