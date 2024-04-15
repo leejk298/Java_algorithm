a8f4969e6bed96d282881466d2e14b2d5973fccf
leejk298 <5552269@naver.com>
2024-04-15 18:38:25 +0900

    최솟값구하기_Deque_010

--- src/최솟값구하기_Deque_010.java
+++ src/최솟값구하기_Deque_010.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class 최솟값구하기_Deque_010 {
 
