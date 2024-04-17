0da0e4ed1a9c32160b7ad0182c65aec349fc5234
leejk298 <5552269@naver.com>
2024-04-17 19:21:56 +0900

    다리놓기_조합_079

--- src/다리놓기_조합_079.java
+++ src/다리놓기_조합_079.java
@@ -1,8 +1,11 @@
 import java.util.*;
+import java.io.*;
 
 public class 다리놓기_조합_079 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // 입력
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
+        StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
         long D[][] = new long[30][30]; // 0 < N <= M < 30, 29까지 포함
         for (int i = 1; i < 30; i++) { // 1부터 29까지
@@ -14,10 +17,13 @@ public class 다리놓기_조합_079 {
             for (int j = 2; j < i; j++) // 2 ~, 선택 수가 총 개수보다 클 수 없음
                 D[i][j] = D[i - 1][j - 1] + D[i - 1][j]; // 조합 점화식
 
-        int T = sc.nextInt(); // 테스트 개수
+        int T = Integer.parseInt(st.nextToken()); // 테스트 개수
+
         for (int i = 0; i < T; i++) { // 개수만큼
-            int N = sc.nextInt(); // 선택
-            int M = sc.nextInt(); // 총 개수
+            st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
+
+            int N = Integer.parseInt(st.nextToken()); // 선택
+            int M = Integer.parseInt(st.nextToken()); // 총 개수
 
             System.out.println(D[M][N]); // 출력
         }
