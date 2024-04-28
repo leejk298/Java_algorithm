96779f1140100777035449a4e3361038b3d6a6cc
leejk298 <5552269@naver.com>
2024-04-28 22:35:50 +0900

    문자열압축_프로그래머스

--- src/문자열압축_프로그래머스.java
+++ src/문자열압축_프로그래머스.java
@@ -9,9 +9,9 @@ public class 문자열압축_프로그래머스 {
 
             int answer = s.length();    // 최초 길이로 초기화
             int count = 1;  // 자신 포함
+            StringBuilder sb = new StringBuilder(); // 문자열 만들기
 
             for (int i = 1; i <= s.length() / 2; i++) {  // 절반만 비교
-                StringBuilder sb = new StringBuilder(); // 문자열 만들기
                 String first = s.substring(0, i);   // 압축 레벨만큼, 비교할 문자열
 
                 for (int j = i; j <= s.length(); j += i) {  // 비교될 문자열
@@ -21,9 +21,8 @@ public class 문자열압축_프로그래머스 {
                     if (first.equals(second))    // 같으면 카운트세기
                         count++;
                     else {  // 다르면
-                        if (count >= 2) {    // 이전에 같았으면
+                        if (count >= 2)    // 이전에 같았으면
                             sb.append(count);   // 같은 개수만큼 문자열에 추가
-                        }
 
                         sb.append(first);   // 비교할 문자열 추가
                         first = second;     // 비교할 문자열 갱신
