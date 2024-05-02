7fa174d08b3185632a86c4d661386bfbd2bb2e57
leejk298 <5552269@naver.com>
2024-05-02 12:26:51 +0900

    연속된부분수열의합_프로그래머스

--- src/연속된부분수열의합_프로그래머스.java
+++ src/연속된부분수열의합_프로그래머스.java
@@ -3,42 +3,39 @@ import java.util.*;
 public class 연속된부분수열의합_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.solution(new int[] {1, 1, 1, 2, 3, 4, 5}, 5)));
+        System.out.println(Arrays.toString(solution.solution(new int[]{1, 1, 1, 2, 3, 4, 5}, 5)));
     }
 
     static class Solution {
-        private static List<int[]> list;    // 결과 리스트
         public int[] solution(int[] sequence, int k) {
-            list = new ArrayList<>();
 
-            int[] sum = new int[sequence.length + 1];   // 합배열
+            List<int[]> list = new ArrayList<>();   // 결과리스트
+            int[] sumArr = new int[sequence.length + 1];    // 합배열
 
-            for(int i = 1; i < sum.length; i++)     // 누적합
-                sum[i] = sum[i - 1] + sequence[i - 1];
+            for (int i = 1; i < sumArr.length; i++)  // 크기만큼
+                sumArr[i] = sumArr[i - 1] + sequence[i - 1];    // 합배열 저장
 
-            int i = 0, j = 1;   // 투포인터
-            while(j != sum.length) {    // 끝까지
-                int num = sum[j] - sum[i];  // 구간합
+            int s = 0, e = 1;   // 인덱스
 
-                if(num < k)
-                    j++;
-                else if(num > k)
-                    i++;
-                else { // 같으면
-                    list.add(new int[] {i, j - 1}); // 리스트에 추가
-                    i++;
-                    j++;
+            while (e < sumArr.length) {  // 종료조건: 선형탐색
+                int sum = sumArr[e] - sumArr[s];    // 구간합
+
+                if (sum > k) // k보다 크면
+                    s++;    // 시작인덱스 갱신
+                else if (sum < k)   // k보다 작으면
+                    e++;    // 끝인덱스 갱신
+                else {  // k와 같으면
+                    list.add(new int[]{s, e - 1}); // 리스트에 구간 저장, e - 1 => 구간합 특징
+                    s++;    // 인덱스 갱신
+                    e++;
                 }
             }
 
             Collections.sort(list, (o1, o2) -> {    // 정렬
-                if((o1[1] - o1[0]) >= (o2[1] - o2[0]))  // 구간이 짧은 순
-                    return 1;
-                else
-                    return -1;
-            });
+                return ((o1[1] - o1[0]) - (o2[1] - o2[0]));
+            }); // 구간이 짧은 것을 더 앞으로 => 음수가 나오면 두 원소의 위치를 바꾸지 않고, 양수가 나오면 두 원소의 위치를 바꿔줍니다.
 
-            return list.get(0); // 가장 짧은 구간 출력
+            return list.get(0); // 제일 짧은 구간 리턴
         }
     }
 }
