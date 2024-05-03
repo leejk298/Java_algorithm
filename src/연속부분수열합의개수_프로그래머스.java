fe850791d3f94c61275d81319a6fd567f24c2663
leejk298 <5552269@naver.com>
2024-05-03 11:36:50 +0900

    연속부분수열합의개수_프로그래머스

--- src/연속부분수열합의개수_프로그래머스.java
+++ src/연속부분수열합의개수_프로그래머스.java
@@ -3,28 +3,27 @@ import java.util.*;
 public class 연속부분수열합의개수_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(new int[] {7, 9, 1, 1, 4}));
+        System.out.println(solution.solution(new int[]{7, 9, 1, 1, 4}));
     }
 
     static class Solution {
         public int solution(int[] elements) {
-            Set<Integer> set = new HashSet<>();
-            int len = elements.length;
 
-            for(int i = 1; i <= len; i++) {
-                for(int j = 0; j < len; j++) {
-                    int sum = 0;
+            Set<Integer> hashSet = new HashSet<>(); // 집합, 중복제거
+            int len = elements.length;  // 길이
 
-                    for(int k = 0; k < i; k++) {
-                        int num = (j + k) % len;
-                        sum += elements[num];
-                    }
+            for (int i = 1; i <= len; i++) {    // 몇 회전
+                for (int j = 0; j < len; j++) { // 현재 인덱스
+                    int sum = 0;    // 총 합
 
-                    set.add(sum);
+                    for (int k = 0; k < i; k++) // 연속 몇 번까지
+                        sum += elements[(j + k) % len]; // 합 갱신
+
+                    hashSet.add(sum);   // 집합에 삽입, 중복제거
                 }
             }
 
-            return set.size();
+            return hashSet.size();  // 총 개수 리턴
         }
     }
 }
