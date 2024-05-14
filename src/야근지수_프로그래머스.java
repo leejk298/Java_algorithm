5be18bcb5bb06cd577578db56dc3855bc713f3f2
leejk298 <5552269@naver.com>
2024-05-14 14:29:06 +0900

    야근지수_프로그래머스

--- src/야근지수_프로그래머스.java
+++ src/야근지수_프로그래머스.java
@@ -9,20 +9,19 @@ public class 야근지수_프로그래머스 {
     static class Solution {
         public long solution(int n, int[] works) {
 
-            // 큰 값 우선 줄이기위해 => 최대힙 구현
-            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());    // 큰 값 우선 줄이기위해 => 최대힙 구현
 
-            for (int w : works)
+            for (int w : works) // 입력배열 순회
                 pq.offer(w);    // 우선순위 큐에 삽입
 
             for (int i = 0; i < n; i++) {    // 크기만큼
                 int now = pq.poll();    // 하나 꺼내어
 
-                if (now > 1)
+                if (now > 1)    // 1보다 크면
                     pq.offer(now - 1);  // 하나 줄이고 다시 삽입 => 크기 비교
             }
 
-            long answer = 0;
+            long answer = 0;    // 결과값
 
             while (!pq.isEmpty()) {  // 비어있지않으면
                 int num = pq.poll();    // 하나 꺼내어
@@ -30,7 +29,7 @@ public class 야근지수_프로그래머스 {
                 answer += (long) Math.pow(num, 2);   // 제곱
             }
 
-            return answer;
+            return answer;  // 결과값 리턴
         }
     }
 }
