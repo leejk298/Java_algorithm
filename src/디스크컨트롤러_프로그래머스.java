ab4c5ac57b3b8735e1dae484407a33a71e79094d
leejk298 <5552269@naver.com>
2024-05-15 19:40:21 +0900

    디스크컨트롤러_프로그래머스

--- src/디스크컨트롤러_프로그래머스.java
+++ src/디스크컨트롤러_프로그래머스.java
@@ -3,24 +3,23 @@ import java.util.*;
 public class 디스크컨트롤러_프로그래머스 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(new int[][] {{0, 3}, {1, 9}, {2, 6}}));
+        System.out.println(solution.solution(new int[][]{{0, 3}, {1, 9}, {2, 6}}));
     }
 
     static class Solution {
         public int solution(int[][] jobs) {
 
+            Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);   // 요청시간 오름차순
+            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);  // 소요시간 오름차순
+
             int len = jobs.length;  // 길이
             int index = 0, count = 0, end = 0, sum = 0; // 초기화
 
-            Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);   // 요청시간 오름차순
-
-            PriorityQueue<int []> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);  // 소요시간 오름차순
-
-            while(count < len) {    // 길이만큼
-                while(index < len && jobs[index][0] <= end)     // 유효하면
+            while (count < len) {    // 길이만큼
+                while (index < len && jobs[index][0] <= end)     // 유효하면
                     pq.offer(jobs[index++]);
 
-                if(pq.isEmpty())    // 비어있으면
+                if (pq.isEmpty())    // 비어있으면
                     end = jobs[index][0];   // 해당 작업 요청시간
                 else {  // 비어있지않으면
                     int[] now = pq.poll();  // 하나 꺼내어 -> minHeap (소요시간)
