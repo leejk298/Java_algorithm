5bad2eff38ec709d2b21fffc9c20b3404cc4a064
leejk298 <5552269@naver.com>
2024-05-17 12:15:54 +0900

    최고의집합_프로그래머스

--- src/최고의집합_프로그래머스.java
+++ src/최고의집합_프로그래머스.java
@@ -6,22 +6,24 @@ public class 최고의집합_프로그래머스 {
 
     static class Solution {
         public int[] solution(int n, int s) {
+
             int[] answer = new int[n];  // 크기: n
 
-            if(n > s)   // 존재 x
-                return new int[]{-1};
+            if (n > s)   // 존재 x
+                return new int[]{-1};   // -1 리턴
 
-            int i = 0;  // index
-            int N = n;  // copy
-            while(i < N) {  // 크기만큼
-                int num =  s / n;   // 자연스럽게 오름차순
-                answer[i++] = num;
+            int i = 0, N = n;  // 인덱스, copy
+
+            while (i < N) {  // 크기만큼
+                int num = s / n;   // 자연스럽게 오름차순
+
+                answer[i++] = num;  // 결과배열 저장
 
                 s -= num;   // 합 갱신
                 n--;    // 개수 갱신
             }
 
-            return answer;
+            return answer; // 결과배열 리턴
         }
     }
 }
