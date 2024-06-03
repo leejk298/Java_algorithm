10a3860f0004310b52679699158771ba7bc24cdf
leejk298 <5552269@naver.com>
2024-06-03 10:02:04 +0900

    최소연산_백준

--- src/최소연산_백준.java
+++ src/최소연산_백준.java
@@ -10,6 +10,7 @@ public class 최소연산_백준 {
     static long A, B, count;    // A -> B, 걸리는 최소연산수
 
     public static void init() { // 초기화
+
         Scanner sc = new Scanner(System.in);    // 입력
 
         A = sc.nextLong();  // A
@@ -18,10 +19,11 @@ public class 최소연산_백준 {
     }
 
     public static void DFS(long A, int cnt) {   // DFS
-        if(A > B)   // 크면 함수 종료
+
+        if (A > B)   // 크면 함수 종료
             return;
 
-        if(A == B) {    // 같으면
+        if (A == B) {    // 같으면
             count = cnt;    // 최소연산수 저장
             return;     // 종료
         }
