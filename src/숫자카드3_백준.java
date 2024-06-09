cfb3e188dffb64c904875145ea17366ce1d4de8d
leejk298 <5552269@naver.com>
2024-06-09 13:26:46 +0900

    숫자카드3_백준

--- src/숫자카드3_백준.java
+++ src/숫자카드3_백준.java
@@ -19,21 +19,23 @@ public class 숫자카드3_백준 {
 
         N = Integer.parseInt(st.nextToken());   // 크기
         hashMap = new HashMap<>();  // 초기화
-        StringBuilder sb = new StringBuilder(); // 결과 문자열
 
         st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
-        for(int i = 0; i < N; i++) {    // 크기만큼
+
+        for (int i = 0; i < N; i++) {    // 크기만큼
             int num = Integer.parseInt(st.nextToken()); // 숫자
             hashMap.put(num, hashMap.getOrDefault(num, 0) + 1); // 해시맵 구현
         }
 
         M = Integer.parseInt(bf.readLine());    // 크기
+        StringBuilder sb = new StringBuilder(); // 결과 문자열
 
         st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
-        for(int i = 0; i < M; i++) {    // 크기만큼
+
+        for (int i = 0; i < M; i++) {    // 크기만큼
             int num = Integer.parseInt(st.nextToken()); // 숫자
 
-            if(hashMap.containsKey(num))    // 해당 키값이 존재하면
+            if (hashMap.containsKey(num))    // 해당 키값이 존재하면
                 sb.append(hashMap.get(num) + " ");  // 해당하는 밸류값 추가
             else    // 아니면
                 sb.append(0 + " "); // 0 추가
