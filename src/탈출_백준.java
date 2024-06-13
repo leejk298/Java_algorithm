bd0d6cffe15d54a620923997f60bb005d9ba5547
leejk298 <5552269@naver.com>
2024-06-13 19:28:57 +0900

    탈출_백준

--- src/탈출_백준.java
+++ src/탈출_백준.java
@@ -17,6 +17,7 @@ public class 탈출_백준 {
     static Queue<int[]> xQ; // *
 
     public static void init() throws IOException {  // 초기화
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));   // 입력 버퍼
         StringTokenizer st = new StringTokenizer(bf.readLine());    // 한 줄 스트링
 
@@ -49,45 +50,47 @@ public class 탈출_백준 {
 
     public static boolean BFS() {   // BFS
 
-        while(!sQ.isEmpty()) {  // S가 있으면
+        while (!sQ.isEmpty()) {  // S가 있으면
             int xSize = xQ.size();  // * 크기 -> 한 회전씩 돌기위해
-            for(int i = 0; i < xSize; i++) {    // 크기만큼
+            for (int i = 0; i < xSize; i++) {    // 크기만큼
                 int[] now = xQ.poll();  // 하나 꺼내어
 
                 int nowX = now[0], nowY = now[1];   // 좌표
-                for(int k = 0; k < 4; k++) {    // 4방향
+
+                for (int k = 0; k < 4; k++) {    // 4방향
                     int tmpX = nowX + dx[k], tmpY = nowY + dy[k];   // 다음 좌표
 
-                    if(isNotValidPos(tmpX, tmpY))  // 유효한지
+                    if (isNotValidPos(tmpX, tmpY))  // 유효한지
                         continue;
 
-                    if(map[tmpX][tmpY] == '.') {    // . 이면
+                    if (map[tmpX][tmpY] == '.') {    // . 이면
                         map[tmpX][tmpY] = '*';  // * 로 바꿈
-                        xQ.offer(new int[] {tmpX, tmpY});   // 큐에 삽입 -> 다음 회전에서 사용
+                        xQ.offer(new int[]{tmpX, tmpY});   // 큐에 삽입 -> 다음 회전에서 사용
                     }
                 }
             }
 
             int sSize = sQ.size();  // S 크기 -> 한 회전씩
-            for(int i = 0; i < sSize; i++) {    // 크기만큼
+
+            for (int i = 0; i < sSize; i++) {    // 크기만큼
                 int[] now = sQ.poll();  // 하나 꺼내어
                 int nowX = now[0], nowY = now[1], nowD = now[2];    // 좌표, 거리
 
-                for(int k = 0; k < 4; k++) {    // 4방향
+                for (int k = 0; k < 4; k++) {    // 4방향
                     int tmpX = nowX + dx[k], tmpY = nowY + dy[k];   // 다음 좌표
 
-                    if(isNotValidPos(tmpX, tmpY))  // 유효한지
+                    if (isNotValidPos(tmpX, tmpY))  // 유효한지
                         continue;
 
-                    if(map[tmpX][tmpY] == 'D') {    // D 이면, 도착이면
-                        answer = Math.min(answer, nowD + 1);    // 최소값, 값이 여러개 나올 수 있으므로 
+                    if (map[tmpX][tmpY] == 'D') {    // D 이면, 도착이면
+                        answer = Math.min(answer, nowD + 1);    // 최소값, 값이 여러개 나올 수 있으므로
 
                         return true;    // true 리턴
                     }
 
-                    if(map[tmpX][tmpY] == '.') {    // . 이면, 갈 수 있으면
+                    if (map[tmpX][tmpY] == '.') {    // . 이면, 갈 수 있으면
                         map[tmpX][tmpY] = 'S';  // S 로 바꿈
-                        sQ.offer(new int[] {tmpX, tmpY, nowD + 1}); // 좌표와 거리 + 1 저장, 다음 회전에 사용
+                        sQ.offer(new int[]{tmpX, tmpY, nowD + 1}); // 좌표와 거리 + 1 저장, 다음 회전에 사용
                     }
                 }
             }
@@ -100,7 +103,7 @@ public class 탈출_백준 {
 
         init(); // 초기화
 
-        if(BFS())   // true 이면
+        if (BFS())   // true 이면
             System.out.print(answer);   // 최단거리 출력
         else    // false 이면
             System.out.print("KAKTUS");  // KAKTUS 출력
