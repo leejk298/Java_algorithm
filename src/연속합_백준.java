5476e3304eb6a1bb0b661ceacd13494e98ee82e4
leejk298 <5552269@naver.com>
2024-06-20 14:20:13 +0900

    연속합_백준

--- src/연속합_백준.java
+++ src/연속합_백준.java
@@ -15,21 +15,19 @@ public class 연속합_백준 {
 
         N = sc.nextInt();   // 크기
 
-        // 초기화
-        arr = new int[N];
-        dp = new int[N];
+        arr = new int[N];   // 입력배열
+        dp = new int[N];    // dp 배열
 
-        for(int i = 0; i < N; i++)  // 크기만큼
+        for (int i = 0; i < N; i++)  // 크기만큼
             arr[i] = sc.nextInt();  // 입력배열 저장
     }
 
     public static void printMax() { // 최대값 출력
 
-        // 초기화
-        dp[0] = arr[0];
-        max = arr[0];
+        dp[0] = arr[0]; // 초기값
+        max = arr[0];   // 최대값
 
-        for(int i = 1; i < N; i++) {    // 크기만큼
+        for (int i = 1; i < N; i++) {    // 크기만큼
             dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);   // dp 배열 저장
             max = Math.max(max, dp[i]); // 최대값 저장
         }
